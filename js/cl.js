36a8f3bc489cafc17b2c8e9af3efe659cc567d29
Leo Meyerovich <leo@graphistry.com>
2014-01-29 22:25:12 -0800

    Canonicalize promise code

--- js/cl.js
+++ js/cl.js
@@ -5,27 +5,25 @@ define(["Q"], function (Q) {
     catch (e) { }
     console.log("Current?", CURRENT_CL);
     
-	function create(gl) {
-		var deferred = Q.defer();
-		
+	var create = Q.promised(function create (gl) {
 		if (typeof(webcl) === "undefined") {
-		    deferred.reject("WebCL does not appear to be supported in your browser");
+		    throw "WebCL does not appear to be supported in your browser";
 		}
 		
 		var cl = webcl;
 		if (cl === null) {
-			deferred.reject("Can't access WebCL object");
+			throw "Can't access WebCL object";
 		}
 		
 		var platforms = cl.getPlatforms();
 		if (platforms.length === 0) {
-			deferred.reject("Can't find any WebCL platforms");
+			throw "Can't find any WebCL platforms";
 		}
 		var platform = platforms[0];
 		
 		var devices = platform.getDevices(cl.DEVICE_TYPE_GPU);
 		if (devices.length === 0) {
-			deferred.reject("No WebCL devices of specified type (" + cl.DEVICE_TYPE_GPU + ") found")
+			throw "No WebCL devices of specified type (" + cl.DEVICE_TYPE_GPU + ") found";
 		}
 		var device = devices[0];
 		
@@ -46,7 +44,7 @@ define(["Q"], function (Q) {
             });
         }
 		if (context === null) {
-			deferred.reject("Error creating WebCL context");
+			throw "Error creating WebCL context";
 		}
 		
 		var queue = context.createCommandQueue(device, null);
@@ -66,191 +64,136 @@ define(["Q"], function (Q) {
 		clObj.createBuffer = createBuffer.bind(this, clObj);
 		clObj.createBufferGL = createBufferGL.bind(this, clObj);
 		
-		deferred.resolve(clObj);
-		
-		return deferred.promise;
-	}
+		return clObj;
+	});
 	
 	
-	function compile(cl, source, kernelName) {
-		var deferred = Q.defer()
-		
-		try {
-			var program = cl.context.createProgram(source);
-			program.build([cl.device]);
-			var kernel = program.createKernel(kernelName);
+	var compile = Q.promised(function (cl, source, kernelName) {
+		var program = cl.context.createProgram(source);
+		program.build([cl.device]);
+		var kernel = program.createKernel(kernelName);
 			
-			var kernelObj = {
-				"cl": cl,
-				"kernel": kernel,
-			}
-			kernelObj.call = call.bind(this, kernelObj);
-			kernelObj.setArgs = setArgs.bind(this, kernelObj);
-			
-			deferred.resolve(kernelObj);
-		} catch(e) {
-			deferred.reject(e);
+		var kernelObj = {
+			"cl": cl,
+		    "kernel": kernel,
 		}
-		
-		return deferred.promise;
-	}
+		kernelObj.call = call.bind(this, kernelObj);
+	 	kernelObj.setArgs = setArgs.bind(this, kernelObj);
+			
+	    return kernelObj;
+	});
 	
 	
 	// Executes the specified kernel, with `threads` number of threads, setting each element in
 	// `args` to successive position arguments to the kernel before calling.
-	function call(kernel, threads, args, types) {
+	var call = Q.promised(function (kernel, threads, args, types) {
 		args = args || [];
-		
-		return Q.promise(function(resolve, reject, notify) {
-			// For all args other than the first (which is the kernel,) set each to successive
-			// positional arguments to the kernel
-			try {
-				// for(var i = 0; i < args.length; i++) {
-				// 	kernel.kernel.setArg(i, args[i]);
-				// }
-				
-				var workgroupSize = new Int32Array([threads]);
-				if (CURRENT_CL) {
-				    kernel.cl.queue.enqueueNDRangeKernel(kernel.kernel, workgroupSize.length, [], workgroupSize, []);
-				} else {
-				    kernel.cl.queue.enqueueNDRangeKernel(kernel.kernel, null, workgroupSize, null);				
-				}
-				kernel.cl.queue.finish();
-			} catch(err) {
-				reject(err);
-			}
-			
-			resolve(kernel);
-		});
-	}
-	
-	
-	function setArgs(kernel, args, types) {		
-		var deferred = Q.defer();
-		
-		try {
-			for(var i = 0; i < args.length; i++) {
-			    if (CURRENT_CL) {
-				    kernel.kernel.setArg(i, args[i]);
-				} else {
-				    kernel.kernel.setArg(i, args[i].length ? args[i][0] : args[i], types[i]);
-				}
-			}			
-			deferred.resolve(kernel);
-		} catch(err) {
-			deferred.reject(err);
+
+		// For all args other than the first (which is the kernel,) set each to successive
+		// positional arguments to the kernel
+
+		var workgroupSize = new Int32Array([threads]);
+		if (CURRENT_CL) {
+		    kernel.cl.queue.enqueueNDRangeKernel(kernel.kernel, workgroupSize.length, [], workgroupSize, []);
+		} else {
+		    kernel.cl.queue.enqueueNDRangeKernel(kernel.kernel, null, workgroupSize, null);				
 		}
+		kernel.cl.queue.finish();
 		
-		return deferred;
-	}
+		return kernel;
+	});
 	
 	
-	function createBuffer(cl, size) {
-		return Q.promise(function(resolve, reject, notify) {
-			var buffer = cl.context.createBuffer(cl.cl.MEM_READ_WRITE, size);
-			if(buffer === null) {
-				reject("Could not create the WebCL buffer");
+	var setArgs = Q.promised(function (kernel, args, types) {		
+	    for (var i = 0; i < args.length; i++) {
+		    if (CURRENT_CL) {
+			    kernel.kernel.setArg(i, args[i]);
 			} else {
-				var bufObj = {
-					"buffer": buffer,
-					"cl": cl,
-					"size": size,
-					"acquire": function() {
-						return Q.promise(function(resolve) {
-							resolve();
-						});
-					},
-					"release": function() {
-						return Q.promise(function(resolve) {
-							resolve();
-						});
-					}
-				};
-				bufObj.write = write.bind(this, bufObj);
-				bufObj.read = read.bind(this, bufObj);
-				bufObj.copyBuffer = copyBuffer.bind(this, cl, bufObj);
-				resolve(bufObj);
+			    kernel.kernel.setArg(i, args[i].length ? args[i][0] : args[i], types[i]);
 			}
-		});
-	}
+		}			
+		return kernel;
+	});
+	
+		
+	var createBuffer = Q.promised(function createBuffer(cl, size) {
+	    var buffer = cl.context.createBuffer(cl.cl.MEM_READ_WRITE, size);
+		if (buffer === null) {
+		    throw "Could not create the WebCL buffer";
+		} else {
+		    var bufObj = {
+			    "buffer": buffer,
+				"cl": cl,
+				"size": size,
+				"acquire": function() { return Q(); },
+				"release": function() { return Q(); }
+			};
+			bufObj.write = write.bind(this, bufObj);
+			bufObj.read = read.bind(this, bufObj);
+			bufObj.copyBuffer = copyBuffer.bind(this, cl, bufObj);
+			return bufObj;
+		}
+	});
 	
 	
 	// TODO: If we call buffer.acquire() twice without calling buffer.release(), it should have no
 	// effect.
-	function createBufferGL(cl, vbo) {
-		return Q.promise(function(resolve, reject, notify) {
-			var buffer = cl.context.createFromGLBuffer(cl.cl.MEM_READ_WRITE, vbo);
-			if (buffer === null) {
-				reject("Could not create WebCL buffer from WebGL buffer");
-			} else {
-				var bufObj = {
-					"buffer": buffer,
-					"cl": cl,
-					"size": buffer.getInfo(cl.cl.MEM_SIZE),
-					"acquire": function() {
-						return Q.promise(function(resolve) {
-							try{
-								cl.queue.enqueueAcquireGLObjects([buffer]);
-								resolve();
-							} catch(err) {
-								reject(err);
-							}
-						});
-					},
-					"release": function() {
-						return Q.promise(function(resolve) {
-							try{
-								cl.queue.enqueueReleaseGLObjects([buffer]);
-								resolve();
-							} catch(err) {
-								reject(err);
-							}
-						});
-					}
-				};
-				bufObj.write = write.bind(this, bufObj);
-				bufObj.read = read.bind(this, bufObj);
-				bufObj.copyBuffer = copyBuffer.bind(this, cl, bufObj);
-				
-				resolve(bufObj);
-			}
-		});
-	}
+	var createBufferGL = Q.promised(function (cl, vbo) {
+		var buffer = cl.context.createFromGLBuffer(cl.cl.MEM_READ_WRITE, vbo);
+		if (buffer === null) {
+			throw "Could not create WebCL buffer from WebGL buffer";
+		} else {
+			var bufObj = {
+				"buffer": buffer,
+				"cl": cl,
+				"size": buffer.getInfo(cl.cl.MEM_SIZE),
+				"acquire": Q.promised(function() {
+					cl.queue.enqueueAcquireGLObjects([buffer]);
+				}),
+				"release": Q.promised(function() {
+					cl.queue.enqueueReleaseGLObjects([buffer]);
+				})
+			};
+			bufObj.write = write.bind(this, bufObj);
+			bufObj.read = read.bind(this, bufObj);
+			bufObj.copyBuffer = copyBuffer.bind(this, cl, bufObj);
+			
+			return bufObj;
+		}		
+	});
 	
 	
-	function copyBuffer(cl, source, destination) {
-		return Q.promise(function(resolve, reject, notify) {
-			// TODO: acquire the buffers before copying them
-			cl.queue.enqueueCopyBuffer(source.buffer, destination.buffer, 0, 0, Math.min(source.size, destination.size));
-			resolve(destination);
-		});
-	}
+	var copyBuffer = Q.promised(function (cl, source, destination) {
+		// TODO: acquire the buffers before copying them
+		cl.queue.enqueueCopyBuffer(source.buffer, destination.buffer, 0, 0, Math.min(source.size, destination.size));
+		return destination;
+	});
 	
-	
-	function write(buffer, data) {
-		return (buffer.acquire().then(function() {
-			buffer.cl.queue.enqueueWriteBuffer(buffer.buffer, true, 0, data.byteLength, data);
-			return buffer.release().then(function() {
+
+	var write = Q.promised(function write(buffer, data) {
+		return buffer.acquire()
+		    .then(function () { 
+		        buffer.cl.queue.enqueueWriteBuffer(buffer.buffer, true, 0, data.byteLength, data);		    
+			    return buffer.release();
+		    })
+		    .then(function() {
 				buffer.cl.queue.finish();
 				return buffer;
-			})
-		}))
-	}
+		    });
+	});
 	
 	
-	function read(buffer, target) {
-		return (
-			buffer.acquire()
+	var read = Q.promised(function (buffer, target) {
+		return buffer.acquire()
 			.then(function() {
 				var copySize = Math.min(buffer.size, target.length * target.BYTES_PER_ELEMENT);
 				buffer.cl.queue.enqueueReadBuffer(buffer.buffer, true, 0, copySize, target);
-				return buffer.release;
+				return buffer.release();
 			})
 			.then(function() {
 				return buffer;
-			})
-		);
-	}
+			});
+	});
 	
 	
 	return {
