7d160f4ce11339597c54b66cbfc92f48624f8163
Matt Torok <matt@graphistry.com>
2014-02-06 03:06:48 -0800

    naive: Refactor main.js setup code to be more readable.
    This moves from the messy nested promises and nested functions that existed previously into more linear, straightforward code.
    
    As part of this, we removed the FPS counter on the console (it wasn't especially accurate anyway,) and the 'auto-shutoff when animation becomes slow' feature (it would trigger when requestAnimationFrame() would pause due to the window losing focus, and would often have false positives during regular operation.)

--- js/main.js
+++ js/main.js
@@ -12,128 +12,111 @@ require.config({
 	}
 });
 
-require(["jQuery", "NBody", "glMatrix", "RenderGL", "SimCL", "MatrixLoader", "Q"], 
-  function($, NBody, glMatrix, RenderGL, SimCL, MatrixLoader, Q) {
-  
-    function drawGraph (clGraph, graph) {
-      console.log('drawing', clGraph, graph);
-      var points = graph.nodes.map(function (node, i) {
-        return [node.index / graph.nodes.length, i / graph.nodes.length, 0];        
-      });
-      clGraph.setPoints(points);      
-    }  
-    
-    function loadMatrices(clGraph) {
-      var files = MatrixLoader.ls("data/matrices.json");
-      files.then(function (files) {
-        $('#matrices').append(
-          files
-            .map(function (file) {
-              var base = file.f.split(/\/|\./)[1]
-              var size = file.KB > 1000 ? (Math.round(file.KB / 1000) + " MB") : (file.KB + " KB");
-              var link = $("<a></a>")
-                .attr("href", "javascript:void(0)")
-                .text(base + " (" + size + ")")
-                .click(function () {
-                  $('#filename').text(base);
-                  $('#filesize').text(size);              
-                  var graph = MatrixLoader.load(file.f);
-                  graph.then(function (v) {             
-                    console.log('got', v);
-                    $('#filenodes').text(v.nodes.length);        
-                    $('#fileedges').text(v.links.length);
-                    $('#fileedgelist').text(
-                      v.links
-                        .slice(0, 20)
-                        .map(function (pair) { 
-                          return '(' + pair.source.index + ',' + pair.target.index + ')'; })
-                        .join(' '));
-                                            
-                  });
-                  Q.promised(drawGraph)(clGraph, graph);
-                });
-              return $('<li></li>').append(link);
-            }));
-      });
-    }
-  
-	function run() {
+require(["jQuery", "NBody", "glMatrix", "RenderGL", "SimCL", "MatrixLoader", "Q"],
+	function($, NBody, glMatrix, RenderGL, SimCL, MatrixLoader, Q) {
+	var graph = null,
+		animId = null;
+
+
+	// Given a set of graph data, load the points into the N-body simulation
+	function drawGraph (clGraph, graphData) {
+		console.log('drawing', clGraph, graphData);
+		var points = graphData.nodes.map(function (node, i) {
+		return [node.index / graphData.nodes.length, i / graphData.nodes.length, 0];
+		});
+		clGraph.setPoints(points);
+	}
+
+
+	// Load the index of all the matrices we know about, and create links on the page to load each
+	// of them
+	function loadMatrices(clGraph) {
+		var files = MatrixLoader.ls("data/matrices.json");
+		files.then(function (files) {
+		$('#matrices').append(
+			files
+			.map(function (file) {
+				var base = file.f.split(/\/|\./)[1]
+				var size = file.KB > 1000 ? (Math.round(file.KB / 1000) + " MB") : (file.KB + " KB");
+				var link = $("<a></a>")
+				.attr("href", "javascript:void(0)")
+				.text(base + " (" + size + ")")
+				.click(function () {
+					$('#filename').text(base);
+					$('#filesize').text(size);
+					var graphData = MatrixLoader.load(file.f);
+					graphData.then(function (v) {
+					console.log('got', v);
+					$('#filenodes').text(v.nodes.length);
+					$('#fileedges').text(v.links.length);
+					$('#fileedgelist').text(
+						v.links
+						.slice(0, 20)
+						.map(function (pair) {
+							return '(' + pair.source.index + ',' + pair.target.index + ')'; })
+						.join(' '));
+					});
+					Q.promised(drawGraph)(clGraph, graphData);
+				});
+				return $('<li></li>').append(link);
+			}));
+		});
+	}
+
+
+	function animatePromise(promise) {
+		if(animId == null) {
+			return promise.then(function() {
+				animId = window.requestAnimationFrame(function() {
+					animatePromise(promise);
+				});
+				return animId;
+			});
+		} else {
+			return Q.promised(function() { return animId; });
+		}
+	}
+
+
+	function stopAnimation() {
+		if(animId != null) {
+			window.cancelAnimationFrame(animationId);
+			animId = null;
+		}
+	}
+
+
+	function setup() {
 		console.log("Running Naive N-body simulation");
 
-		var points = createPoints(4096);
-
-		var graph = NBody.create(SimCL, RenderGL, $("#simulation")[0])
-		graph.then(function(graph) {
+		return NBody.create(SimCL, RenderGL, $("#simulation")[0])
+		.then(function(createdGraph) {
+			graph = createdGraph;
 			console.log("N-body graph created.");
 
-			graph.setPoints(points)
-			.then(function() {
-				var button = $("#step-button");
-				var animationId = null;
-				var lastFrameTime = Date.now();
-				var frameTimer = Date.now();
-				var frames = 0;
+			var points = createPoints(4096);
+			return graph.setPoints(points);
+		})
+		.then(function() {
+			var button = $("#step-button");
 
-				function startAnimation() {
-					button.text("Stop");
+			button.on("click", function() {
+				button.text("Stop");
+
+				button.on("click", function() {
+					button.text("Animate");
 					button.on("click", stopAnimation);
-					frameTimer = Date.now();
-					lastFrameTime = Date.now();
+				});
 
-					function runAnimation() {
-						if(animationId === null) {
-							return;
-						}
-
-						graph.tick().then(function() {
-							frames++;
-							var currentFrameTime = Date.now();
-
-							if(frames % 100 === 0) {
-								var newFrameTimer = Date.now();
-								console.debug("FPS:", (newFrameTimer - frameTimer)/100);
-								frameTimer = newFrameTimer;
-							}
-
-							if(frames > 2 && currentFrameTime - lastFrameTime > 5000) {
-								console.error("Peformance seems to be dying. Disabling animation.")
-								lastFrameTime = currentFrameTime;
-								stopAnimation();
-							}
-							// else if(frames > 100) {
-							//  console.log("Ran 100 frames. Stopping.");
-							//  lastFrameTime = currentFrameTime;
-							//  stopAnimation();
-							// }
-							else {
-								lastFrameTime = currentFrameTime;
-								animationId = window.requestAnimationFrame(runAnimation);
-							}
-						}, function(err) {
-							console.error("Could not execute a tick. Error:", err);
-						})
-					}
-
-					animationId = window.requestAnimationFrame(runAnimation);
-				}
-
-				function stopAnimation() {
-					window.cancelAnimationFrame(animationId);
-					animationId = null;
-					button.text("Start");
-					button.on("click", startAnimation);
-				}
-
-				button.on("click", startAnimation);
-				button.prop("disabled", false);
-
-				return graph;
-			}, function(err) {
-				console.error("Error creating N-body graph:", err.message);
-				console.error(err);
+				animatePromise(graph.tick());
 			});
+
+			button.prop("disabled", false);
+
+			return graph;
+		}, function(err) {
+			console.err("Fatal error trying to setup graph:", err);
 		});
-		return graph;
 	}
 
 
@@ -150,8 +133,8 @@ require(["jQuery", "NBody", "glMatrix", "RenderGL", "SimCL", "MatrixLoader", "Q"
 
 		return points;
 	}
-	
-	
-	var graph = run();
+
+
+	setup();
 	loadMatrices(graph);
 });
