db7d021f5e852812394f4288eeb09351373c5f59
Matt Torok <matt@graphistry.com>
2014-02-06 16:34:28 -0800

    Naive: toward a more accurate force calculation
    With the default set of 6 points, all of them line up in a grid after a short period (which is what we want.) However, with other graphs, this is not the case. Working on fixing that.

--- js/main.js
+++ js/main.js
@@ -166,9 +166,9 @@ require(["jQuery", "NBody", "glMatrix", "RenderGL", "SimCL", "MatrixLoader", "Q"
 
 		points.push([0.5, 0.5]);
 		points.push([0.5, 0.5]);
-		// points.push([0.1, 0.1]);
-		// points.push([0.9, 0.1]);
-		// points.push([0.9, 0.9]);
+		points.push([0.1, 0.1]);
+		points.push([0.9, 0.1]);
+		points.push([0.9, 0.9]);
 		points.push([0.1, 0.9]);
 
 		return points;
