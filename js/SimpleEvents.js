3cfd6bfdf0d380509f06fd723836a24b05c6b4d7
Matt Torok <matt@graphistry.com>
2014-03-14 19:21:17 -0700

    Create simple event handling library
    This library stores a global list of event listeners, making it easy to fire an event from anywhere in the app, and have that event heard from any other points, without any intermediate layers having to know about the event and pass it up the abstraction stack.

---
+++ js/SimpleEvents.js
@@ -0,0 +1,29 @@
+define([], function () {
+    "use strict";
+    var exports = {};
+
+    // A module-level list of all the event listeners
+    var listeners = {};
+
+
+    exports.listen = function(event, callback) {
+        var eventListeners = listeners[event] || [];
+        eventListeners.push(callback);
+        listeners[event] = eventListeners;
+    };
+
+
+    // TODO: add 'remove' function
+
+
+    exports.fire = function(event, args) {
+        var eventListeners = listeners[event] || [];
+
+        for(var i in eventListeners) {
+            eventListeners[i].call(this, args);
+        }
+    };
+
+
+    return exports;
+});
