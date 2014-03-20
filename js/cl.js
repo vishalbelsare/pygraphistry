9c2307a1ac2f595a50fdcea3c866a1882a725189
Leo Meyerovich <leo@graphistry.com>
2014-03-19 21:05:29 -0700

    Bugfix for legacy platform: coerce Int32Array into Array to call reduce

--- js/cl.js
+++ js/cl.js
@@ -37,7 +37,7 @@ define(["Q"], function (Q) {
                 device: d,
                 DEVICE_TYPE: typeToString(d.getInfo(cl.DEVICE_TYPE)),
                 DEVICE_MAX_WORK_ITEM_SIZES: workItems,
-                computeUnits: workItems.reduce(function (a, b) { return a * b})
+                computeUnits: [].slice.call(workItems, 0).reduce(function (a, b) { return a * b; })
             };
         });
         devices.sort(function (a, b) { return b.computeUnits - a.computeUnits; });
