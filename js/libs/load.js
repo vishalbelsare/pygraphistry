87da4d13d0802e6b67be6663f0c472029449c3ae
Matt Torok <matt@graphistry.com>
2014-03-21 17:05:04 -0700

    Whitespace in load.js

--- js/libs/load.js
+++ js/libs/load.js
@@ -6,15 +6,16 @@ define(["jQuery", "Q"], function ($, Q) {
       var parts = matrixJson.split('/');
       parts.pop();
       var base = parts.join('/') + '/';
-            
+
       return Q($.ajax(matrixJson, {dataType: "text"}))
         .then(eval)
         .then(function (lst) {
-          return lst.map(function (f) { 
+          return lst.map(function (f) {
             return {KB: f.KB, 'f': base + f.f};
           });
         });
-    },    
+    },
+
     loadBinary: function (file) { // -> Promise Binary
       var t0 = new Date().getTime();
 
@@ -25,11 +26,11 @@ define(["jQuery", "Q"], function ($, Q) {
           max: buf[1],
           numNodes: buf[2],
           numEdges: buf[3]
-        };  
+        };
       }
 
       var res = Q.defer();
-   
+
       var xhr = new XMLHttpRequest();
       xhr.open('GET', file, true);
       xhr.responseType = 'arraybuffer';
@@ -37,19 +38,20 @@ define(["jQuery", "Q"], function ($, Q) {
         res.resolve(Binary(new Uint32Array(this.response)));
         console.log('binary load', new Date().getTime() - t0, 'ms');
       };
-      xhr.send(); 
+      xhr.send();
 
       return res.promise;
     },
+
     load: function (file) {
       var t0 = new Date().getTime();
 
       return Q($.ajax(file, {dataType: "text"})).then(function (str) {
 
         console.log('naive parse', new Date().getTime() - t0, 'ms');
-          
+
         //http://bl.ocks.org/mbostock/2846454
-        var nodes = [];    
+        var nodes = [];
         var links = str
           .split(/\n/g) // split lines
           .filter(function(d) { return d.charAt(0) != "%"; }) // skip comments
@@ -61,10 +63,10 @@ define(["jQuery", "Q"], function ($, Q) {
                 source: nodes[source] || (nodes[source] = {index: source}),
                 target: nodes[target] || (nodes[target] = {index: target})
             };
-        });    
-        
+        });
+
         console.log('naive parse + transform', new Date().getTime() - t0, 'ms');
-        
+
         return {
           nodes: nodes,
           links: links
