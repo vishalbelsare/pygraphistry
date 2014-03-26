8fc33790673a1087c2e0b7cd8f464de6d1ea861c
Matt Torok <matt@graphistry.com>
2014-03-25 17:49:16 -0700

    Refactor data list code in main.js

--- js/main.js
+++ js/main.js
@@ -19,8 +19,7 @@ require.config({
 
 require(["jQuery", "NBody", "RenderGL", "SimCL", "MatrixLoader", "Q", "Stats", "SimpleEvents"],
 function($, NBody, RenderGL, SimCL, MatrixLoader, Q, Stats, events) {
-    'use strict';
-    //Q.longStackSupport = true;
+    "use strict";
 
     var graph = null,
         animating = null,
@@ -28,76 +27,6 @@ function($, NBody, RenderGL, SimCL, MatrixLoader, Q, Stats, events) {
         num,
         numEdges = numPoints,
         dimensions = [1,1]; //[960,960];
-
-
-    // Given a set of graph data, load the points into the N-body simulation
-    function drawGraph (clGraph, graphFile) {
-
-        var points = createPoints(graphFile.numNodes, clGraph.dimensions);
-
-        return clGraph.setPoints(points)
-        .then(function() {
-            return clGraph.setEdges(graphFile.edges);
-        })
-        .then(function() {
-            return clGraph.tick();
-        });
-    }
-
-
-    function loadMatrices(clGraph) {
-        function fileNfo (file) {
-          return {
-            base: file.f.split(/\/|\./)[file.f.split(/\/|\./).length - 3],
-            size: file.KB > 1000 ? (Math.round(file.KB / 1000) + " MB") : (file.KB + " KB")
-          };
-        }
-
-        var files = MatrixLoader.ls("data/matrices.binary.json");
-        files.then(function (files) {
-          var options = files.map(function (file, i) {
-            var nfo = fileNfo(file);
-            return $('<option></option>')
-              .attr('value', i)
-              .text(nfo.base + " (" + nfo.size + ")");
-          });
-          $('#datasets')
-            .append(options)
-            .on('change', function () {
-              var file = files[parseInt(this.value)];
-              var graphFile = MatrixLoader.loadBinary(file.f);
-              graphFile.then(function (v) {
-                // console.log('got', v);
-                $('#filenodes').text('Nodes: ' + v.numNodes);
-                $('#fileedges').text('Edges: ' + v.numEdges);
-              });
-              Q.promised(drawGraph)(clGraph, graphFile);
-            });
-        });
-
-        return files;
-    }
-
-
-    function animatePromise(promise) {
-        return promise()
-        .then(function() {
-            if(animating){
-                return window.setTimeout(function() {
-                        animatePromise(promise);
-                    }, 0);
-            } else {
-                return null;
-            }
-        }, function(err) {
-            console.error("Error during animation:", err);
-        });
-    }
-
-
-    function stopAnimation() {
-        animating = false;
-    }
 
 
     function setup() {
@@ -163,7 +92,7 @@ function($, NBody, RenderGL, SimCL, MatrixLoader, Q, Stats, events) {
                 graph.tick()
                 .then(function() {
                     stepButton.prop("disabled", false);
-                })
+                });
             });
 
             animButton.prop("disabled", false);
@@ -174,21 +103,176 @@ function($, NBody, RenderGL, SimCL, MatrixLoader, Q, Stats, events) {
     }
 
 
+    function animatePromise(promise) {
+        return promise()
+        .then(function() {
+            if(animating){
+                return window.setTimeout(function() {
+                        animatePromise(promise);
+                    }, 0);
+            } else {
+                return null;
+            }
+        }, function(err) {
+            console.error("Error during animation:", err);
+        });
+    }
+
+
+    function stopAnimation() {
+        animating = false;
+    }
+
+
+    function bindSliders(graph) {
+        $('#charge').on('change', function (e) {
+        var v = $(this).val();
+        var res = 0.1;
+        for (var i = 0; i < (100-v); i++) res /= 1.3;
+        var scaled = -1 * res;
+//      console.log('charge', v, '->', scaled);
+        graph.setPhysics({charge: scaled});
+        });
+        $('#gravity').on('change', function (e) {
+        var v = $(this).val();
+        var res = 100.0;
+        for (var i = 0; i < (100-v); i++) res /= 1.3;
+        var scaled = 1 * res;
+//      console.log('gravity', v, '->', scaled);
+        graph.setPhysics({gravity: scaled});
+        });
+        $('#strength').on('change', function (e) {
+        var v = $(this).val();
+        var res = 100.0;
+        for (var i = 0; i < (100-v); i++) res /= 1.3;
+        var scaled = 1 * res;
+//      console.log('strength', v, '->', scaled);
+        graph.setPhysics({edgeStrength: scaled});
+        });
+        $('#length').on('change', function (e) {
+        var v = $(this).val();
+        var res = 100.0;
+        for (var i = 0; i < (100-v); i++) res /= 1.3;
+        var scaled = 1 * res;
+//      console.log('length', v, '->', scaled);
+        graph.setPhysics({edgeDistance: scaled});
+        });
+
+        ['points', 'edges', 'midpoints', 'midedges'].forEach(function (name) {
+            function bang () {
+                var obj = {};
+                obj[name] = $(this).is(':checked');
+                graph.setVisible(obj);
+            };
+            $('#' + name).on('change', bang);
+            bang.call($('#' + name));
+        });
+
+        ['lockPoints', 'lockEdges', 'lockMidpoints', 'lockMidedges'].forEach(function (name) {
+            function bang () {
+                var obj = {};
+                obj[name] = $(this).is(':checked');
+                graph.setLocked(obj);
+            }
+            $('#' + name).on('change', bang);
+            bang.call($('#' + name));
+        });
+
+    }
+
+
+    /**
+     * Populate the data list dropdown menu with available data, and setup actions to load the data
+     * when the user selects one of the options.
+     *
+     * @param clGraph - the NBody graph object created by NBody.create()
+     */
+    function loadDataList(clGraph) {
+        // Given a URI of a JSON data index, return an array of objects, with keys for display name,
+        // file URI, and data size
+        function getDataList(listURI) {
+            return MatrixLoader.ls(listFile)
+            .then(function (files) {
+                var listing = [];
+
+                files.forEach(function (file, i) {
+                    listing.push({
+                        f: file.f,
+                        base: file.f.split(/\/|\./)[file.f.split(/\/|\./).length - 3],
+                        KB: file.KB,
+                        size: file.KB > 1000 ? (Math.round(file.KB / 1000) + " MB") : (file.KB + " KB")
+                    });
+                });
+
+                return listing;
+            });
+        }
+
+        var dataList = [];
+
+        return getDataList("data/matrices.binary.json")
+        .then(function(matrixList){
+            matrixList = matrixList.map(function(fileInfo) {
+                fileInfo["loader"] = loadMatrix;
+                return fileInfo;
+            });
+
+            dataList = dataList.concat(matrixList);
+
+            var dataEl = $("#datasets");
+
+            dataList.forEach(function(dataSet, i) {
+                dataEl.append($('<option></option>')
+                    .attr('value', i)
+                    .text(dataSet.base + " (" + dataSet.size + ")")
+                );
+            })
+
+            $('#datasets')
+            .on('change', function () {
+                var dataSet = dataList[parseInt(this.value)];
+
+                return dataSet.loader(clGraph, dataSet.f)
+                .catch(function(err) {
+                    console.error("Error loading matrix:", err);
+                    throw err;
+                })
+            });
+
+            return dataList;
+        });
+    }
+
+
+    /**
+     * Loads the matrix data at the given URI into the NBody graph.
+     */
+    function loadMatrix(clGraph, graphFileURI) {
+        var graphFile;
+
+        return MatrixLoader.loadBinary(graphFileURI)
+        .then(function (v) {
+            graphFile = v;
+            $('#filenodes').text('Nodes: ' + v.numNodes);
+            $('#fileedges').text('Edges: ' + v.numEdges);
+
+            var points = createPoints(graphFile.numNodes, clGraph.dimensions);
+
+            return clGraph.setPoints(points);
+        })
+        .then(function() {
+            return clGraph.setEdges(graphFile.edges);
+        })
+        .then(function() {
+            return clGraph.tick();
+        });
+    }
+
+
     // Generates `amount` number of random points
     function createPoints(amount, dim) {
         // Allocate 2 elements for each point (x, y)
         var points = [];
-
-        // points.push([0.5, 0.5]);
-        // points.push([0.5, 0.5]);
-        // // points.push([0.5, 0.5]);
-
-        // points.push([0.2, 0.2]);
-        // points.push([0.1, 0.1]);
-        // points.push([0.9, 0.1]);
-        // points.push([0.9, 0.9]);
-        // points.push([0.1, 0.9]);
-        // points.push([0.5, 0.5]);
 
         for(var i = 0; i < amount; i++) {
             points.push([Math.random() * dim[0], Math.random() * dim[1]]);
@@ -212,66 +296,13 @@ function($, NBody, RenderGL, SimCL, MatrixLoader, Q, Stats, events) {
     }
 
 
-    function bindSliders(graph) {
-      $('#charge').on('change', function (e) {
-        var v = $(this).val();
-        var res = 0.1;
-        for (var i = 0; i < (100-v); i++) res /= 1.3;
-        var scaled = -1 * res;
-//      console.log('charge', v, '->', scaled);
-        graph.setPhysics({charge: scaled});
-      });
-      $('#gravity').on('change', function (e) {
-        var v = $(this).val();
-        var res = 100.0;
-        for (var i = 0; i < (100-v); i++) res /= 1.3;
-        var scaled = 1 * res;
-//      console.log('gravity', v, '->', scaled);
-        graph.setPhysics({gravity: scaled});
-      });
-      $('#strength').on('change', function (e) {
-        var v = $(this).val();
-        var res = 100.0;
-        for (var i = 0; i < (100-v); i++) res /= 1.3;
-        var scaled = 1 * res;
-//      console.log('strength', v, '->', scaled);
-        graph.setPhysics({edgeStrength: scaled});
-      });
-      $('#length').on('change', function (e) {
-        var v = $(this).val();
-        var res = 100.0;
-        for (var i = 0; i < (100-v); i++) res /= 1.3;
-        var scaled = 1 * res;
-//      console.log('length', v, '->', scaled);
-        graph.setPhysics({edgeDistance: scaled});
-      });
-
-      ['points', 'edges', 'midpoints', 'midedges'].forEach(function (name) {
-          function bang () {
-              var obj = {};
-              obj[name] = $(this).is(':checked');
-              graph.setVisible(obj);
-          };
-          $('#' + name).on('change', bang);
-          bang.call($('#' + name));
-      });
-
-      ['lockPoints', 'lockEdges', 'lockMidpoints', 'lockMidedges'].forEach(function (name) {
-            function bang () {
-              var obj = {};
-              obj[name] = $(this).is(':checked');
-              graph.setLocked(obj);
-            }
-            $('#' + name).on('change', bang);
-            bang.call($('#' + name));
-      });
-
-    }
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 
 
     setup().
     then(function() {
-        return loadMatrices(graph);
+        return loadDataList(graph);
     }, function(err) {
         console.error("Error setting up animation:", err);
     }).then(function () {
