843355599617c22c61226db0353dce27db78043e
Matt Torok <matt@graphistry.com>
2014-04-16 16:30:59 -0700

    Create getImage util method
    Takes in the URL of an image file, and returns a promise fulfilled with a HTML Image element containing the image at the URL, once loaded.

--- js/util.js
+++ js/util.js
@@ -1,10 +1,37 @@
 define(["jQuery", "Q"], function($, Q) {
     'use strict';
 
+
     function getSource(id) {
+        // TODO: Could we use HTML <script> elements instead of AJAX fetches? We could possibly
+        // set the src of the script to our content, the type to something other than JS. Then, we
+        // listen for the onload event. In this way, we may be able to load content from disk
+        // without running a server.
+
         var url = "shaders/" + id;
 
         return Q($.ajax(url, {dataType: "text"}));
+    }
+
+    /**
+     * Fetch an image as an HTML Image object
+     *
+     * @returns a promise fulfilled with the HTML Image object, once loaded
+     */
+    function getImage(url) {
+        var deferred = Q.defer();
+        var img = new Image();
+
+        img.addEventListener("load", function() {
+            deferred.resolve(img);
+        });
+        img.addEventListener("error", function(msg) {
+            deferred.reject(msg);
+        });
+
+        img.src = url;
+
+        return deferred.promise;
     }
 
 
@@ -16,6 +43,7 @@ define(["jQuery", "Q"], function($, Q) {
 
     return {
         "getSource": getSource,
+        "getImage": getImage,
         "extend": extend
     };
 });
