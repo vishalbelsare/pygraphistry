88d750348edcd61442492c16b277414d3205f2b3
Matt Torok <github@overblown.net>
2014-08-06 07:28:47 -0700

    Basic graph main module

---
+++ src/main.graph.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var $            = require("jquery"),
+    renderConfig = require("render-config"),
+    renderer     = require("./renderer.js"),
+    Cameras      = require("../../../../superconductorjs/src/Camera.js"),
+    interaction  = require("./interaction.js"),
+    ui           = require("./ui.js");
+
+
+function init (canvas) {
+
+    var camera = new Cameras.Camera2d({
+            left: -0.15,
+            right: 5,
+            bottom: 5, // (5 * (1 / (700/700))) - 0.15,
+            top: -0.15 // - 0.15
+        });
+
+    var socket = io.connect("http://localhost", {reconnection: false, transports: ["websocket"]});
+
+    var gl = renderer.init(canvas);
+    renderer.setGlOptions(gl, renderConfig.options);
+    var programs = renderer.createPrograms(gl, renderConfig.programs);
+    var buffers = renderer.createBuffers(gl, renderConfig.models);
+    renderer.setCamera(renderConfig, gl, programs, camera);
+
+    interaction.setupDrag($(".sim-container"), camera)
+        .merge(interaction.setupScroll($(".sim-container"), camera))
+        .subscribe(function(newCamera) {
+            renderer.setCamera(renderConfig, gl, programs, newCamera);
+            renderer.render(renderConfig, gl, programs, buffers);
+        });
+
+    var glBufferStoreSize = 0;
+    var lastHandshake = new Date().getTime();
+
+    socket.on("vbo_update", function (data, handshake) {
+        try {
+            var now = new Date().getTime();
+            console.log("got VBO update message", now - lastHandshake, "ms");
+
+            renderer.loadBuffer(gl, buffers.mainVBO, trimmedArray, data.numVertices <= glBufferStoreSize);
+            renderer.render(renderConfig, gl, programs, buffers, data.numVertices);
+
+            glBufferStoreSize = Math.max(glBufferStoreSize, data.numVertices);
+
+            handshake(Date.now() - now);
+        } catch (e) {
+            console.error("Error retrieving WebGL buffer data from server:", e, new Error().stack);
+        }
+    });
+
+    socket.on("error", function(reason) {
+        ui.error("Connection error (reason: " + reason + ")");
+    });
+    socket.on("disconnect", function(reason) {
+        ui.error("Disconnected (reason: " + reason + ")");
+    });
+}
+
+window.addEventListener("load", function(){
+    init($("#simulation")[0]);
+});
