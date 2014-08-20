715ef3926e1cb3d9d66215deb9b1a1abfea5b108
Matt Torok <github@overblown.net>
2014-08-20 00:37:40 -0700

    Add function to UI to parse URL query string

--- src/ui.js
+++ src/ui.js
@@ -14,3 +14,26 @@ exports.error = function(message) {
         .append($msg)
         .css("visibility", "visible");
 };
+
+
+/**
+ * Returns an Object representing each of the window URL's query paramters.
+ * @example the url "index.html?foo=bar&baz" returns {"foo": "bar", "baz": true}
+ */
+exports.getQueryParams = function() {
+    var query = window.location.search.substring(1);
+
+    var spaces = /\+/g;
+    var qParts = /([^&=]+)(=([^&]*))?/;
+
+    return query.split("&").reduce(function(res, param) {
+        if(param === "") { return res; }
+
+        var parts = qParts.exec(param);
+        var key = parts[1].replace(spaces, " ");
+        var value = (typeof parts[3] === "undefined" ? "" : parts[3]).replace(spaces, " ");
+
+        res[key] = value;
+        return res;
+    }, {});
+};
