b7f0af97f91053eae9b0d969d81b4ad778d56df9
Matt Torok <github@overblown.net>
2014-11-14 16:37:03 -0800

    (WIP) Import Quinn's touch interaction commit from sc2 repo

--- src/interaction.js
+++ src/interaction.js
@@ -10,31 +10,87 @@ require('rx-jquery');
 
 
 /**
+ * Set a variable to detect if the device is touch based.
+ */
+var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+var touchBased = iOS;
+
+/**
+ * Helper function to compute distance for pinch-zoom
+ */
+function straightLineDist(p1, p2) {
+    var dx = p1.x - p2.x;
+    var dy = p1.y - p2.y;
+    return Math.sqrt((dx*dx) + (dy*dy));
+}
+
+
+/**
  * Adds event listeners for drag/zoom and changes the local camera position in response
  */
 function setupDrag($eventTarget, camera) {
-    return $eventTarget.mousedownAsObservable()
-        .flatMapLatest(function(clickPos) {
-            clickPos.preventDefault();
+    if (touchBased) {
+        var $$eventTarget = $$('.sim-container');
 
-            return $('html').mousemoveAsObservable()
-                .takeUntil($('html').mouseupAsObservable())
-                .distinctUntilChanged(function(pos) { return {x: pos.pageX, y: pos.pageY}; })
-                .scan({x: clickPos.pageX, y: clickPos.pageY, deltaX: 0, deltaY: 0}, function(accPos, curPos) {
-                    // Calculate the distance moved (since last event) for each move event
-                    return {
-                        deltaX: (curPos.pageX - accPos.x) / $eventTarget.width(),
-                        deltaY: (curPos.pageY - accPos.y) / $eventTarget.height(),
-                        x: curPos.pageX,
-                        y: curPos.pageY
-                    };
-                })
-                .map(function(dragEvent) {
-                    camera.center.x -= dragEvent.deltaX * camera.width ;
-                    camera.center.y -= dragEvent.deltaY * camera.height;
-                    return camera;
-                });
-        });
+        return Rx.Observable.fromEvent($$eventTarget, 'swiping')
+            .merge(Rx.Observable.fromEvent($$eventTarget, 'swipe')
+                .map( function (ev) {ev.preventDefault(); return 0; }))
+
+            .scan(0, function (acc, ev) {
+                var data = {
+                    cam: camera,
+                    oldX: 0,
+                    oldY: 0,
+                    reset: false
+                };
+
+                if (ev === 0) {
+                    data.reset = true;
+                    return data;
+                }
+
+                ev.preventDefault();
+                data.oldX = ev.originalEvent.currentTouch.x;
+                data.oldY = ev.originalEvent.currentTouch.y;
+                if (acc && !acc.reset) {
+                    var dx = (ev.originalEvent.currentTouch.x - acc.oldX) / $$eventTarget.width();
+                    var dy = (ev.originalEvent.currentTouch.y - acc.oldY) / $$eventTarget.height();
+
+                    camera.center.x -= dx * camera.width;
+                    camera.center.y -= dy * camera.height;
+                    data.cam = camera;
+                }
+                return data;
+
+            })
+            .map(function(data) {
+                return data.cam;
+            });
+
+    } else {
+        return $eventTarget.mousedownAsObservable()
+            .flatMapLatest(function(clickPos) {
+                clickPos.preventDefault();
+
+                return $('html').mousemoveAsObservable()
+                    .takeUntil($('html').mouseupAsObservable())
+                    .distinctUntilChanged(function(pos) { return {x: pos.pageX, y: pos.pageY}; })
+                    .scan({x: clickPos.pageX, y: clickPos.pageY, deltaX: 0, deltaY: 0}, function(accPos, curPos) {
+                        // Calculate the distance moved (since last event) for each move event
+                        return {
+                            deltaX: (curPos.pageX - accPos.x) / $eventTarget.width(),
+                            deltaY: (curPos.pageY - accPos.y) / $eventTarget.height(),
+                            x: curPos.pageX,
+                            y: curPos.pageY
+                        };
+                    })
+                    .map(function(dragEvent) {
+                        camera.center.x -= dragEvent.deltaX * camera.width ;
+                        camera.center.y -= dragEvent.deltaY * camera.height;
+                        return camera;
+                    });
+            });
+    }
 }
 
 
@@ -55,24 +111,63 @@ function setupMousemove($eventTarget, renderState, texture) {
 
 
 function setupScroll($eventTarget, camera) {
-    return Rx.Observable.fromEvent($eventTarget[0], 'mousewheel')
-        .map(function(wheelEvent) {
-            wheelEvent.preventDefault();
+    if (touchBased) {
+        var $$eventTarget = $$('.sim-container');
+        return Rx.Observable.fromEvent($$eventTarget, 'pinching')
+            .merge(Rx.Observable.fromEvent($$eventTarget, 'pinch')
+                .map( function (ev) {ev.preventDefault(); return 0; }))
 
-            var aspectRatio = camera.width / camera.height;
-            var scrollY =
-                wheelEvent.wheelDeltaY ||
-                -wheelEvent.deltaY ||
-                (wheelEvent.originalEvent ?
-                    (wheelEvent.originalEvent.wheelDeltaY || -wheelEvent.originalEvent.deltaY)
-                    : 0)
-                | 0; //NaN protection
+            .scan(0, function (acc, ev) {
+                var data = {
+                    cam: camera,
+                    oldDist: -1,
+                    oldWidth: camera.width,
+                    oldHeight: camera.height
+                };
 
-            camera.width -= camera.width * (scrollY / 100.0);
-            camera.height = camera.width / aspectRatio;
+                if (ev === 0) {
+                    return data;
+                }
+                ev.preventDefault();
 
-            return camera;
-        });
+                var curDist = straightLineDist(ev.originalEvent.currentTouch[0], ev.originalEvent.currentTouch[1]);
+                data.oldDist = curDist;
+
+                if (acc && acc.oldDist >= 0) {
+                    var aspectRatio = acc.oldWidth / acc.oldHeight;
+                    var scale = acc.oldDist / curDist;
+
+                    camera.width = acc.oldWidth * scale;
+                    camera.height = camera.width / aspectRatio;
+                    data.cam = camera;
+                }
+                return data;
+
+            })
+            .map(function(data) {
+                return data.cam;
+            });
+
+    } else {
+        return Rx.Observable.fromEvent($eventTarget[0], 'mousewheel')
+            .map(function(wheelEvent) {
+                wheelEvent.preventDefault();
+
+                var aspectRatio = camera.width / camera.height;
+                var scrollY =
+                    wheelEvent.wheelDeltaY ||
+                    -wheelEvent.deltaY ||
+                    (wheelEvent.originalEvent ?
+                        (wheelEvent.originalEvent.wheelDeltaY || -wheelEvent.originalEvent.deltaY)
+                        : 0)
+                    | 0; //NaN protection
+
+                camera.width -= camera.width * (scrollY / 100.0);
+                camera.height = camera.width / aspectRatio;
+
+                return camera;
+            });
+    }
 }
 
 
