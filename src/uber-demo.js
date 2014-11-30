0cfdf3cc7a2dc0818a1c527645a97a626295401c
Leo Meyerovich <leo@graphistry.com>
2014-11-29 21:30:01 -0800

    more lazily create labels

--- src/uber-demo.js
+++ src/uber-demo.js
@@ -95,7 +95,10 @@ function renderLabels($labelCont, renderState) {
 
             var pos = camera.canvasCoords(points[2 * lbl.idx], -points[2 * lbl.idx + 1], 1, cnv, mtx);
 
-            if (pos.x < 0 || pos.y < 0 || pos.x > cnv.clientWidth || pos.y > cnv.clientHeight) {
+            var isOffScreen = pos.x < 0 || pos.y < 0 || pos.x > cnv.clientWidth || pos.y > cnv.clientHeight;
+            var isDecayed = Math.random() > 0.5;
+
+            if (isOffScreen || isDecayed) {
                 //remove
                 inactiveLabels.push(lbl);
                 delete activeLabels[lbl.idx];
@@ -156,7 +159,7 @@ function setupInteractions($eventTarget, renderState) {
 
     var $labelCont = $('<div>').addClass('graph-label-container');
     $eventTarget.append($labelCont);
-    var labels = _.range(1,500).map(function (i) {
+    var labels = _.range(1,10).map(function (i) {
         return genLabel($labelCont, i);
     });
     labels.forEach(function ($lbl, i) {
