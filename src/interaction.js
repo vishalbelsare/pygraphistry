a1ac1a6a85a1f8537907c7c492dbb3642c75d657
thibaudh <thibaud@graphistry.com>
2014-12-12 13:24:28 -0800

    Fixed zoom with mouse by using normalized wheeldelta from jquery-mousewheel. Must npm install to get jquery-mousewheel

--- src/interaction.js
+++ src/interaction.js
@@ -8,6 +8,7 @@ var debug    = require('debug')('StreamGL:interaction');
 var renderer = require('./renderer');
 
 require('rx-jquery');
+require('jquery-mousewheel')($);
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,28 +67,35 @@ function setupMousemove($eventTarget, renderState, texture) {
         });
 }
 
-
 function setupScroll($eventTarget, camera) {
-
-    return Rx.Observable.merge(
-            Rx.Observable.fromEvent($eventTarget[0], 'wheel'), //ffox/chrome
-            Rx.Observable.fromEvent($eventTarget[0], 'mousewheel')) //safari/chrome
+    //var bounds = $('canvas', $eventTarget[0])[0].getBoundingClientRect();
+    return $eventTarget.onAsObservable('mousewheel')
         .do(function (wheelEvent) {
             wheelEvent.preventDefault();
         })
         .sample(1)
         .map(function(wheelEvent) {
-            var aspectRatio = camera.width / camera.height;
-            var scrollY =
-                wheelEvent.wheelDeltaY ||
-                wheelEvent.deltaY ||
-                ((wheelEvent.originalEvent) ?
-                    (wheelEvent.originalEvent.wheelDeltaY || -wheelEvent.originalEvent.deltaY) : 0)
-                | 0; //NaN protection
+            //debug("wheelEvent: %o", wheelEvent);
 
-            camera.width -= camera.width * (scrollY / 100.0);
-            camera.height = camera.width / aspectRatio;
+            var zoomBase = wheelEvent.deltaFactor / 35.0;
+            var zoomFactor = (wheelEvent.deltaY < 0 ? zoomBase : 1.0 / zoomBase) || 1.0;
+            
+            /* FIXME Attemp to implement "follow mouse" zoom.
+             * Camera.center is clearly not what I think it is.
+             * /
+            /*
+            var pos = {
+                x: (wheelEvent.clientX - bounds.left) / (bounds.right - bounds.left),
+                y: -1.0 * (wheelEvent.clientY - bounds.top) / (bounds.bottom  - bounds.top)
+            };
+            var xoffset = pos.x - camera.center.x;
+            var yoffset = pos.y - camera.center.y;
+            camera.center.x += xoffset * (1.0 - zoomFactor);
+            camera.center.y += yoffset * (1.0 - zoomFactor);
+            */
 
+            camera.width = camera.width * zoomFactor;
+            camera.height = camera.height * zoomFactor;
             return camera;
         });
 }
