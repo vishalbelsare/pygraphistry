4f64a112cfab9cfb1a5ae70ff5e63b12c189e99b
Matt Torok <matt@graphistry.com>
2014-12-16 17:19:26 -0800

    Split MONGO_SERVER option into parts (but leave MONGO_SERVER for now)
    Instead of crafting one giant connection string for Mongo in the
    MONGO_SERVER option (which doesn't give us insight into the details of
    the connection, or granularity to change it,) this commit splits it
    into MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTS, and MONGO_DATABASE. A
    complete connection string can then be constructed from these options
    from any code that needs to connect to Mongo.
    
    The DATABASE option is already redundant with the MONGO_SERVER option,
    so that should also be removed in the future.
    
    The old 'MONGO_SERVER' and 'DATABASE' options remain for now, for
    backward compatibility reasons.

--- packages/legacy/config/index.js
+++ packages/legacy/config/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// FIXME: Once we connect to mongo using a URL, don't re-connect to the given database
+// FIXME: Find hostname from the environment, not config file
+// FIXME: We need to grab DATALISTURI using npm path resolution, not as a hard-code UNIX path
+// FIXME: Use 'MONGO_{USERNAME,PASSWORD,URLS,DATABASE}' instead of 'MONGO_SERVER' and 'DATABASE'
+
+
 var _ = require('underscore');
 
 
@@ -22,7 +28,12 @@ function defaults() {
         DATALISTURI: 'node_modules/datasets/all.json',
 
         MONGO_SERVER: 'mongodb://localhost:27017/graphistry-local',
-        DATABASE: 'graphistry-local'
+        DATABASE: 'graphistry-local',
+
+        MONGO_USERNAME: '',
+        MONGO_PASSWORD: '',
+        MONGO_HOSTS: ['localhost:27017'],
+        MONGO_DATABASE: 'graphistry-local'
     };
 }
 
@@ -53,24 +64,35 @@ function commandLine(options) {
  * @return {Object} A new set of options combining existing options with ENVIRONMENT options
  */
 function deployEnv(options) {
+    // Common options for 'staging' and 'production'
+    var serverOptions = {
+        MONGO_USERNAME: 'graphistry',
+        MONGO_PASSWORD: 'graphtheplanet',
+        MONGO_HOSTS: ['lighthouse.2.mongolayer.com:10048', 'lighthouse.3.mongolayer.com:10048']
+    };
+
     var stagingOptions = {
         DATABASE: 'graphistry-staging',
-        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-staging'
+        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-staging',
+
+        MONGO_DATABASE: 'graphistry-staging'
     };
 
     var prodOptions = {
         DATABASE: 'graphistry-prod',
-        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-prod'
+        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-prod',
+
+        MONGO_DATABASE: 'graphistry-prod'
     };
 
     switch(options.ENVIRONMENT) {
         case 'staging':
-            return _.extend({}, options, stagingOptions);
+            return _.extend({}, options, serverOptions, stagingOptions);
             break;
         case 'production':
-            return _.extend({}, options, prodOptions);
+            return _.extend({}, options, serverOptions, prodOptions);
             break;
-        default:
+        default:  // 'local'
             return _.extend({}, options);
     }
 }
