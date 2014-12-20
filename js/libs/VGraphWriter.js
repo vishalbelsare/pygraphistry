5e443b723b429ef4784d56452421919342365d7f
Leo Meyerovich <leo@graphistry.com>
2014-12-19 19:49:34 -0800

    Refactor and bug fixes:   -- refactor to split up massive write function into async steps   -- bug fix: promise resolves when when all uploads done   -- bug fix: read target buffer was specifying length in bytes, not elements (fix on line 39)

--- js/libs/VGraphWriter.js
+++ js/libs/VGraphWriter.js
@@ -25,12 +25,94 @@ pb.loadProtoFile(path.resolve(__dirname, 'graph_vector.proto'), function (err, b
     }
 });
 
+//{<name>-> CLJSBuffer} -> Promise [ protobufvector ]
+function readBuffers(buffers) {
+
+    var vectors = [];
+
+    // Iterate through each buffer
+    var arrs = Object.keys(buffers).map(function(index){
+
+        var buffer = buffers[index];
+
+        // TODO: Set this dynamically based on type?
+        var target = new Float32Array(buffer.size / Float32Array.BYTES_PER_ELEMENT);
+
+        // Read the buffer data into a typed array and push to vectors array
+        return buffer.read(target).then(function(buf) {
+            var vector = new pb_root.VectorGraph.Int32BufferVector();
+            var normalArray = Array.prototype.slice.call(target);
+
+            vector.values = normalArray;
+            vector.name = index;
+            vectors.push(vector);
+
+            // sanity check
+            console.log('sanity checking TODO remove', index, '/', Object.keys(buffers).length);
+            var total = normalArray.reduce(function(a, b) {
+              return a + b;
+            });
+            debug('readBuffer verif', index, total, normalArray.length);
+            return target;
+        })
+    })
+
+    return Q.all(arrs).then(_.constant(vectors));
+
+}
+
+//Promise? graph * Promise? [ ProtobufVector ] -> Promise
+var uploadBuffers = Q.promised(function (graph, vectors) {
+
+    var done = Q.defer();
+
+    try {
+        // Set the data vectors on the VGraph
+        graph.vg.int32_buffer_vectors = vectors;
+
+        // Encode the protobuf
+        var byteBuffer = graph.vg.encode();
+
+        // Gzip and upload to S3
+        zlib.gzip(byteBuffer.toBuffer(), function(err, zipped){
+            if (err) { return done.reject(new Error(err)); }
+
+            var savedName = graph.metadata.name.replace('.serialized','') + ".serialized"
+            debug('saving as ' + savedName);
+            var params = {
+                Bucket: config.BUCKET,
+                Key: savedName,
+                ACL: 'private',
+                Metadata: {
+                    type: graph.metadata.type,
+                    config: JSON.stringify(graph.metadata.config)
+                },
+                Body: zipped,
+                ServerSideEncryption: 'AES256'
+            };
+
+            config.S3.putObject(params, function(err, data) {
+                if (err) { return done.reject(new Error(err)); }
+
+                debug("saved and uploaded " + savedName);
+                Q.resolve();
+            });
+        });
+
+    } catch (e) {
+        done.reject(new Error(e));
+    }
+
+    return done.promise;
+});
+
+
+// Graph -> Promise
 function write(graph) {
     debug('serializing and saving state...')
-    
+
     // Grab the buffers from the simulator
     var buffers = graph.simulator.buffers;
-    var vectors = [];
 
     // Add vertices. It's a flattened array, so compose into tuples.
     var untypedVertices = Array.prototype.slice.call(graph.__pointsHostBuffer);
@@ -47,70 +129,9 @@ function write(graph) {
     }
 
     if (graph.vg) {
-        Q().then(function() {
-            // Iterate through each buffer
-            var arrs = Object.keys(buffers).map(function(index){
-
-                var buffer = buffers[index];
-
-                // TODO: Set this dynamically based on type?
-                var target = new Float32Array(buffer.size);
-                
-                // Read the buffer data into a typed array and push to vectors array
-                return buffer.read(target).then(function(buf) {
-                    var vector = new pb_root.VectorGraph.Int32BufferVector();
-                    var normalArray = Array.prototype.slice.call(target);
-                    
-                    vector.values = normalArray;
-                    vector.name = index;
-                    vectors.push(vector)
-
-                    // sanity check
-                    var total = normalArray.reduce(function(a, b) {
-                      return a + b;
-                    });
-                    console.log(index, total, normalArray.length)
-                    return target;
-                })
-            })
-            return Q.all(arrs);
-         }).then(function(arrs){
-            try {
-                // Set the data vectors on the VGraph
-                graph.vg.int32_buffer_vectors = vectors;
-                
-                // Encode the protobuf
-                var byteBuffer = graph.vg.encode();
-                
-                // Gzip and upload to S3
-                zlib.gzip(byteBuffer.toBuffer(), function(err, zipped){
-                    var savedName = graph.metadata.name.replace('.serialized','') + ".serialized"
-                    console.log('saving as ' + savedName);
-                    var params = {
-                        Bucket: config.BUCKET,
-                        Key: savedName,
-                        ACL: 'private',
-                        Metadata: {
-                            type: graph.metadata.type,
-                            config: JSON.stringify(graph.metadata.config)
-                        },
-                        Body: zipped,
-                        ServerSideEncryption: 'AES256'
-                    };
-        
-                    config.S3.putObject(params, function(err, data) {
-                        if (err) {
-                            debug(err)
-                        } else {
-                            debug("saved and uploaded " + savedName)
-                        }
-                    })            
-                })
-            } catch (e) {
-                console.log(e)
-            }
-        })
-        return;
+        return uploadBuffers(graph, readBuffers(buffers));
+    } else {
+        return Q();
     }
 }
 
