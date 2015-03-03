a5185a7884e943ac6d3026c007c7eee81cf0d4f8
Quinn Johnson <quinn@graphistry.com>
2015-03-02 17:23:34 -0800

    Updated semanticZoom estimation to work better for small datasets

--- src/semanticZoom.js
+++ src/semanticZoom.js
@@ -1,9 +1,25 @@
 'use strict';
 
 function pointZoomScalingFactor(width, height, numPoints) {
-    var pointSizeConstant = 0.15;
+
+    // To zoom in, we use the quadroot of the ratio of
+    // points to area to estimate our zoom level. This also has
+    // to be adjusted by a constant, which depends on the size of the
+    // dataset. We combine constants for small datasets and large datasets
+    // using alpha, which is a measure of how "big" a dataset is.
+
+    var pointSizeConstantBig = 1.0;
+    var pointSizeConstantSmall = 7.5;
+    var numPointsOffset = Math.max(numPoints - 150, 10);
+    var alpha = Math.min(1, Math.log(numPointsOffset)/Math.log(10000000));
+
     var area = width * height;
-    return pointSizeConstant * (Math.sqrt(numPoints) / (Math.sqrt(Math.sqrt(area))));
+    var scalingFactor = Math.sqrt(Math.sqrt(numPoints/area));
+
+    var normalizedScalingFactor =
+        scalingFactor * ((1-alpha)*pointSizeConstantSmall + alpha*pointSizeConstantBig);
+
+    return normalizedScalingFactor;
 }
 
 module.exports = {
