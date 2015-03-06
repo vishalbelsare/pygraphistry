4b485169a73b83f7a3a946801c68aec04f385b87
Leo Meyerovich <leo@graphistry.com>
2015-03-06 12:29:43 -0800

    simple statistics in labeler and fix label autosizing

--- js/labeler.js
+++ js/labeler.js
@@ -1,10 +1,32 @@
 'use strict';
 
+var _ = require('underscore');
+
 function defaultLabels (graph, labels) {
     var offset = graph.simulator.timeSubset.pointsRange.startIdx;
 
     return labels.map(
-        function (idx) { return 'node ' + (offset + idx); });
+        function (rawIdx) {
+            var idx = offset + rawIdx;
+
+            var outDegree = graph.simulator.bufferHostCopies.forwardsEdges.degreesTyped[idx];
+            var inDegree = graph.simulator.bufferHostCopies.backwardsEdges.degreesTyped[idx];
+            var degree = outDegree + inDegree;
+
+            var data = [
+                {'degree': inDegree + ' in + ' + outDegree + ' out'},
+                {'index': idx}
+            ];
+
+            return '<div class="graph-label-container">'
+                + '<span class="graph-label-title">node_' + (offset + idx) + '</span>'
+                + '<div class="graph-label-contents">'
+                + _.map(data, function (row) {
+                    var name = _.keys(row)[0];
+                    return '<span class="graph-label-pair"><span class="graph-label-key">' + name + '</span><span class="graph-label-value">' + row[name] + '</span></span>';
+                }).join(' ')
+                + '</div></div>';
+        });
 }
 
 function presetLabels (graph, labels) {
