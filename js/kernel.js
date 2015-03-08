e9b21d58da09c00ec7f0745369a74b0530600750
Paden Tomasello <pdtomasello@gmail.com>
2015-03-07 20:10:16 -0800

    Disable synchronized kernels.

--- js/kernel.js
+++ js/kernel.js
@@ -21,7 +21,7 @@ var Kernel = function (name, argNames, argTypes, file, clContext) {
     var source = util.getKernelSource(file);
 
     // Set synchronous based on debug value
-    var synchronous = true;
+    var synchronous = false;
     if (process.env.DEBUG && process.env.DEBUG.indexOf('perf') != -1) {
         util.info('Kernel ' + name + ' is synchronous because DEBUG=perf');
         synchronous = true;
