fd8038ad22489c16f38325ffb2a6ad86246981c3
Leo Meyerovich <leo@graphistry.com>
2015-03-21 10:19:16 -0700

    Bugfix: retina hit testing

--- src/picking.js
+++ src/picking.js
@@ -95,6 +95,9 @@ function hitTestN(state, textures, x, y, r) {
     });
 
     var canvas = state.get('gl').canvas;
+    var pixelRatio = state.get('camera').pixelRatio;
+    x = x * pixelRatio;
+    y = y * pixelRatio;
     var maps = [];
 
     _.each(textures, function (texture) {
