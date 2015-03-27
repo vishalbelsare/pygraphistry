706c554176093812e4a8e83f3b2d3ca87e5ee49b
Quinn Johnson <quinn@graphistry.com>
2015-03-27 14:19:14 -0700

    Pulled canvas rendering/interactions out

--- src/graphVizApp/vizApp.js
+++ src/graphVizApp/vizApp.js
@@ -6,232 +6,24 @@ var debug   = require('debug')('graphistry:StreamGL:graphVizApp:vizApp');
 var $       = window.$;
 var Rx      = require('rx');
               require('../rx-jquery-stub');
-var _       = require('underscore');
 
-var interaction     = require('./interaction.js');
 var shortestpaths   = require('./shortestpaths.js');
 var colorpicker     = require('./colorpicker.js');
-var util            = require('./util.js');
-var labels          = require('./labels.js');
 var controls        = require('./controls.js');
+var canvas          = require('./canvas.js');
 
-var renderer        = require('../renderer');
 var poiLib          = require('../poi.js');
-
-
-var DEBOUNCE_TIME = 60;
-
-
-function renderScene(lastRender, renderer, currentState, data) {
-    lastRender.onNext({renderer: renderer, currentState: currentState, data: data});
-}
-
-
-function setupDragHoverInteractions($eventTarget, renderState, bgColor, settingsChanges, poi, labelHover, lastRender, currentlyRendering) {
-    //var currentState = renderState;
-    var stateStream = new Rx.Subject();
-    var latestState = new Rx.ReplaySubject(1);
-    stateStream.subscribe(latestState, util.makeErrorHandler('bad stateStream'));
-    stateStream.onNext(renderState);
-
-    var camera = renderState.get('camera');
-    var canvas = renderState.get('canvas');
-
-    var $marquee = $('#marqueerectangle i.fa');
-
-    //pan/zoom
-    //Observable Event
-    var interactions;
-    if(interaction.isTouchBased) {
-        debug('Detected touch-based device. Setting up touch interaction event handlers.');
-        var eventTarget = $eventTarget[0];
-        interactions = interaction.setupSwipe(eventTarget, camera)
-            .merge(
-                interaction.setupPinch($eventTarget, camera)
-                .filter(function () { return !$marquee.hasClass('toggle-on'); }));
-    } else {
-        debug('Detected mouse-based device. Setting up mouse interaction event handlers.');
-        interactions = interaction.setupDrag($eventTarget, camera)
-            .merge(interaction.setupScroll($eventTarget, canvas, camera));
-
-    }
-    interactions = Rx.Observable.merge(
-        interactions,
-        interaction.setupCenter($('#center'),
-                                renderState.get('hostBuffers').curPoints,
-                                camera),
-        interaction.setupZoomButton($('#zoomin'), camera, 1 / 1.25)
-            .filter(function () { return !$marquee.hasClass('toggle-on'); }),
-        interaction.setupZoomButton($('#zoomout'), camera, 1.25)
-            .filter(function () { return !$marquee.hasClass('toggle-on'); })
-    );
-
-    // Picks objects in priority based on order.
-    var hitMapTextures = ['hitmap'];
-    var latestHighlightedObject = labels.getLatestHighlightedObject($eventTarget, renderState, labelHover, hitMapTextures, poi);
-
-    var $labelCont = $('<div>').addClass('graph-label-container');
-    $eventTarget.append($labelCont);
-    labels.setupLabels($labelCont, latestState, latestHighlightedObject, labelHover, currentlyRendering, poi);
-
-
-    //TODO refactor this is out of place
-    var stateWithColor =
-        bgColor.map(function (rgb) {
-
-            var currentState = renderState;
-
-            var color = [[rgb.r/256, rgb.g/256, rgb.b/256,
-                rgb.a === undefined ? 1 : rgb.a/256]];
-
-            var config = currentState.get('config');
-            var options = config.get('options');
-
-            return currentState.set('config',
-                    config.set('options',
-                        options.set('clearColor', color)));
-        });
-
-    //render scene on pan/zoom (get latest points etc. at that time)
-    //tag render changes & label changes
-    var renderStateUpdates = interactions
-        .flatMapLatest(function (camera) {
-            return Rx.Observable.combineLatest(
-                renderState.get('hostBuffers').curPoints,
-                renderState.get('hostBuffers').pointSizes,
-                stateWithColor,
-                settingsChanges,
-                function (curPoints, pointSizes, renderState, settingsChange) {
-                    return {renderTag: Date.now(),
-                            camera: camera,
-                            curPoints: curPoints,
-                            pointSizes: pointSizes,
-                            settingsChange: settingsChange,
-                            renderState: renderState};
-                });
-        })
-        .flatMapLatest(function (data) {
-            // TODO: pass in dim. Handle Dim.
-            // Temporary hack -- ignore edges.
-            return latestHighlightedObject.map(function (highlightIndices) {
-                return _.extend({labelTag: Date.now(), highlightIndices: highlightIndices}, data);
-            });
-        })
-        .do(function(data) {
-            var currentState = renderer.setCameraIm(data.renderState, data.camera);
-            stateStream.onNext(currentState);
-            renderScene(lastRender, renderer, currentState, data);
-        })
-        .pluck('renderState');
-
-    return renderStateUpdates;
-}
-
-
-function setupRendering(lastRender, currentlyRendering) {
-
-    // Determine if it's a quiet/noisy state
-    var startRendering = lastRender
-        .scan({prev: null, cur: null}, function (acc, v) { return {prev: acc.cur, cur: v}; })
-        .filter(function (pair) {
-            return (!pair.prev || (pair.cur.data.renderTag !== pair.prev.data.renderTag));
-        })
-        .sample(DEBOUNCE_TIME);
-
-    var stopRendering = lastRender
-        .scan({prev: null, cur: null}, function (acc, v) { return {prev: acc.cur, cur: v}; })
-        .filter(function (pair) {
-            return (!pair.prev || (pair.cur.data.renderTag !== pair.prev.data.renderTag));
-        })
-        .debounce(DEBOUNCE_TIME);
-
-    // What to do when starting noisy/rendering state
-    startRendering
-        .do(function () {
-            $('.graph-label-container').css('display', 'none');
-        })
-        .do(function () {
-            currentlyRendering.onNext(true);
-        })
-        .subscribe(_.identity, util.makeErrorHandler('Start Rendering'));
-
-    // What to do when exiting noisy/rendering state
-    stopRendering
-        .filter(function() {
-            // TODO: Pull this from a proper stream in a refactor instead of a global dom object
-            return !$('#simulate .fa').hasClass('toggle-on');
-        })
-        .do(function (pair) {
-            pair.cur.renderer.render(pair.cur.currentState, 'interactionPicking', null,
-                {renderListOverride: ['pointpicking', 'edgepicking', 'pointsampling']});
-        })
-        .do(function () {
-            $('.graph-label-container').css('display', 'block');
-        })
-        .do(function () {
-            currentlyRendering.onNext(false);
-        })
-        .subscribe(_.identity, util.makeErrorHandler('Stop Rendering'));
-
-    //Render gpu items, text on reqAnimFrame
-    //Slower, update the pointpicking sampler (does GPU->CPU transfer)
-    lastRender
-        .bufferWithTime(10)
-        .filter(function (arr) { return arr.length; })
-        .map(function (arr) {
-            var res = arr[arr.length - 1];
-            _.extend(res.data,
-                arr.reduce(
-                    function (acc, v) {
-                        return {
-                            data: {
-                                renderTag: Math.max(v.data.renderTag, acc.data.renderTag),
-                                labelTag: Math.max(v.data.labelTag, acc.data.labelTag)
-                            }
-                        };
-                    },
-                    {data: { renderTag: 0, labelTag: 0}}));
-            return res;
-        })
-        .scan({prev: null, cur: null}, function (acc, v) { return {prev: acc.cur, cur: v}; })
-        .do(function (pair) {
-            var cfg = pair.cur;
-            if (!pair.prev || (cfg.data.renderTag !== pair.prev.data.renderTag)) {
-                cfg.renderer.render(cfg.currentState, 'interactionRender');
-            }
-
-            labels.renderCursor(cfg.currentState, new Float32Array(cfg.data.curPoints.buffer),
-                         cfg.data.highlightIndices, new Uint8Array(cfg.data.pointSizes.buffer));
-        })
-        .pluck('cur')
-        .scan({prev: null, cur: null}, function (acc, v) { return {prev: acc.cur, cur: v}; })
-        .bufferWithTime(80)
-        .filter(function (arr) { return arr.length; })
-        .map(function (arr) {
-            var res = arr[arr.length - 1];
-            _.extend(res.data,
-                arr.reduce(
-                    function (acc, v) {
-                        return {
-                            renderTag: Math.max(v.renderTag, acc.renderTag),
-                            labelTag: Math.max(v.labelTag, acc.labelTag)
-                        };
-                    },
-                    {data: { renderTag: 0, labelTag: 0}}));
-            return res;
-        })
-        .subscribe(_.identity, util.makeErrorHandler('render effect'));
-}
 
 
 // ... -> Observable renderState
 function init(socket, $elt, renderState, vboUpdates, workerParams, urlParams) {
-    var poi = poiLib(socket);
+    debug('Initializing vizApp.');
 
     //////////////////////////////////////////////////////////////////////////
     // App State
     //////////////////////////////////////////////////////////////////////////
 
+    var poi = poiLib(socket);
     // Observable DOM
     var labelHover = new Rx.Subject();
     var lastRender = new Rx.Subject();
@@ -244,9 +36,9 @@ function init(socket, $elt, renderState, vboUpdates, workerParams, urlParams) {
     // Setup
     //////////////////////////////////////////////////////////////////////////
 
-    setupRendering(lastRender, currentlyRendering);
+    canvas.setupRendering(lastRender, currentlyRendering);
     var colors = colorpicker($('#foregroundColor'), $('#backgroundColor'), socket);
-    var renderStateUpdates = setupDragHoverInteractions($elt, renderState, colors.backgroundColor,
+    var renderStateUpdates = canvas.setupDragHoverInteractions($elt, renderState, colors.backgroundColor,
                 settingsChanges, poi, labelHover, lastRender, currentlyRendering);
     shortestpaths($('#shortestpath'), poi, socket);
     controls.init(socket, $elt, renderState, vboUpdates, workerParams, urlParams, settingsChanges);
