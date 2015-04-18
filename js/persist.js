0cba665bda2f171bbadab39f08a256e6db6916d5
Leo Meyerovich <leo@graphistry.com>
2015-04-17 17:54:10 -0700

    bug fix (renderconfig persist) and refactor (persist now multiplle methods and separated out check)

--- js/persist.js
+++ js/persist.js
@@ -5,8 +5,11 @@ var _           = require('underscore');
 
 
 var NAME = 'facebook';
-var SAVE_AT_EACH_STEP = false;
+var SAVE_AT_EACH_STEP = true;
 var CHECK_AT_EACH_SAVE = true;
+
+var basePath = __dirname + '/../assets/viz/' + NAME + '.';
+
 
 //============
 
@@ -16,57 +19,75 @@ var prevHeader = {elements: {}, bufferByteLengths: {}};
 //============
 
 
+function checkWrite (vboPath, raw, buff) {
+    var readback = fs.readFileSync(vboPath);
+    debug('readback', readback.length);
+    for (var j = 0; j < raw.byteLength; j++) {
+        if (buff[j] !== raw[j]) {
+            console.error('bad write', j, buff[j], raw[j]);
+            throw 'exn';
+        }
+    }
+    for (var j = 0; j < raw.byteLength; j++) {
+        if (buff[j] !== readback[j]) {
+            console.error('mismatch', j, buff[j], readback[j]);
+            throw 'exn';
+        }
+    }
+    var read = fs.readFileSync(basePath + 'metadata.json', {encoding: 'utf8'});
+    debug('readback metadata', read);
+}
+
 
 module.exports =
-    !SAVE_AT_EACH_STEP ? _.identity
-    : function (vbos, step) {
-
-        if (step > 3 && Math.random() < 0.95) {
-            return;
+    !SAVE_AT_EACH_STEP ?
+        {
+            maybeSaveConfig: _.identity,
+            maybeSaveVbos: _.identity
         }
+    : {
+        maybeSaveConfig: function (renderConfig) {
 
-        var basePath = __dirname + '/../assets/viz/' + NAME + '.';
-
-        debug('serializing vbo');
-        prevHeader = {
-            elements: _.extend(prevHeader.elements, vbos.elements),
-            bufferByteLengths: _.extend(prevHeader.bufferByteLengths, vbos.bufferByteLengths)
-        };
-        fs.writeFileSync(basePath + 'metadata.json', JSON.stringify(prevHeader));
-        var buffers = vbos.uncompressed;
-        for (var i in buffers) {
-            var vboPath = basePath + i + '.vbo';
-            var raw = buffers[i];
-            var buff = new Buffer(raw.byteLength);
-            var arr = new Uint8Array(raw);
-            for (var j = 0; j < raw.byteLength; j++) {
-                buff[j] = raw[j];
+            if (Math.random() < 0.95) {
+                return;
             }
 
-            fs.writeFileSync(vboPath, buff);
+            debug('saving config', renderConfig);
+            fs.writeFileSync(basePath + 'renderconfig.json', JSON.stringify(renderConfig));
 
-            debug('writing', vboPath, raw.byteLength, buff.length);
+        },
 
-            if (!CHECK_AT_EACH_SAVE) {
-                continue;
+        maybeSaveVbos: function (vbos, step) {
+
+            if (step > 3 && Math.random() < 0.95) {
+                return;
             }
-            var readback = fs.readFileSync(vboPath);
-            debug('readback', readback.length);
-            for (var j = 0; j < raw.byteLength; j++) {
-                if (buff[j] !== raw[j]) {
-                    console.error('bad write', j, buff[j], raw[j]);
-                    throw 'exn';
+
+            debug('serializing vbo');
+            prevHeader = {
+                elements: _.extend(prevHeader.elements, vbos.elements),
+                bufferByteLengths: _.extend(prevHeader.bufferByteLengths, vbos.bufferByteLengths)
+            };
+            fs.writeFileSync(basePath + 'metadata.json', JSON.stringify(prevHeader));
+            var buffers = vbos.uncompressed;
+            for (var i in buffers) {
+                var vboPath = basePath + i + '.vbo';
+                var raw = buffers[i];
+                var buff = new Buffer(raw.byteLength);
+                var arr = new Uint8Array(raw);
+                for (var j = 0; j < raw.byteLength; j++) {
+                    buff[j] = raw[j];
                 }
-            }
-            for (var j = 0; j < raw.byteLength; j++) {
-                if (buff[j] !== readback[j]) {
-                    console.error('mismatch', j, buff[j], readback[j]);
-                    throw 'exn';
-                }
-            }
 
+                fs.writeFileSync(vboPath, buff);
+
+                debug('writing', vboPath, raw.byteLength, buff.length);
+
+                if (!CHECK_AT_EACH_SAVE) {
+                    continue;
+                }
+                checkWrite(vboPath, raw, buff);
+            }
+            debug('wrote/read', prevHeader, _.keys(buffers));
         }
-        var read = fs.readFileSync(basePath + 'metadata.json', {encoding: 'utf8'});
-        debug('wrote/read', JSON.parse(read), _.keys(buffers));
-
     };
