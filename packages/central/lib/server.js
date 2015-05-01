1fbfb92da29910bfb7aa822f9d80875ff435b1c2
Matt Torok <matt@graphistry.com>
2015-05-01 16:08:05 -0700

    Split worker routing logic into own file

--- packages/central/lib/server.js
+++ packages/central/lib/server.js
@@ -1,18 +1,13 @@
+/// <reference path="../typings/node/node.d.ts"/>
 'use strict';
 
-var os          = require('os');
 var fs          = require('fs');
-var path        = require('path')
+var path        = require('path');
 var url         = require('url');
 
-
 var debug       = require('debug')('graphistry:central:server');
-var mongo       = require('mongodb');
-var MongoClient = mongo.MongoClient;
-var assert      = require('assert');
 var Rx          = require('rx');
 var Q           = require('q');
-var _           = require('underscore');
 var config      = require('config')();
 
 debug("Config set to %j", config);
@@ -27,6 +22,8 @@ var request     = require('request');
 
 //for etl setup
 var io = require('socket.io-client');
+
+var router = require('./worker-router.js');
 
 
 app.use(compression());
@@ -60,189 +57,6 @@ var SPLUNK_STATIC_PATH  = path.resolve(require('splunk-viz').staticFilePath(), '
 var HTTP_SERVER_LISTEN_ADDRESS = config.HTTP_LISTEN_ADDRESS;
 var HTTP_SERVER_LISTEN_PORT = config.HTTP_LISTEN_PORT;
 
-// FIXME: Get real viz server IP:port from DB
-var VIZ_SERVER_HOST = get_likely_local_ip();
-var VIZ_SERVER_PORT = config.VIZ_LISTEN_PORT;
-debug("Will route clients to viz server at %s:%d", VIZ_SERVER_HOST, VIZ_SERVER_PORT);
-
-
-/**
- * Uses a naive heuristic to find this machines IP address
- * @return {string} the IP address as a string
- */
-function get_likely_local_ip() {
-
-    if (config.ENVIRONMENT === 'local') {
-        return config.VIZ_LISTEN_ADDRESS;
-    }
-
-
-    var public_iface = _.map(os.networkInterfaces(), function(ifaces) {
-        return _.filter(ifaces, function(iface) {
-            return (!iface.internal) && (iface.family === 'IPv4');
-        });
-    });
-
-    public_iface = _.flatten(public_iface, true);
-
-    return (public_iface.length > 0) ? public_iface[0].address : 'localhost';
-}
-
-
-//string -> Observable {ips: ..., results: ...}
-function getIPs() {
-    // The absolute Date that defines the time threshild between fresh/stale pings
-    var freshDate = new Date(Date.now() - (config.GPU_PING_TIMEOUT * 1000));
-
-    // Find all the server running workers, sort by available GPU memory
-    var workerServers = db.collection('gpu_monitor').find(
-            {'updated': {'$gt': freshDate}},
-            {'sort': [['gpu_memory_free', 'desc']]}
-        );
-
-    return Rx.Observable
-        .fromNodeCallback(workerServers.toArray, workerServers)()
-        .flatMap(function (ips) {
-            if (ips.length < 1) {
-                debug('No worker currently registered to this cluster.');
-                return Rx.Observable.throw(new Error(
-                    'There are no worker currently registered to this cluster. Please contact help@graphistry.com for further assistance.'));
-            }
-
-            // Find all idle node processes
-            var nodeCollection = db.collection('node_monitor').find({
-                'active': false,
-                'updated': {'$gt': freshDate}
-            });
-
-            return Rx.Observable.fromNodeCallback(nodeCollection.toArray, nodeCollection)()
-                .map(function (results) {
-                    if (!results.length) {
-                        debug('All workers are currently busy');
-                        var msg = "All workers are currently busy, and your request can't be serviced at this time. Please contact help@graphistry.com for private access. (Reason: could not find an available worker in the worker ping database.)";
-                        throw new Error(msg);
-                    }
-                    return {ips: ips, results: results};
-                });
-        });
-}
-
-
-
-//{hostname, port} -> Observable bool
-//TODO protocol as part of handshake (http vs https)
-function handshakeIp (workerNfo) {
-    var url = 'http://' + workerNfo.hostname + ':' + workerNfo.port + '/claim';
-    var cfg = {url: url, json: true, timeout: 250};
-    debug('Trying worker', cfg, workerNfo);
-    return Rx.Observable.fromNodeCallback(request.get.bind(request))(cfg)
-        .pluck(1)
-        .map(function (resp) {
-            debug('Worker response', resp);
-            return !!resp.success;
-        })
-        .catch(function catchHandshakeHTTPErrors(err) {
-            console.warn('Handshake error: encountered a HTTP error attempting to handshake "%s". Catching error and reporting unsuccessful handshake to caller. Error message: %s',
-                url, err);
-            return Rx.Observable.return(false);
-        });
-}
-
-// ... -> [{hostname,port,timestamp}]
-function listIps (o) {
-
-    var workers = [];
-
-    // Try each IP in order of free space
-    var ips = o.ips;
-    var results = o.results;
-    for (var i in ips) {
-        var ip = ips[i]['ip'];
-
-        for (var j in results) {
-
-            if (results[j]['ip'] != ip) {
-                continue;
-            }
-
-            workers.push(
-                {'hostname': ip,
-                 'port': results[j]['port'],
-                 'timestamp': Date.now()
-                });
-        }
-    }
-
-    return workers;//{i: 0, workers: workers, worker: null};
-}
-
-// resp: JSON {success: bool, error} + {success: true, hostname, port}
-function assign_worker(req, res) {
-    pickWorker(function (err, worker) {
-        if (err) {
-            console.error('Error while assigning visualization worker:', err);
-            return res.json({
-                success: false,
-                error: (err||{}).message || 'Error while assigning visualization worker.'
-            });
-        }
-        debug('Assigning client a worker', req.ip, worker);
-        return res.json(_.extend({success: true}, worker));
-    });
-}
-
-//(exn * {hostname, port} -> ()) -> ()
-function pickWorker (k) {
-
-    var ips;
-
-    if(config.ENVIRONMENT === 'production' || config.ENVIRONMENT === 'staging') {
-        ips = getIPs()
-            .flatMap(function (o) {
-                return Rx.Observable.fromArray(listIps(o));
-            });
-    } else {
-        debug('Using local hostname/port', VIZ_SERVER_HOST, VIZ_SERVER_PORT);
-        ips = Rx.Observable.return({hostname: VIZ_SERVER_HOST, port: VIZ_SERVER_PORT});
-    }
-
-
-    // Create a controlled Observable of IPs so that it only emits an item when we ask it to,
-    // instead of emitting them all at once. (Warning: keep a reference to the Observable right
-    // after `controlled` is applied, as further operators will mask the `request()` method.)
-    var ipsControlled = ips.controlled();
-    // Emit one IP to start
-    ipsControlled.request(1);
-
-    var ip = ipsControlled
-        .flatMap(function (workerNfo) {
-            return handshakeIp(workerNfo)
-                .map(function(success) { return (success) ? workerNfo : false; });
-        })
-        .filter(function filterWorkerAndRequest(workerNfo) {
-            // If we're going to reject this worker, also request the next one
-            return !!(workerNfo || (ipsControlled.request(1) && false));
-        })
-        .take(1);
-
-    var count = 0;
-    ip.do(function (worker) {
-            debug("Assigning worker on %s, port %d", worker.hostname, worker.port);
-            k(null, worker);
-        })
-        .subscribe(
-            function () { count++; },
-            function (err) {
-                console.error('assign_worker error', err, (err || {}).stack);
-                k(err || new Error('Unexpected error while assigning workers.'));
-            },
-            function () {
-                if (!count) {
-                    console.error('assign_worker exhausted search (too many users?)');
-                    k(new Error('Too many users, please contact help@graphistry.com for private access.'));
-                }
-            });
-}
 
 function logClientError(req, res) {
     var writeError = function (msg) {
@@ -280,6 +94,26 @@ function logClientError(req, res) {
     });
 }
 
+
+/**
+ * Handles a `/vizaddr` HTTP request by finding a viz worker server process, reserving it for the
+ * the user, and returning the worker's address to the user so she can connect to it.
+ */
+function assign_worker(req, res) {
+    router.pickWorker(function (err, worker) {
+        if (err) {
+            console.error('Error while assigning visualization worker:', err);
+            return res.json({
+                success: false,
+                error: (err||{}).message || 'Error while assigning visualization worker.'
+            });
+        }
+        debug('Assigning client a worker', req.ip, worker);
+        return res.json(_.extend({success: true}, worker));
+    });
+}
+
+
 app.get('/vizaddr/graph', function(req, res) {
     assign_worker(req, res);
 });
@@ -311,7 +145,7 @@ app.use('/api/v0.2/splunk',   express.static(SPLUNK_STATIC_PATH));
 // Temporarly handle ETL request from Splunk
 app.post('/etl', bodyParser.json({type: '*', limit: '64mb'}), function (req, res) {
     debug('etl request');
-    pickWorker(function (err, worker) {
+    router.pickWorker(function (err, worker) {
         debug('picked etl worker', req.ip, worker);
 
         if (err) {
@@ -363,12 +197,12 @@ app.use('/graphistry', express.static(MAIN_STATIC_PATH));
 
 
 app.get('/horizon', function(req, res) {
-    debug('redirecting to horizon')
+    debug('redirecting to horizon');
     res.redirect('/horizon/src/demo/index.html' + (req.query.debug !== undefined ? '?debug' : ''));
 });
 
 app.get('/uber', function(req, res) {
-    debug('redirecting to graph')
+    debug('redirecting to graph');
     res.redirect('/uber/index.html' + (req.query.debug !== undefined ? '?debug' : ''));
 });
 
@@ -385,17 +219,6 @@ function start() {
                         return url.parse(req.url).path.replace(RegExp('worker/' + config.VIZ_LISTEN_PORT + '/'),'/');
                     }
                 }));
-            }
-        })
-        .flatMap(function () {
-            if(config.ENVIRONMENT === 'local') {
-                return Rx.Observable.return();
-            } else {
-                return Rx.Observable.fromNodeCallback(
-                    MongoClient.connect.bind(MongoClient, config.MONGO_SERVER))({auto_reconnect: true})
-                    .do(function (database) {
-                        db = database.db(config.DATABASE);
-                    });
             }
         })
         .flatMap(function () {
---
+++ packages/central/lib/worker-router.js
@@ -0,0 +1,215 @@
+/// <reference path="../typings/node/node.d.ts"/>
+/// <reference path="../typings/lodash/lodash.d.ts"/>
+/// <reference path="../typings/rx/rx.d.ts"/>
+'use strict';
+
+var os = require('os');
+
+var _ = require('underscore');
+var Rx = require('rx');
+var debug = require('debug')('graphistry:central:worker-router');
+var request = require('request');
+var MongoClient = require('mongodb').MongoClient;
+
+var config = require('config')();
+
+
+var mongoClientConnect = Rx.Observable.fromNodeCallback(MongoClient.connect, MongoClient);
+var dbObs = (config.ENVIRONMENT === 'local') ?
+    (Rx.Observable.return()) :
+    (mongoClientConnect(config.MONGO_SERVER, {auto_reconnect: true})
+        .map(function(database) { return  database.db(config.DATABASE); })
+        .shareReplay(1));
+
+
+/**
+ * Uses a naive heuristic to find this machines IP address
+ * @return {string} the IP address as a string
+ */
+function get_likely_local_ip() {
+
+    if (config.ENVIRONMENT === 'local') {
+        return config.VIZ_LISTEN_ADDRESS;
+    }
+
+
+    var public_iface = _.map(os.networkInterfaces(), function(ifaces) {
+        return _.filter(ifaces, function(iface) {
+            return (!iface.internal) && (iface.family === 'IPv4');
+        });
+    });
+
+    public_iface = _.flatten(public_iface, true);
+
+    return (public_iface.length > 0) ? public_iface[0].address : 'localhost';
+}
+
+
+// FIXME: Get real viz server IP:port from DB
+var VIZ_SERVER_HOST = get_likely_local_ip();
+var VIZ_SERVER_PORT = config.VIZ_LISTEN_PORT;
+
+
+/**
+ * Gets all the currently registered viz worker servers, and all currently registered viz worker
+ * processes.
+ *
+ * @returns {Rx.Observable<servers, workers>} An Observable with a single item, an object with a
+ * `servers` property containing the list of all viz servers, and a `workers` property, containing
+ * the list of all worker process.
+ */
+function getIPs() {
+    // The absolute Date that defines the time threshild between fresh/stale pings
+    var freshDate = new Date(Date.now() - (config.GPU_PING_TIMEOUT * 1000));
+
+    return dbObs.flatMap(function(db) {
+            // Find all the server running workers, sort by available GPU memory
+            var workerServers = db.collection('gpu_monitor').find(
+                    {'updated': {'$gt': freshDate}},
+                    {'sort': [['gpu_memory_free', 'desc']]}
+                );
+
+            return Rx.Observable.fromNodeCallback(workerServers.toArray, workerServers)()
+                .flatMap(function (ips) {
+                    if (ips.length < 1) {
+                        debug('No worker currently registered to this cluster.');
+                        return Rx.Observable.throw(new Error(
+                            'There are no worker currently registered to this cluster. Please contact help@graphistry.com for further assistance.'));
+                    }
+
+                    // Find all idle node processes
+                    var nodeCollection = db.collection('node_monitor').find({
+                        'active': false,
+                        'updated': {'$gt': freshDate}
+                    });
+
+                    return Rx.Observable.fromNodeCallback(nodeCollection.toArray, nodeCollection)()
+                        .map(function (results) {
+                            if (!results.length) {
+                                debug('All workers are currently busy');
+                                var msg = "All workers are currently busy, and your request can't be serviced at this time. Please contact help@graphistry.com for private access. (Reason: could not find an available worker in the worker ping database.)";
+                                throw new Error(msg);
+                            }
+                            return {servers: ips, workers: results};
+                        });
+                });
+        });
+}
+
+
+/**
+ * Queries a worker's to see if it is available. If it's available, the query has the side effect of
+ * reserving the worker for a short period of time, making it unavailable for assignment during this
+ * period.
+ *
+ * @param {Object.<hostname, port>} workerNfo - The worker to query.
+ *
+ * @returns {Rx.Observable<bool>} an Observable with a single item: a bool indicating if this worker
+ * is available (and, consequently, has been reserved for the current user.)
+ */
+function handshakeIp (workerNfo) {
+    var url = 'http://' + workerNfo.hostname + ':' + workerNfo.port + '/claim';
+    var cfg = {url: url, json: true, timeout: 250};
+    debug('Trying worker', cfg, workerNfo);
+    return Rx.Observable.fromNodeCallback(request.get.bind(request))(cfg)
+        .pluck(1)
+        .map(function (resp) {
+            debug('Worker response', resp);
+            return !!resp.success;
+        })
+        .catch(function catchHandshakeHTTPErrors(err) {
+            console.warn('Handshake error: encountered a HTTP error attempting to handshake "%s". Catching error and reporting unsuccessful handshake to caller. Error message: %s',
+                url, err);
+            return Rx.Observable.return(false);
+        });
+}
+
+
+/**
+ * Given a list of servers and workers, combines them to return a list of worker info (hostname,
+ * port, ping timestamp), sorted in order of free space.
+ */
+function combineWorkerInfo (servers, workers) {
+    var workerInfo = [];
+
+    // Try each IP in order of free space
+    for (var i in servers) {
+        var ip = servers[i]['ip'];
+
+        for (var j in workers) {
+            if (workers[j]['ip'] != ip) {
+                continue;
+            }
+
+            workerInfo.push(
+                {'hostname': ip,
+                 'port': workers[j]['port'],
+                 'timestamp': Date.now()
+                });
+        }
+    }
+
+    return workerInfo; //{i: 0, workers: workers, worker: null};
+}
+
+
+/**
+ * Finds an available viz worker process, reserves it temporarily (prevent re-assignignment for a
+ * short period of time), and calls the callback with the address of the assigned worker, or an
+ * error if this process was unsuccessful.
+ */
+function pickWorker (cb) {
+    var ips;
+
+    if(config.ENVIRONMENT === 'production' || config.ENVIRONMENT === 'staging') {
+        ips = getIPs()
+            .flatMap(function (o) {
+                return Rx.Observable.fromArray(combineWorkerInfo(o.servers, o.workers));
+            });
+    } else {
+        debug('Using local hostname/port', VIZ_SERVER_HOST, VIZ_SERVER_PORT);
+        ips = Rx.Observable.return({hostname: VIZ_SERVER_HOST, port: VIZ_SERVER_PORT});
+    }
+
+
+    // Create a controlled Observable of IPs so that it only emits an item when we ask it to,
+    // instead of emitting them all at once. (Warning: keep a reference to the Observable right
+    // after `controlled` is applied, as further operators will mask the `request()` method.)
+    var ipsControlled = ips.controlled();
+    // Emit one IP to start
+    ipsControlled.request(1);
+
+    var ip = ipsControlled
+        .flatMap(function (workerNfo) {
+            return handshakeIp(workerNfo)
+                .map(function(success) { return (success) ? workerNfo : false; });
+        })
+        .filter(function filterWorkerAndRequest(workerNfo) {
+            // If we're going to reject this worker, also request the next one
+            return !!(workerNfo || (ipsControlled.request(1) && false));
+        })
+        .take(1);
+
+    var count = 0;
+    ip.do(function (worker) {
+            debug("Assigning worker on %s, port %d", worker.hostname, worker.port);
+            cb(null, worker);
+        })
+        .subscribe(
+            function () { count++; },
+            function (err) {
+                console.error('assign_worker error', err, (err || {}).stack);
+                cb(err || new Error('Unexpected error while assigning workers.'));
+            },
+            function () {
+                if (!count) {
+                    console.error('assign_worker exhausted search (too many users?)');
+                    cb(new Error('Too many users, please contact help@graphistry.com for private access.'));
+                }
+            });
+}
+
+
+module.exports = {
+    pickWorker: pickWorker
+};
