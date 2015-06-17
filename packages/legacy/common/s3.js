38ddd6ac660227c9bae123964ed3337ec9d07fa2
Brian T. Rice <brian@graphistry.com>
2015-06-17 16:19:59 -0700

    Allow an acl parameter for s3.upload.

--- packages/legacy/common/s3.js
+++ packages/legacy/common/s3.js
@@ -7,22 +7,24 @@ var debug    = require('debug')('graphistry:common:s3');
 
 
 // S3 * String * {name: String, ...} * Buffer -> Promise
-function upload(S3, bucket, metadata, binaryBuffer) {
+function upload(S3, bucket, metadata, binaryBuffer, params) {
     debug('Uploading binary blob', metadata.name);
+
+    var acl = params && params.acl;
 
     return Q.nfcall(zlib.gzip, binaryBuffer)
         .then(function (zipped) {
-            var params = {
+            var putParams = {
                 Bucket: bucket,
                 Key: metadata.name,
-                ACL: 'private',
+                ACL: acl || 'private',
                 Metadata: metadata,
                 Body: zipped,
                 ServerSideEncryption: 'AES256'
             };
 
             debug('Upload size', (zipped.length/1000).toFixed(1), 'KB');
-            return Q.nfcall(S3.putObject.bind(S3), params);
+            return Q.nfcall(S3.putObject.bind(S3), putParams);
         }).then(function () {
             debug('Upload done', metadata.name);
         });
