5337248747f65a62d225bd28450b7e1437437e28
Paden Tomasello <pdtomasello@gmail.com>
2015-07-14 00:46:33 -0700

    Demo!

--- js/layout.config.js
+++ js/layout.config.js
@@ -86,9 +86,9 @@ var uberControls = {
             params: {
                 edgeBundling: new BoolParam('Edge Bundling', false),
                 midpoints: new DiscreteParam('Splits', defaultNumSplits , 0, 32),
-                tau: new ContinuousParam('Speed', 0.5, 0.0000001, 1),
-                charge: new ContinuousParam('Charge', -1, -10, -0.0000001),
-                springStrength: new ContinuousParam('Spring Strength', 200, 0, 400),
+                tau: new ContinuousParam('Speed', 1, 0.0000001, 10),
+                charge: new ContinuousParam('Charge', -0.05, -1, -0.0000000001),
+                springStrength: new ContinuousParam('Spring Strength', 400, 0, 800),
                 springDistance: new ContinuousParam('Spring Distance', 0.5, 0.0000001, 1),
                 // TODO : Remove these
                 gravity: new ContinuousParam('Center Magnet', 1.0, 0.01, 100),
