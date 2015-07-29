d2b69b1f361558177ab8461900dbab131059f02e
Quinn Johnson <quinn@graphistry.com>
2015-07-29 12:09:20 -0700

    Added small commandline utility to clean up bunyan output for debugging

---
+++ packages/legacy/common/oneLineLogs.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict';
+
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+var lastDate;
+rl.on('line', function (line) {
+    try {
+        var log = JSON.parse(line);
+        if (log.msg === undefined || !log.level === undefined) {
+            console.log(line);
+            return;
+        }
+
+        var str = '[' + log.module + '][' + log.level + '] ' + log.msg;
+        var date = new Date(log.time);
+
+        var timeDeltaMs;
+        if (!lastDate) {
+            timeDeltaMs = 0;
+        } else {
+            timeDeltaMs = date - lastDate;
+        }
+        lastDate = date;
+
+        str += '\t+' + timeDeltaMs + 'ms';
+        console.log(str);
+
+    } catch (err) {
+        console.log(line);
+    }
+});
