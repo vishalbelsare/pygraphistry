75dae24210b52589eb526b9a43395e4fd2770303
Leo Meyerovich <leo@graphistry.com>
2015-08-16 22:24:25 -0700

    Portability fix: nested functions disallowed in Safari's strict mode

--- src/graphVizApp/persist.js
+++ src/graphVizApp/persist.js
@@ -18,9 +18,9 @@ function rgb2hex(rgb) {
         if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
 
         rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-        function hex(x) {
+        var hex = function (x) {
             return ("0" + parseInt(x).toString(16)).slice(-2);
-        }
+        };
 
         return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
     } catch (e) {
