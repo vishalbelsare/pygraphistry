7d6caab7a8f272614da049858d34ddf066742afe
Brian T. Rice <brian@graphistry.com>
2015-08-25 15:58:55 -0700

    Fix style warnings in data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -58,14 +58,14 @@ function downloadDataset(query) {
 }
 
 function httpDownloader(http, url) {
-    logger.trace('Attemping to download dataset using HTTP');
+    logger.trace('Attempting to download dataset using HTTP');
     var result = Q.defer();
 
     // Q.denodeify fails http.get because it does not follow
     // the usual nodejs conventions
     http.request(_.extend(url, {method: 'HEAD'}), function (res) {
         var mtime = new Date(res.headers['last-modified']);
-        //Try to read from cache otherwise download the dataset
+        // Try to read from cache otherwise download the dataset
         tmpCache.get(url, mtime).then(function (data) {
             result.resolve(data);
         }).fail(function () {
@@ -87,7 +87,7 @@ function httpDownloader(http, url) {
                 logger.error(err, 'Cannot download dataset at', url.href);
                 result.reject(err);
             });
-        })
+        });
     }).on('error', function (err) {
         logger.error(err, 'Cannot fetch headers from', url.href);
         result.reject(err);
@@ -103,8 +103,8 @@ function httpDownloader(http, url) {
 function graphistryS3Downloader(url) {
     logger.trace('Attempting to download from S3 ' + url.href);
     var params = {
-      Bucket: config.BUCKET,
-      Key: url.href
+        Bucket: config.BUCKET,
+        Key: url.href
     };
     var res = Q.defer();
 
@@ -145,7 +145,7 @@ function loadDatasetIntoSim(graph, dataset) {
     var loader = loaders[dataset.metadata.type];
 
     // If body is gzipped, decompress transparently
-    if (dataset.body.readUInt16BE(0) === 0x1f8b) { //Do we care about big endian? ARM?
+    if (dataset.body.readUInt16BE(0) === 0x1f8b) { // Do we care about big endian? ARM?
         logger.trace('Dataset body is gzipped, decompressing');
         return Q.denodeify(zlib.gunzip)(dataset.body).then(function (gunzipped) {
             dataset.body = gunzipped;
@@ -162,7 +162,7 @@ function createPoints(amount, dim) {
     // Allocate 2 elements for each point (x, y)
     var points = [];
 
-    for(var i = 0; i < amount; i++) {
+    for (var i = 0; i < amount; i++) {
         points.push([Math.random() * dim[0], Math.random() * dim[1]]);
     }
 
@@ -173,7 +173,7 @@ function createPoints(amount, dim) {
 function createEdges(amount, numNodes) {
     var edges = [];
     // This may create duplicate edges. Oh well, for now.
-    for(var i = 0; i < amount; i++) {
+    for (var i = 0; i < amount; i++) {
         var source = (i % numNodes),
             target = (i + 1) % numNodes;
 
@@ -185,20 +185,20 @@ function createEdges(amount, numNodes) {
 
 
 function loadRandom(graph, dataset) {
-    var cfg = dataset.Metadata.config
+    var cfg = dataset.Metadata.config;
     var points = createPoints(cfg.npoints, cfg.dimensions);
     var edges = createEdges(cfg.nedges, cfg.npoints);
 
     return graph.setPoints(points).then(function() {
-        graph.setColorMap("test-colormap2.png");
+        graph.setColorMap('test-colormap2.png');
         return graph.setEdges(edges);
     });
 }
 
 
 function loadRectangle(graph, dataset) {
-    var cfg = dataset.Metadata.config
-    logger.trace("Loading rectangle", cfg.rows, cfg.columns);
+    var cfg = dataset.Metadata.config;
+    logger.trace('Loading rectangle', cfg.rows, cfg.columns);
 
     var points =
         _.flatten(
@@ -218,7 +218,7 @@ function loadRectangle(graph, dataset) {
 
 
 function loadSocioPLT(graph, dataset) {
-    logger.trace("Loading SocioPLT");
+    logger.trace('Loading SocioPLT');
 
     var data = require('./libs/socioplt/generateGraph.js').process(dataset.body);
 
@@ -231,7 +231,7 @@ function loadSocioPLT(graph, dataset) {
     var pointSizes = new Uint8Array(_.pluck(data.nodes, 'size'));
     var pointColors = new Uint32Array(_.pluck(data.nodes, 'color'));
 
-    //data.edges = [{src: 0, dst: 1}];
+    // data.edges = [{src: 0, dst: 1}];
 
     var edges = _.flatten(data.edges.map(function (edge) {
             return [edge.src, edge.dst];
@@ -240,34 +240,34 @@ function loadSocioPLT(graph, dataset) {
             return [edge.color, edge.color];
         }));
 
-        //graph.setVisible({edgeStrength: -10});
-        //physicsControls.gravity     ( 0.020083175556898723);
-        //physicsControls.edgeStrength( 4.292198241799153);
-        //physicsControls.edgeDistance( 0.0000158);
+    // graph.setVisible({edgeStrength: -10});
+    // physicsControls.gravity     ( 0.020083175556898723);
+    // physicsControls.edgeStrength( 4.292198241799153);
+    // physicsControls.edgeDistance( 0.0000158);
 
 
 
     return graph.setPoints(new Float32Array(_.flatten(points)), pointSizes, pointColors)
         .then(function () {
             return graph.setEdgesAndColors(
-                new Uint32Array(edges),//new Uint32Array(_.flatten(edges).map(function (idx, i) { return idx; })),
+                new Uint32Array(edges), // new Uint32Array(_.flatten(edges).map(function (idx, i) { return idx; })),
                 new Uint32Array(edgeColors));
         }).then(function () {
             return graph;
         }, function (err) {
             throw err;
-        })
+        });
 }
 
 
 function loadGeo(graph, dataset) {
-    logger.trace("Loading Geo");
+    logger.trace('Loading Geo');
 
     return Q(MatrixLoader.loadGeo(dataset.body))
      .then(function(geoData) {
         var processedData = MatrixLoader.processGeo(geoData, 0.3);
 
-        logger.debug("Processed %d/%d nodes/edges", processedData.points.length, processedData.edges.length);
+        logger.debug('Processed %d/%d nodes/edges', processedData.points.length, processedData.edges.length);
 
         return graph.setPoints(processedData.points)
             .then(function () {
@@ -275,9 +275,9 @@ function loadGeo(graph, dataset) {
                     return '<b>' + i + '</b><hr/>' + v[0].toFixed(4) + ', ' + v[1].toFixed(4);
                 }));
             })
-            .then(_.constant(processedData))
+            .then(_.constant(processedData));
     })
-    .then(function(processedData) {
+    .then(function (processedData) {
 
         var position = function (points, edges) {
             return edges.map(function (pair){
@@ -286,7 +286,7 @@ function loadGeo(graph, dataset) {
                 return [start[0], start[1], end[0], end[1]];
             });
         };
-        var k = 6; //need to be <= # supported colors, currently 9
+        var k = 6; // need to be <= # supported colors, currently 9
         var steps =  50;
         var positions = position(processedData.points, processedData.edges);
         var clusters = kmeans(positions, k, steps); //[ [0--1]_4 ]_k
@@ -312,7 +312,7 @@ function loadGeo(graph, dataset) {
                 });
     })
     .then(function() {
-        logger.trace("Done setting geo points, edges");
+        logger.trace('Done setting geo points, edges');
         return graph;
     });
 }
@@ -322,11 +322,9 @@ function loadGeo(graph, dataset) {
  * Loads the matrix data at the given URI into the NBody graph.
  */
 function loadMatrix(graph, dataset) {
-    var graphFile;
+    logger.debug('Loading dataset %s', dataset.body);
 
-    logger.debug("Loading dataset %s", dataset.body);
-
-    var v = MatrixLoader.loadBinary(dataset.body)
+    var v = MatrixLoader.loadBinary(dataset.body);
     var graphFile = v;
     if (typeof($) != 'undefined') {
         $('#filenodes').text('Nodes: ' + v.numNodes);
@@ -336,10 +334,10 @@ function loadMatrix(graph, dataset) {
     var points = createPoints(graphFile.numNodes, graph.dimensions);
 
     return graph.setPoints(points)
-    .then(function() {
+    .then(function () {
         return graph.setEdges(graphFile.edges);
     })
-    .then(function() {
+    .then(function () {
         return graph;
     });
 }
