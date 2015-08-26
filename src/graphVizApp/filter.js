d2939593c9c28490e758b6af0c84f4cbce929e4b
Brian T. Rice <brian@graphistry.com>
2015-08-26 08:34:11 -0700

    Filter panel boilerplate Backbone code from histogram.

--- src/graphVizApp/filter.js
+++ src/graphVizApp/filter.js
@@ -4,14 +4,25 @@ var _       = require('underscore');
 var Rx      = require('rx');
 require('../rx-jquery-stub');
 
+var util    = require('./util.js');
+
 
 module.exports = {
     init: function (appState, socket, urlParams, $button /*, $filteringItems */) {
-
         if (!urlParams.debug) {
             $button.css({display: 'none'});
+            return;
         }
 
+        Rx.Observable.fromCallback(socket.emit, socket)('inspect_header', null)
+            .do(function (reply) {
+                if (!reply || !reply.success) {
+                    console.error('Server error on inspectHeader', (reply||{}).error);
+                }
+            }).filter(function (reply) { return reply && reply.success; })
+            .map(function (metadata) {
+
+            }).subscribe(_.identity, util.makeErrorHandler('fetch inspectHeader'));
     },
 
     filterRangeParameters: function (type, attribute, start, stop) {
---
+++ src/graphVizApp/filterPanel.js
@@ -0,0 +1,45 @@
+var $       = window.$;
+var Rx      = require('rx');
+              require('../rx-jquery-stub');
+var _       = require('underscore');
+var Backbone = require('backbone');
+    Backbone.$ = $;
+
+module.exports = {
+    init: function () {
+        var $filterPanel = $('#filteringItems');
+
+        var FilterModel = Backbone.Model.extend({});
+        var FilterCollection = Backbone.Collection.extend({
+            model: FilterModel
+        });
+        var filterSet = new FilterCollection();
+
+        var AllFiltersView = Backbone.View.extend({
+            el: $filterPanel,
+            filtersContainer: $('#filters'),
+            initialize: function () {
+                this.listenTo(filterSet, 'add', this.addFilter);
+                this.listenTo(filterSet, 'remove', this.removeFilter);
+                this.listenTo(filterSet, 'reset', this.clear);
+                this.listenTo(filterSet, 'all', this.render);
+                this.listenTo(filterSet, 'change:timestamp', this.update);
+            },
+            render: function () {
+
+            },
+            addFilter: undefined,
+            removeFilter: undefined,
+            clear: undefined,
+            update: undefined
+        });
+
+        var allFiltersView = new AllFiltersView();
+
+        return {
+            view: allFiltersView,
+            collection: filterSet,
+            model: FilterModel
+        };
+    }
+};
