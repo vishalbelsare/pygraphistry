e3657445dfbb27d0f82c61a08f2bf7c5b94965bb
Paden Tomasello <pdtomasello@gmail.com>
2015-10-08 19:04:46 -0700

    Remove dead code in gpu/forceAtlas2/faSwingsKernel.js

--- js/layouts/gpu/forceAtlas2/faSwingsKernel.js
+++ js/layouts/gpu/forceAtlas2/faSwingsKernel.js
@@ -2,7 +2,6 @@
 var Kernel = require('../../../kernel.js'),
     Q = require('q'),
     _     = require('underscore'),
-    cljs  = require('../../../cl.js'),
     ArgsType = require('../../ArgsType.js');
 
 var log         = require('common/logger.js');
@@ -10,38 +9,17 @@ var logger      = log.createLogger('graph-viz:cl:barnesKernels');
 
 var faSwingKernel = function (clContext) {
 
+    var args = {
+        prevForces : null,
+        curForces : null,
+        swings : null,
+        tractions : null
+    }
 
-    this.argsSwings = ['prevForces', 'curForces', 'swings', 'tractions'];
-
-    this.faSwings = new Kernel('faSwingsTractions', this.argsSwings,
-                               ArgsType, 'layouts/gpu/forceAtlas2/faSwingsTractions.cl', clContext);
-
+    this.faSwings = new Kernel('faSwingsTractions', Object.keys(args), args,
+                               'layouts/gpu/forceAtlas2/faSwingsTractions.cl', clContext);
 
     this.kernels = [this.faSwings];
-
-    this.setPhysics = function (cfg, mask) {
-        _.each(this.kernels, function (k) {
-            k.set(_.pick(cfg, k.argNames));
-        });
-        this.faSwings.set({flags: mask});
-    };
-
-
-
-    this.setEdges = function (simulator) {
-    };
-
-    this.setMidPoints = function(simulator, layoutBuffers) {
-      var buffers = simulator.buffers;
-      this.faSwings.set({
-        prevForces: layoutBuffers.prevForces.buffer,
-        curForces: layoutBuffers.curForces.buffer,
-        swings: layoutBuffers.swings.buffer,
-        tractions: layoutBuffers.tractions.buffer
-      });
-    };
-
-
 
     this.execKernels = function(simulator, workItems) {
 
@@ -66,22 +44,6 @@ var faSwingKernel = function (clContext) {
         return this.faSwings.exec([simulator.dataframe.getNumElements('point')], resources)
             .fail(log.makeQErrorHandler(logger, 'Executing FaSwing failed'));
     };
-
-    this.execMidPointsKernels = function(simulator, workItems) {
-
-      //var resources = [
-          //simulator.buffers.swings,
-          //simulator.buffers.tractions
-      //];
-      var resources = [];
-
-      //simulator.tickBuffers(['swings', 'tractions']);
-      // var numMidpoints = simulator.numMidPoints;
-      var numMidpoints = simulator.dataframe.getNumElements('midPoints');
-      return this.faSwings.exec([numMidpoints], resources)
-        .fail(log.makeQErrorHandler(logger, 'Executing FaSwing failed'));
-    };
-
 }
 
 module.exports = faSwingKernel;
