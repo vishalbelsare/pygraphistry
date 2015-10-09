7c696c4f0b9c748c26120969bba2e06872bf0947
thibaudh <thibaud@graphistry.com>
2015-10-09 11:46:03 -0700

    ETL v2 now uploads to S3. Data format to be discussed.

--- src/etl2.js
+++ src/etl2.js
@@ -3,10 +3,18 @@
 var _        = require('underscore');
 var Q        = require('q');
 var urllib   = require('url');
+var crypto   = require('crypto');
+var sprintf  = require('sprintf-js').sprintf;
+
+var config   = require('config')();
+var s3       = require('common/s3.js');
 var Log      = require('common/logger.js');
 var apiKey   = require('common/api.js');
 var logger   = Log.createLogger('etlworker:etl2');
 
+
+
+var supportedAgents = ['pygraphistry'];
 
 
 function parseParts(parts) {
@@ -22,6 +30,7 @@ function parseParts(parts) {
 
 
 function etl(msg, params) {
+    var folder = params.agent + '/' + crypto.randomBytes(16).toString('hex');
     var desc = {
         agent: params.agent,
         agentVersion: params.agentVersion,
@@ -32,31 +41,68 @@ function etl(msg, params) {
         types: msg.metadata.types,
     };
 
-    desc.datasources = _.map(msg.metadata.datasources, function (datasource) {
+    var qDatasources = _.map(msg.metadata.datasources, function (datasource) {
         var url = datasource.url;
         if (datasource.type == 'vgraph' && urllib.parse(url).protocol === null) {
-            return _.extend({}, datasource, {url: uploadBuffer(msg[url])});
+            var buffer = msg[url];
+            var sha1 = crypto.createHash('sha1').update(buffer).digest('hex');
+            var key = sprintf('%s/%s.vgraph', folder, sha1);
+            return uploadBuffer(buffer, key).then(function (url) {
+                var extra = {
+                    size: buffer.length,
+                    sha1: sha1,
+                    url: url
+                }
+                return _.extend({}, datasource, extra);
+            });
         } else {
-            return datasource;
+            return Q(datasource);
         }
     });
 
-    logger.info('Dataset', desc);
-    return desc;
+    return Q.all(qDatasources).then(function (datasources) {
+        desc.datasources = datasources;
+        logger.info('Dataset', desc);
+        return uploadJSON(desc, sprintf('%s/dataset.json', folder)).then(function (url) {
+            return {name: url, nnodes: '?', nedges: '?'};
+        });
+    });
 }
 
 
-function uploadBuffer(buf) {
-    return 'todo';
+function uploadBuffer(buf, key) {
+    var opts = {
+        ContentType: 'application/octet-stream',
+        ContentEncoding: 'gzip',
+        should_compress: false
+    };
+    return s3.upload(config.S3, config.BUCKET, {name: key}, buf, opts).then(function() {
+        return sprintf('s3://%s/%s', config.BUCKET, key);
+    });
+}
+
+
+function uploadJSON(obj, key) {
+    var opts = {
+        ContentType: 'application/json',
+        ContentEncoding: 'gzip',
+        should_compress: true
+    };
+    return s3.upload(config.S3, config.BUCKET, {name: key}, JSON.stringify(obj), opts).then(function() {
+        return sprintf('s3://%s/%s', config.BUCKET, key);
+    });
 }
 
 
 function process(req, res, params) {
+    if (!_.contains(supportedAgents, params.agent)) {
+        throw new Error('Unsupported agent: ' + params.agent);
+    }
+
+
     var msg = parseParts(req.files);
     logger.info('ETL2: got metadata', msg.metadata);
-    etl(msg, params);
-
-    throw new Error('Not implelemented yet');
+    return etl(msg, params);
 }
 
 
