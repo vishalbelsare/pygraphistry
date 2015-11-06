702d3d492d3efd1c8ee5280467af8b48b76173da
thibaudh <thibaud@graphistry.com>
2015-11-05 17:39:54 -0800

    Pulled api stuff in its own file

--- src/graphVizApp/vizApp.js
+++ src/graphVizApp/vizApp.js
@@ -6,7 +6,6 @@ var debug   = require('debug')('graphistry:StreamGL:graphVizApp:vizApp');
 var $       = window.$;
 var Rx      = require('rx');
               require('../rx-jquery-stub');
-var _       = require('underscore');
 
 var shortestpaths   = require('./shortestpaths.js');
 var colorPicker     = require('./colorpicker.js');
@@ -17,6 +16,7 @@ var ui              = require('../ui.js');
 var poiLib          = require('../poi.js');
 var util            = require('./util.js');
 var highlight       = require('./highlight.js');
+var api             = require('./api.js');
 
 
 function init(socket, initialRenderState, vboUpdates, apiEvents, apiActions,
@@ -155,63 +155,7 @@ function init(socket, initialRenderState, vboUpdates, apiEvents, apiActions,
     }).take(1).do(ui.hideSpinnerShowBody).delay(700);
 
     controls.init(appState, socket, $toolbar, doneLoading, workerParams, urlParams);
-    setupAPIHooks(socket, appState, doneLoading);
-}
-
-
-function encodeEntities(socket, sel) {
-    return Rx.Observable.fromCallback(socket.emit, socket)('get_global_ids', sel)
-        .do(function (reply) {
-            if (!reply || !reply.success) {
-                console.error('Server error on get_global_ids', (reply||{}).error);
-            }
-        }).filter(function (reply) {
-            return reply && reply.success;
-        }).map(function (reply) {
-            return reply.ids;
-        });
-}
-
-
-function setupAPIHooks(socket, appState, doneLoading) {
-    var apiEvents = appState.apiEvents;
-
-    doneLoading.do(function () {
-        apiEvents.onNext({event: 'loaded'});
-    }).subscribe(_.identity, util.makeErrorHandler('API hook for doneLoading'));
-
-    appState.latestHighlightedObject.flatMapLatest(function (sel) {
-        return encodeEntities(socket, sel);
-    }).do(function (ids) {
-        apiEvents.onNext({
-            event: 'highlighted',
-            ids: ids
-        });
-    }).subscribe(_.identity, util.makeErrorHandler('API hook for latestHighlightedObject'));
-
-    appState.activeSelection.flatMapLatest(function (sel) {
-        return encodeEntities(socket, sel);
-    }).do(function (ids) {
-        apiEvents.onNext({
-            event: 'selected',
-            ids: ids
-        });
-    }).subscribe(_.identity, util.makeErrorHandler('API hook for activeSelection'));
-
-    appState.settingsChanges.do(function (setting) {
-        apiEvents.onNext({
-            event: 'settingChanged',
-            setting: setting.name,
-            value: setting.value
-        });
-    }).subscribe(_.identity, util.makeErrorHandler('API hook for settingsChanges'));
-
-    appState.simulateOn.do(function (bool) {
-        apiEvents.onNext({
-            event: 'simulating',
-            value: bool
-        });
-    }).subscribe(_.identity, util.makeErrorHandler('API hook for simulateOn'));
+    api.setupAPIHooks(socket, appState, doneLoading);
 }
 
 
