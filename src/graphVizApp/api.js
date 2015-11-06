2859ad1eb94b5f3af13465291ab3eac5d78014d9
thibaudh <thibaud@graphistry.com>
2015-11-06 07:38:54 -0800

    Added missing file api.js

---
+++ src/graphVizApp/api.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var Rx      = require('rx');
+              require('../rx-jquery-stub');
+var _       = require('underscore');
+
+var util            = require('./util.js');
+
+
+
+function encodeEntities(socket, sel) {
+    return Rx.Observable.fromCallback(socket.emit, socket)('get_global_ids', sel)
+        .do(function (reply) {
+            if (!reply || !reply.success) {
+                console.error('Server error on get_global_ids', (reply||{}).error);
+            }
+        }).filter(function (reply) {
+            return reply && reply.success;
+        }).map(function (reply) {
+            return reply.ids;
+        });
+}
+
+
+function setupAPIHooks(socket, appState, doneLoading) {
+    var apiEvents = appState.apiEvents;
+
+    doneLoading.do(function () {
+        apiEvents.onNext({event: 'loaded'});
+    }).subscribe(_.identity, util.makeErrorHandler('API hook for doneLoading'));
+
+    appState.latestHighlightedObject.flatMapLatest(function (sel) {
+        return encodeEntities(socket, sel);
+    }).do(function (ids) {
+        apiEvents.onNext({
+            event: 'highlighted',
+            ids: ids
+        });
+    }).subscribe(_.identity, util.makeErrorHandler('API hook for latestHighlightedObject'));
+
+    appState.activeSelection.flatMapLatest(function (sel) {
+        return encodeEntities(socket, sel);
+    }).do(function (ids) {
+        apiEvents.onNext({
+            event: 'selected',
+            ids: ids
+        });
+    }).subscribe(_.identity, util.makeErrorHandler('API hook for activeSelection'));
+
+    appState.settingsChanges.do(function (setting) {
+        apiEvents.onNext({
+            event: 'settingChanged',
+            setting: setting.name,
+            value: setting.value
+        });
+    }).subscribe(_.identity, util.makeErrorHandler('API hook for settingsChanges'));
+
+    appState.simulateOn.do(function (bool) {
+        apiEvents.onNext({
+            event: 'simulating',
+            value: bool
+        });
+    }).subscribe(_.identity, util.makeErrorHandler('API hook for simulateOn'));
+}
+
+
+module.exports = {
+    setupAPIHooks: setupAPIHooks
+};
