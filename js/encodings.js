d4fafef8088351d51931278e98cd8fae90e517ef
Brian T. Rice <brian@graphistry.com>
2016-02-02 10:29:52 -0800

    Handle more combinations of binning arrangements with color encodings.

--- js/encodings.js
+++ js/encodings.js
@@ -53,6 +53,7 @@ module.exports = {
                     }
             }
         }
+        var distinctValues = _.keys(summary.distinctValues).sort();
         switch (encodingType) {
             case 'pointSize':
                 // Has to have a magnitude, not negative:
@@ -78,24 +79,30 @@ module.exports = {
                     return {
                         encodingType: encodingType,
                         bufferName: encodingType + 's',
-                        palette: summary.distinctValues,
+                        palette: distinctValues,
                         scaling: _.identity
                     };
                 }
                 if (summary.isCategorical) {
                     if (variation === 'quantitative' && summary.isOrdered) {
                         // User can request a quantitative interpretation of ordered categorical domains.
-                        scaling = undefined;
+                        var domain = defaultDomain;
+                        if (!summary.isNumeric) {
+                            domain = _.range(distinctValues.length);
+                        }
+                        scaling = d3Scale.linear()
+                            .domain(domain)
+                            .range(defaults.color.isQuantitative.sequential.range);
                     } else if (summary.countDistinct <= 10) {
                         scaling = d3Scale.category10()
-                            .domain(summary.distinctValues);
+                            .domain(distinctValues);
                     } else if (summary.countDistinct <= 20) {
                         scaling = d3Scale.category20()
-                            .domain(summary.distinctValues);
+                            .domain(distinctValues);
                     } else {
                         // TODO ensure wraparound
                         scaling = d3Scale.category20()
-                            .domain(summary.distinctValues);
+                            .domain(distinctValues);
                     }
                 }
                 if (scaling === undefined) {
@@ -131,17 +138,26 @@ module.exports = {
                     return {
                         encodingType: encodingType,
                         bufferName: encodingType + 's',
-                        palette: summary.distinctValues,
+                        palette: distinctValues,
                         scaling: _.identity
                     };
                 }
                 if (summary.isCategorical) {
-                    if (summary.countDistinct < 10) {
+                    if (variation === 'quantitative' && summary.isOrdered) {
+                        // User can request a quantitative interpretation of ordered categorical domains.
+                        var domain = defaultDomain;
+                        if (!summary.isNumeric) {
+                            domain = _.range(distinctValues.length);
+                        }
+                        scaling = d3Scale.linear()
+                            .domain(domain)
+                            .range(defaults.color.isQuantitative.sequential.range);
+                    } else if (summary.countDistinct < 10) {
                         scaling = d3Scale.category10()
-                            .domain(summary.distinctValues);
+                            .domain(distinctValues);
                     } else if (summary.countDistinct < 20) {
                         scaling = d3Scale.category20()
-                            .domain(summary.distinctValues);
+                            .domain(distinctValues);
                     }
                 }
                 if (scaling === undefined) {
@@ -170,19 +186,35 @@ module.exports = {
         }
         var scaleAndEncode = scaling;
         var palette;
-        if (scaling && encodingType.substr(encodingType.length - 5) === 'Color') {
+        if (scaling && encodingType.match(/Color$/)) {
             scaleAndEncode = function (x) { return palettes.hexToInt(scaling(x)); };
             if (binning !== undefined && scaling !== undefined) {
                 var minValue = summary.minValue,
-                    step = binning.binWidth;
-                palette = _.map(binning.bins, function (itemCount, index) {
-                    // Use the scaling to get hex string, not machine integer, for D3/color.
-                    if (summary.isQuantitative) {
-                        return scaling(minValue + step * index);
+                    step = binning.binWidth || 0,
+                    binValues = binning.binValues;
+                if (binning.bins && _.size(binning.bins) > 0) {
+                    if (binning.type === 'countBy') {
+                        palette = {};
+                        _.each(_.keys(binning.bins), function (key, index) {
+                            palette[key] = scaling(index);
+                        });
+                    } else if (summary.isNumeric) {
+                        palette = _.map(binning.bins, function (itemCount, index) {
+                            // Use the scaling to get hex string, not machine integer, for D3/color.
+                            return scaling(minValue + step * index);
+                        });
                     } else {
-                        return scaling(index);
+                        palette = _.map(binning.bins, function (itemCount, index) {
+                            var value = binValues !== undefined && binValues[index] ? binValues[index] : index;
+                            return scaling(value);
+                        });
                     }
-                });
+                } else {
+                    palette = new Array(binning.numBins);
+                    for (var i=0; i<binning.numBins; i++) {
+                        palette[i] = scaling(minValue + step * i);
+                    }
+                }
             }
         }
         return {
