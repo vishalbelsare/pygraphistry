f94cf20811bde990e5efaee1e4a13927c537d0cc
Quinn Johnson <quinn@graphistry.com>
2016-02-02 16:30:16 -0800

    Guard time explorer filters on valid time range

--- src/graphVizApp/timeExplorer.js
+++ src/graphVizApp/timeExplorer.js
@@ -965,6 +965,10 @@ function TimeExplorerPanel (socket, $parent, explorer) {
                             var leftBin = that.mainBarView.getBinForPosition(leftX);
                             var rightBin = that.mainBarView.getBinForPosition(rightX);
 
+                            // Guard edges
+                            leftBin = Math.max(leftBin, 0);
+                            rightBin = Math.min(rightBin, cutoffs.length - 2);
+
                             var mainBarData = that.model.get('all');
                             var cutoffs = mainBarData.cutoffs;
 
