ff65a143860846b30a33171982afd56937f38e6e
Brian T. Rice <brian@graphistry.com>
2016-02-23 16:46:27 -0800

    PyGraphistry version recorded.

--- packages/legacy/config/index.js
+++ packages/legacy/config/index.js
@@ -94,7 +94,7 @@ function defaults() {
 
         PYGRAPHISTRY: {
             minVersion: '0.9.0',
-            latestVersion: '0.9.19'
+            latestVersion: '0.9.20'
         },
 
         LOCAL_CACHE: true,
