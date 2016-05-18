8a78cad289285e2ac83f0c1b9c1e3f6b67edbc22
Brian T. Rice <brian@graphistry.com>
2016-05-17 23:49:29 -0700

    Client formatting cleanup.

--- src/graphVizApp/contentFormatter.js
+++ src/graphVizApp/contentFormatter.js
@@ -42,68 +42,34 @@ function castToMoment (value) {
     return momentVal;
 }
 
-function defaultFormat (value, dataType) {
 
-    if (dataType === undefined) {
-        dataType = typeof value;
+function formatDate (value, short = false) {
+    const momentVal = castToMoment(value);
+
+    if (!momentVal.isValid()) {
+        return 'Invalid Date';
     }
 
-    if (dataType === 'boolean') {
-        if (value === true) {
-            return '✓';
-        } else if (value === false) {
-            return '✗';
-        } else {
-            return '☐';
-        }
+    // If user has specified a timezone, use that to format the time.
+    if (displayTimezone) {
+        momentVal.tz(displayTimezone);
     }
 
-    // null guards
-    if (dataType === 'number' && (isNaN(value) || value === 0x7FFFFFFF)) {
-        return null;
-    }
-    if (dataType === 'string' && (value === 'n/a' || value === '\0')) {
-        return null;
-    }
-
-
-    if (dataType === 'date') {
-        var momentVal = castToMoment(value);
-
-        if (!momentVal.isValid()) {
-            return 'Invalid Date';
-        }
-
-        // If user has specified a timezone, use that to format the time.
-        if (displayTimezone) {
-            momentVal.tz(displayTimezone);
-        }
-
-        return momentVal.format('MMM D YYYY, h:mm:ss a z');
-    }
-
-    if (dataType === 'number') {
-        if (value && (value % 1 !== 0)) {
-            return sprintf('%.4f', value);
-        }
-    }
-
-    if (dataType === 'color') {
-        if (!isNaN(value)) {
-            value = d3ColorFromRGBA(value);
-            return value.toString();
-        }
-    }
-
-    return String(value); // Default
+    return momentVal.format(short ? 'MMM D YY, h:mm:ss a' : 'MMM D YYYY, h:mm:ss a z');
 }
 
-function d3ColorFromRGBA(x) {
-    var r = (x >> 16) & 255,
-        g = (x >> 8) & 255,
-        b = x & 255;
-    return d3.rgb(r, g, b);
+
+function formatToString (value, short = false, limit = 10) {
+    const str = String(value);
+    if (short === false) {
+        return str;
+    } else if (str.length > limit) {
+        return str.substr(0, limit - 1) + '…';
+    } else {
+        return str;
+    }
 }
+
 
 /**
  * Calculate significant figures on this as a radix.
@@ -130,68 +96,116 @@ function maybePrecise (v, significantFigures) {
     return printed;
 }
 
-function shortFormat (value, dataType) {
 
-    if (dataType === undefined) {
-        dataType = typeof value;
+function formatNumber (value, short = false, precision = 4) {
+    if (!short) {
+        return sprintf('%.4f', value);
+    }
+    const abs = Math.abs(value);
+    if (abs > 1e12 || (value !== 0 && abs < 1e-5)) {
+        return String(value.toExponential(precision));
+    } else if (abs > 1e9) {
+        return maybePrecise(value / 1e9, precision) + 'B';
+    } else if (abs > 1e6) {
+        return maybePrecise(value / 1e6, precision) + 'M';
+    } else if (abs > 1e3) {
+        return maybePrecise(value / 1e3, precision) + 'K';
+    } else {
+        value = Math.round(value * 1e6) / 1e6; // Kill rounding errors
+        return String(value);
+    }
+}
+
+function d3ColorFromRGBA (x) {
+    const r = (x >> 16) & 255,
+        g = (x >> 8) & 255,
+        b = x & 255;
+    return d3.rgb(r, g, b);
+}
+
+function formatColor (value) {
+    value = d3ColorFromRGBA(value);
+    return value.toString();
+}
+
+function formatBoolean (value) {
+    if (value === true) {
+        return '✓';
+    } else if (value === false) {
+        return '✗';
+    } else {
+        return '☐';
+    }
+}
+
+
+function defaultFormat (value, dataType = typeof value) {
+    // null guards
+    if (value === undefined) {
+        return null;
+    }
+    if (dataType === 'number' && (isNaN(value) || value === 0x7FFFFFFF)) {
+        return null;
+    }
+    if (dataType === 'string' && (value === 'n/a' || value === '\0')) {
+        return null;
     }
 
     if (dataType === 'boolean') {
-        if (value === true) {
-            return '✓';
-        } else if (value === false) {
-            return '✗';
-        } else {
-            return '☐';
-        }
+        return formatBoolean(value);
     }
 
     if (dataType === 'date') {
-        var momentVal = castToMoment(value);
+        return formatDate(value, false);
+    }
 
-        if (!momentVal.isValid()) {
-            return 'Invalid Date';
+    if (dataType === 'number') {
+        if (value && (value % 1 !== 0)) {
+            return formatNumber(value, false);
         }
-
-        // If user has specified a timezone, use that to format the time.
-        if (displayTimezone) {
-            momentVal.tz(displayTimezone);
-        }
-
-        return momentVal.format('MMM D YY, h:mm:ss a');
     }
 
     if (dataType === 'color') {
         if (!isNaN(value)) {
-            value = d3ColorFromRGBA(value);
-            return value.toString();
+            return formatColor(value);
+        }
+    }
+
+    return formatToString(value, false); // Default
+}
+
+
+function shortFormat (value, dataType = typeof value) {
+    // null guards
+    if (value === undefined) {
+        return null;
+    }
+    if (dataType === 'number' && (isNaN(value) || value === 0x7FFFFFFF)) {
+        return null;
+    }
+    if (dataType === 'string' && (value === 'n/a' || value === '\0')) {
+        return null;
+    }
+
+    if (dataType === 'boolean') {
+        return formatBoolean(value);
+    }
+
+    if (dataType === 'date') {
+        return formatDate(value, true);
+    }
+
+    if (dataType === 'color') {
+        if (!isNaN(value)) {
+            value = formatColor(value);
         }
     }
 
     if (isNaN(value)) {
-        var str = String(value);
-        var limit = 10;
-        if (str.length > limit) {
-            return str.substr(0, limit - 1) + '…';
-        } else {
-            return str;
-        }
-    } else {
-        var abs = Math.abs(value);
-        var precision = 4;
-        if (abs > 1e12 || (value !== 0 && abs < 1e-5)) {
-            return String(value.toExponential(precision));
-        } else if (abs > 1e9) {
-            return maybePrecise(value / 1e9, precision) + 'B';
-        } else if (abs > 1e6) {
-            return maybePrecise(value / 1e6, precision) + 'M';
-        } else if (abs > 1e3) {
-            return maybePrecise(value / 1e3, precision) + 'K';
-        } else {
-            value = Math.round(value * 1e6) / 1e6; // Kill rounding errors
-            return String(value);
-        }
+        return formatToString(value, true);
     }
+
+    return formatNumber(value, true);
 }
 
 
