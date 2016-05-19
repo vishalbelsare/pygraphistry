1553d8090d46602af81e3b6babca2f2bc483bec7
thibaudh <thibaud@graphistry.com>
2016-05-19 11:06:06 -0700

    Bumped pygraphistry latest

--- packages/legacy/config/index.js
+++ packages/legacy/config/index.js
@@ -87,7 +87,7 @@ function defaults() {
 
         PYGRAPHISTRY: {
             minVersion: '0.9.0',
-            latestVersion: '0.9.28'
+            latestVersion: '0.9.29'
         },
 
         LOCAL_CACHE: true,
