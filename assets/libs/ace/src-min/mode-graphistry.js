5871de24cc76bbf19b01c53941dc64c129440563
Brian T. Rice <brian@graphistry.com>
2016-05-23 11:09:37 -0700

    Ace mode updates.

--- assets/libs/ace/src-min/mode-graphistry.js
+++ assets/libs/ace/src-min/mode-graphistry.js
@@ -12,7 +12,7 @@ ace.define('ace/mode/graphistry_highlight_rules',
 
             var operatorKeywords = (
                 'between|and|or|not|in|memberof|' +
-                'is|isnull|notnull|' +
+                'is|isnull|notnull|defined|undefined|' +
                 'like|ilike|regexp|similar|to|escape|' +
                 'union|intersect'
             );
@@ -88,7 +88,7 @@ ace.define('ace/mode/graphistry_highlight_rules',
                     next:  'quotedIdentifier'
                 }, {
                     token: 'keyword.operator',
-                    regex: '\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|='
+                    regex: '\\+|\\-|\\/|\\/\\/|%|*|**|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|='
                 }, {
                     token: 'paren.lparen',
                     regex: /[\[({]/,
