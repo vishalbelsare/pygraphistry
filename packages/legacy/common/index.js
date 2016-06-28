0009d7d8003d3cfabb3de8d3e613aa9df916a489
Paul Taylor <paul.e.taylor@me.com>
2016-06-27 22:12:24 -0700

    add slack export

--- packages/legacy/common/index.js
+++ packages/legacy/common/index.js
@@ -2,6 +2,7 @@ module.exports = {
     s3: require('./s3'),
     api: require('./api'),
     cache: require('./cache'),
+    slack: require('./slack'),
     logger: require('./logger'),
     perfStats: require('./perfStats'),
 }
