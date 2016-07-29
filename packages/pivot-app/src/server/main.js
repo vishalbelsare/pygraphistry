0690ef6aa26b2ca8592e1439f22304b01e401720
Paden Tomasello <paden@graphistry.com>
2016-07-28 20:17:20 -0700

    More descriptive placeholders.

--- packages/pivot-app/src/server/main.js
+++ packages/pivot-app/src/server/main.js
@@ -15,47 +15,14 @@ const cols = [
     { name: 'Time'},
 ];
 
-const values = [
-    {
-    'Search': 'FireEye',
-    'Links': 'Really important events',
-    'Time': 'Game Time',
-    'url': 'http://staging.graphistry.com/graph/graph.html?dataset=PyGraphistry/DP3S3MNXTY&type=vgraph&viztoken=a37dd223ad09bf9f238f7b88fea91782cb46d7f9&usertag=45d0e486-pygraphistry-0.9.30&info=true'
-    },
-    {
-    'Search': 'Proxy Logs',
-    'Links': 'Really important events',
-    'Time': 'Game Time',
-    'url': 'http://staging.graphistry.com/graph/graph.html?dataset=PyGraphistry/W54T3R8M66&type=vgraph&viztoken=9b40d934c7c6034bc8a67f5bc9db53d984eaf18a&usertag=45d0e486-pygraphistry-0.9.30&info=true'
-    },
-    {
-    'Search': 'FireWall',
-    'Links': 'Really important events',
-    'Time': 'Game Time',
-    'url': 'http://staging.graphistry.com/graph/graph.html?dataset=PyGraphistry/8BM4JAOY3W&type=vgraph&viztoken=5ef5166761d0b6ec6c49aa725afcbc9440f244b2&usertag=45d0e486-pygraphistry-0.9.30&info=true'
-    },
-    {
-    'Search': 'IDS',
-    'Links': 'Really important events',
-    'Time': 'Game Time',
-    'url': 'http://staging.graphistry.com/graph/graph.html?dataset=PyGraphistry/M7AOEPKJUN&type=vgraph&viztoken=dd66d58fe5fdc4d5fcfc787e636cbcd846f4ae74&usertag=45d0e486-pygraphistry-0.9.30&info=true'
-    },
-    {
-    'Search': 'DHCP',
-    'Links': 'Really important events',
-    'Time': 'Game Time',
-    'url': 'http://staging.graphistry.com/graph/graph.html?dataset=PyGraphistry/ZDTLKUH62K&type=vgraph&viztoken=6cd4aded18ca67ced6fc1bd4be4b3411d9a2d3cd&usertag=45d0e486-pygraphistry-0.9.30&info=true'
-    },
-    {
-    'Search': 'Endpoint Protection',
-    'Links': 'Really important events',
-    'Time': 'Game Time',
-    'url': 'http://staging.graphistry.com/graph/graph.html?dataset=PyGraphistry/SM1AOZ551Z&type=vgraph&viztoken=efbc0f8efa58fb887359515c4a4be839bd97f837&usertag=45d0e486-pygraphistry-0.9.30&info=true'
-    },
-]
+const values = {
+    'Search': 'Input Splunk Query',
+    'Links': 'Connect to Attributes',
+    'Time': '07/28/1016/',
+}
 
 const rows = Array.from({ length: 6 }, (x, index) => (
-    createRow(cols, values[index])
+    createRow(cols, values)
 ));
 
 const app = createApp(cols, rows);
