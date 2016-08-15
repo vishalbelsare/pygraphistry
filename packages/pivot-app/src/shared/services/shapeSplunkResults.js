27b8d3eebe070efeb65ae4d295e0080d14bf7e70
Paden Tomasello <paden@graphistry.com>
2016-08-15 14:27:42 -0700

    Fix multiple search bug

--- packages/pivot-app/src/shared/services/shapeSplunkResults.js
+++ packages/pivot-app/src/shared/services/shapeSplunkResults.js
@@ -7,29 +7,27 @@ export function shapeSplunkResults(splunkResults, pivotDict) {
     var destination = pivotDict['Search'];
     var connections = pivotDict['Links'];
     var nodeLabels = [];
-	return splunkResults.flatMap(
-		(results) => splunkResults
-            .map(function(result) {
-                var edges = [];
-                for(let i = 0; i < result.length; i++) {
-                    var eventId = simpleflake().toJSON();
-                    nodeLabels.push({"node": eventId, type:'eventId'});
-                    edges.push(Object.assign({}, result[i], {'destination': destination, 'source': eventId, edgeType: ('eventId -> Search')}))
-                    nodeLabels.push({"node": destination, type:'Search'});
-                    var connectionsArray = connections.split(',').map((connection) => connection.trim());
-                    for(let j = 0; j < connectionsArray.length; j++) {
-                        var connection = connectionsArray[j];
-                        if (results[i][connection]) {
-                            nodeLabels.push({"node": result[i][connection], type:connection});
-                            edges.push(Object.assign({}, result[i], {'destination': results[i][connection], 'source': eventId, edgeType: ('eventId' + '->' + connection)}))
-                        }
+    return splunkResults
+        .map(function(result) {
+            var edges = [];
+            for(let i = 0; i < result.length; i++) {
+                var eventId = simpleflake().toJSON();
+                nodeLabels.push({"node": eventId, type:'eventId'});
+                edges.push(Object.assign({}, result[i], {'destination': destination, 'source': eventId, edgeType: ('eventId -> Search')}))
+                nodeLabels.push({"node": destination, type:'Search'});
+                var connectionsArray = connections.split(',').map((connection) => connection.trim());
+                for(let j = 0; j < connectionsArray.length; j++) {
+                    var connection = connectionsArray[j];
+                    if (results[i][connection]) {
+                        nodeLabels.push({"node": result[i][connection], type:connection});
+                        edges.push(Object.assign({}, result[i], {'destination': results[i][connection], 'source': eventId, edgeType: ('eventId' + '->' + connection)}))
                     }
                 }
-                
-                return {
-                    graph: edges, 
-                    labels: nodeLabels,
-                };
-            })
-	)
+            }
+
+            return {
+                graph: edges, 
+                labels: nodeLabels,
+            };
+        })
 }
