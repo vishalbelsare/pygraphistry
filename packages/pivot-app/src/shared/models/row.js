24b2f34a994e622fde68d9dbb87ba9f44d1b9d36
Paden Tomasello <paden@graphistry.com>
2016-09-07 20:08:27 -0700

    Reset default result count to 0

--- packages/pivot-app/src/shared/models/row.js
+++ packages/pivot-app/src/shared/models/row.js
@@ -3,7 +3,7 @@ import { ref as $ref } from '@graphistry/falcor-json-graph';
 
 export function row(cols, values = {'Search': 'Enter search query', 'Links': 'None', 'Time': '07/28/2016'}, id = simpleflake().toJSON(), enabled = false) {
     return {
-        resultCount:50,
+        resultCount:0,
         enabled,
         id,
         length: cols.length,
