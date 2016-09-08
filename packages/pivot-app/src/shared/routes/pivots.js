1c8894c8e5819c135e1d112b5ab8fbebe2d174d6
Paden Tomasello <paden@graphistry.com>
2016-09-08 01:28:47 -0700

    Code cleanup: Remove unnecessary route

--- packages/pivot-app/src/shared/routes/pivots.js
+++ packages/pivot-app/src/shared/routes/pivots.js
@@ -30,38 +30,5 @@ export function pivots({ loadPivotsById, calcTotals }) {
         get: getPivotsHandler,
         set: setPivotsHandler,
         route: `pivotsById[{keys}][{integers}]['name', 'value']`
-    },{
-        returns: `String`,
-        call: setColumnValueCallRoute({ loadPivotsById, calcTotals }),
-        route: `pivotsById[{keys}][{integers}].setValue`
     }];
-}
-
-function setColumnValueCallRoute({ loadPivotsById, calcTotals }) {
-    return function setColumnValue(path, args) {
-
-        const [ value ] = args;
-        const pivotIds = [].concat(path[1]);
-        const columnIndexes = [].concat(path[2]);
-
-        return loadPivotsById({ pivotIds })
-            .mergeMap(
-                ({ app, pivot }) => columnIndexes,
-                ({ app, pivot }, index) => ({
-                    app, pivot, index
-                })
-            )
-            .map(({ app, pivot, index }) => {
-                const column = pivot[index];
-                column.value = value;
-                return { app, pivot, index, column };
-            })
-            .mergeMap(({ app, pivot, index, column }) => [
-                $pathValue(`total`, app.total),
-                //$pathValue(`rowsById['${row.id}'].total`, row.total),
-                $pathValue(`pivotsById['${pivot.id}'][${index}].value`, column.value),
-            ])
-            .map(mapObjectsToAtoms)
-            .catch(captureErrorStacks);
-    }
 }
