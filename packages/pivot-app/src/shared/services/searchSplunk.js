2043f822e0f7d78a8c677ce874e8807a31265e2d
Paden Tomasello <paden@graphistry.com>
2016-09-28 17:43:35 -0700

    Style: eslint fixes

--- packages/pivot-app/src/shared/services/searchSplunk.js
+++ packages/pivot-app/src/shared/services/searchSplunk.js
@@ -1,21 +1,23 @@
 var splunkjs = require('splunk-sdk');
 var stringHash = require('string-hash');
 import { Observable } from 'rxjs';
-import { pivotToSearchString, pivotIdToSearchString, expandTemplate }
-  from './support/splunkMacros.js';
 
 var service = new splunkjs.Service({
-  host: process.env.SPLUNK_HOST || 'splunk.graphistry.com',
-  username: process.env.SPLUNK_USER || "admin",
-  password: process.env.SPLUNK_PWD || "graphtheplanet"});
+    host: process.env.SPLUNK_HOST || 'splunk.graphistry.com',
+    username: process.env.SPLUNK_USER || 'admin',
+    password: process.env.SPLUNK_PWD || 'graphtheplanet'
+});
 
-service.login(function(err, success) {
+service.login((err, success) => {
+    if (success) {
+        console.log('Successful login to splunk');
+    }
     if (err) {
         throw err;
     }
 });
 
-export function searchSplunk(searchQuery, callback) {
+export function searchSplunk(searchQuery) {
 
     const searchJobId = `pivot-app::${stringHash(searchQuery)}`;
 
@@ -34,13 +36,13 @@ export function searchSplunk(searchQuery, callback) {
     };
 
     const getJobObservable = Observable.bindNodeCallback(service.getJob.bind(service));
+    const serviceObservable = Observable.bindNodeCallback(service.search.bind(service));
 
     return getJobObservable(searchJobId, splunkNameSpace)
         .catch(
-            (err) => {
+            () => {
                 console.log('No job was found, creating new search job');
-                var serviceObservable = Observable.bindNodeCallback(service.search.bind(service));
-                var serviceResult = serviceObservable(
+                const serviceResult = serviceObservable(
                     searchQuery,
                     searchParams,
                     splunkNameSpace
@@ -50,37 +52,34 @@ export function searchSplunk(searchQuery, callback) {
                         var fetchJob = Observable.bindNodeCallback(job.fetch.bind(job));
                         var jobObservable = fetchJob();
                         return jobObservable;
-                    },
-                    function(job, jobFetchResult) {
-                        return job
                     }
-                )
+                );
             }
         )
         .flatMap(
             function(job) {
-                console.log("Search job properties\n---------------------");
-                console.log("Search job ID:         " + job.sid);
-                console.log("The number of events:  " + job.properties().eventCount);
-                console.log("The number of results: " + job.properties().resultCount);
-                console.log("Search duration:       " + job.properties().runDuration + " seconds");
-                console.log("This job expires in:   " + job.properties().ttl + " seconds");
-                var getResults = Observable.bindNodeCallback(job.results.bind(job),
-                    function(results, job) {
+                console.log('Search job properties\n---------------------');
+                console.log('Search job ID:         ' + job.sid);
+                console.log('The number of events:  ' + job.properties().eventCount);
+                console.log('The number of results: ' + job.properties().resultCount);
+                console.log('Search duration:       ' + job.properties().runDuration + ' seconds');
+                console.log('This job expires in:   ' + job.properties().ttl + ' seconds');
+                const getResults = Observable.bindNodeCallback(job.results.bind(job),
+                    function(results) {
                         return ({results, job});
                     });
-                var jobResults = getResults({count: job.properties().resultCount}).catch(
+                const jobResults = getResults({count: job.properties().resultCount}).catch(
                     (e) => {
                         return Observable.throw(new Error(
                             `${e.data.messages[0].text} ========>  Splunk Query: ${searchQuery}`));
-                        }
-                )
+                    }
+                );
                 return jobResults;
             }
         ).map(
             function({results, job}) {
-                var fields = results.fields;
-                var rows = results.rows;
+                const fields = results.fields;
+                const rows = results.rows;
                 const output = new Array(rows.length);
                 var values;
                 for(var i = 0; i < rows.length; i++) {
@@ -94,6 +93,6 @@ export function searchSplunk(searchQuery, callback) {
                 }
                 return {output, resultCount: job.properties().resultCount, splunkSearchID: job.sid};
             }
-        )
+        );
 }
 
