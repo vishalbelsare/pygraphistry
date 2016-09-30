b3b71abb10f3942b05faf01ce0b98cf4e95e7b13
Paden Tomasello <paden@graphistry.com>
2016-09-30 10:11:10 -0700

    Fix: Dataset template uses input field, but still no events returned

--- packages/pivot-app/src/shared/models/pivotTemplates.js
+++ packages/pivot-app/src/shared/models/pivotTemplates.js
@@ -30,8 +30,8 @@ const SEARCH_SPLUNK_DATASET = {
 
     transport: 'Splunk',
     splunk: {
-        toSplunk: function (pivots, app, fields, pivotCache) {
-            return `search ${fields['Search']} | spath output=dataset path="metadata.dataset" | search dataset="*"  | fields msg, dataset | fields - _* | head 10`
+        toSplunk: function (pivotParameters, pivotCache) {
+            return `search ${pivotParameters['input']} | spath output=dataset path="metadata.dataset" | search dataset="*"  | fields msg, dataset | fields - _* | head 10`
         },
         encodings: {
             point: {
