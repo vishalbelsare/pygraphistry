317690429317fdb316947ff28d7b0cd926e0114c
Leo Meyerovich <leo@graphistry.com>
2016-10-22 23:33:32 -0700

    feature: static labels (wip)

--- src/viz-client/components/labels/labels.js
+++ src/viz-client/components/labels/labels.js
@@ -10,23 +10,133 @@ import {
     shallowEqual
 } from 'recompose';
 
+import _ from 'underscore';
+
+import styles from './style.less';
+
+
+
+const defaultProps = {
+    opacity: React.PropTypes.number,
+    background: React.PropTypes.any,
+    foreground: React.PropTypes.any,
+
+    poiEnabled: React.PropTypes.bool,
+    enabled: React.PropTypes.bool,
+
+    onClick: React.PropTypes.func,
+    onFilter: React.PropTypes.func,
+    onExclude: React.PropTypes.func,
+    onPinChange: React.PropTypes.func,
+
+    hideNull: React.PropTypes.bool,
+    selectedColumns: React.PropTypes.object,
+    labels: React.PropTypes.array
+};
+
+
+
+function LabelTitle (props) {
+    return (
+        <div className={styles['graph-label-title']}>
+            <i className={`
+                ${styles['pin']}
+                ${styles['fa']}
+                ${styles['fa-lg']}
+                ${styles['fa-thumb-tack']}`} />
+            <span className={styles['title']}>{ props.label.title }</span>
+            <a className={styles['exclude-by-title']}>
+                <i className={`${styles['fa']} ${styles['fa-ban']}`} />
+            </a>
+            <span className={styles['label-type']}>{ props.label.type }</span>
+        </div>);
+}
+
+function LabelRow (props) {
+    return (
+        <tr className={styles['graph-label-pair']}>
+            <td className={styles['graph-label-key']}>{props.key}</td>
+            <td className={styles['graph-label-value']}>
+                <div className={styles['graph-label-value-wrapper']}>
+                    {props.value}
+                    <div className={styles['graph-label-icons']}>
+                        <a className={`${styles['tag-by-key-value']} ${styles['beta']}`}>
+                            <i className={`${styles['fa']} ${styles['fa-tag']}`} />
+                        </a>
+                        <a className={styles['exclude-by-key-value']}>
+                            <i className={`${styles['fa']} ${styles['fa-ban']}`} />
+                        </a>
+                        <a className={styles['filter-by-key-value']}>
+                            <i className={`${styles['fa']} ${styles['fa-filter']}`} />
+                        </a>
+                    </div>
+                </div>
+            </td>
+        </tr>);
+}
+
+function LabelContents (props) {
+    return (
+        <div className={styles['graph-label-contents']}>
+            <table>
+                <tbody>{
+                    _.pairs(props.label.fields).map( (key, value) => {
+                        return <LabelRow {...props} key={key} value={value}/>
+                    })
+                }</tbody>
+            </table>
+        </div>);
+}
+
+
+
+class DataLabel extends React.Component {
+
+    render () {
+        return (<div className={`
+            ${styles['graph-label']}
+            ${this.props.label.showFull ? styles['on'] : ''}
+            ${this.props.label.pinned ? styles['clicked'] : ''}`}>
+            <div className={`
+                ${styles['graph-label-container']}
+                ${styles['graph-label-' + this.props.label.type]}`}>
+
+                <LabelTitle { ...this.props } />
+
+                <LabelContents { ...this.props } />
+
+            </div>
+        </div>)
+    }
+
+}
+
 class Labels extends React.Component {
     render() {
+
+        if (!this.props.enabled) return <div className={styles['labels-container']} />;
+
+        console.log('labels props', this.props);
+
         return (
-            <div style={{
-                width: `100%`,
-                height: `100%`,
-                position: `absolute`,
-                background: `transparent`
-            }}>
+            <div className={styles['labels-container']}>
+                {
+                    this.props.labels.map( (label) => (
+                        <DataLabel {...this.props} label={label} /> ))
+                }
             </div>
         );
     }
 }
 
+
+
+Labels.propTypes = defaultProps;
+
 Labels = getContext({
     renderState: PropTypes.object,
     renderingScheduler: PropTypes.object,
 })(Labels);
+
 
 export { Labels };
--- src/viz-shared/containers/view.js
+++ src/viz-shared/containers/view.js
@@ -7,6 +7,8 @@ import { Toolbar } from 'viz-shared/containers/toolbar';
 import { Settings } from 'viz-shared/containers/settings';
 import { selectToolbarItem } from 'viz-shared/actions/toolbar';
 
+import { Labels } from 'viz-client/components/labels'
+
 import {
     sceneMouseMove,
     sceneTouchStart,
@@ -30,6 +32,50 @@ let View = ({
     const isBottomPanelOpen = !!panels.bottom;
     return (
         <div style={{ position: `absolute`, width: `100%`, height: `100%` }}>
+            <Labels
+                opacity={0.9}
+                background="red"
+                foreground="blue"
+
+                poiEnabled={true} //our labels
+                enabled={true} //even if not ours, may want a stub for theirs
+
+                //ideally, icon states would also reflect whether there already is a filter/exclude/encode
+                onClick={ (id, type, obj) => console.log('label click', {id, type, obj}) }
+                onFilter={ (id, type, obj) => console.log('request to create filter', {id, type, obj})}
+                onExclude={ (id, type, obj) => console.log('request to create exclude', {id, type, obj})}
+                onPinChange={ (id, type, obj, status) => console.log('request to change whether pinned', {id, type, obj, status})}
+
+                hideNull={true}
+                selectedColumns={ //null => all
+                  {
+                    'field1': true,
+                    'field2': true,
+                    'field3': true,
+                    'field5': true,
+                    'field10': true
+                  }
+                }
+
+                labels={[
+                  {
+                    type: 'point',
+                    id: 'bullwinkle',
+                    title: "the greatest moose",
+
+                    showFull: true, // expanded when :hover or .on
+                    pinned: true,
+
+                    x: 100,
+                    y: 200,
+
+                    fields: {
+
+
+                    }
+                }
+
+              ]}/>
             <Scene data={scene}
                    mouseMove={sceneMouseMove}
                    touchStart={sceneTouchStart}
