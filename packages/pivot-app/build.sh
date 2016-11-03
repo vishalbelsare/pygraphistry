8d02fb36b5d4e65b39b392acfdd47fdb77bb02fa
thibaudh <thibaud@graphistry.com>
2016-11-03 15:10:19 -0700

    Moved build script out of Jenkins into build.sh

---
+++ packages/pivot-app/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+MAJORMINOR=`jq -r .version package.json | cut -d '.' -f 1,2`
+VERSION=${MAJORMINOR}.${BUILD_NUMBER}
+
+COMMIT_ID=`git rev-parse --short HEAD`
+REV_NAME=`git name-rev --name-only HEAD`
+
+docker build --build-arg BUILD_NUMBER=$BUILD_NUMBER --build-arg COMMIT_ID=$COMMIT_ID --build-arg REV_NAME=$REV_NAME -t graphistry/pivot-app:$VERSION .
+docker push graphistry/pivot-app:$VERSION
