474131ad2b9f58e574e59d291db6b6879ad979c8
thibaudh <thibaud@graphistry.com>
2016-11-04 14:08:25 -0700

    Wrap all routes in defer+atom+catch

--- packages/pivot-app/src/shared/routes/index.js
+++ packages/pivot-app/src/shared/routes/index.js
@@ -1,8 +1,11 @@
+import { Observable } from 'rxjs';
+import { mapObjectsToAtoms } from './support/mapObjectsToAtoms.js';
 import { app } from './app';
 import { pivots } from './pivots';
 import { investigations } from './investigations';
 import { users } from './users';
 import { templates } from './templates';
+
 
 export function routes(services) {
     return ([]
@@ -11,5 +14,30 @@ export function routes(services) {
         .concat(investigations(services))
         .concat(users(services))
         .concat(templates(services))
-    );
+    ).map(wrapRouteHandlers);
+}
+
+function wrapRouteHandlers(route) {
+    const handlers = ['get', 'set', 'call'];
+
+    handlers.forEach(handler => {
+        if (typeof route[handler] === 'function') {
+            route[handler] = wrapRouteHandler(route, handler);
+        }
+    });
+
+    return route;
+}
+
+function wrapRouteHandler(route, handler) {
+    const originalHandler = route[handler];
+
+    return function routeHandlerWrapper(...args) {
+        return Observable
+            .defer(() => originalHandler.apply(this, args) || [])
+            .map(mapObjectsToAtoms)
+            .catch(e => {
+               console.error(route.route, e);
+            });
+    }
 }
