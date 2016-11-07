dd179e8ffdaf7e67288e96c16549999811be6a5b
Leo Meyerovich <leo@graphistry.com>
2016-11-06 16:41:55 -0800

    refactor: inspector search   -- separate out query generator, path generation

--- src/viz-shared/routes/falcor/panels/inspector.js
+++ src/viz-shared/routes/falcor/panels/inspector.js
@@ -39,97 +39,43 @@ export function inspector(path, base) {
             route: `${base}['inspector'].controls[{keys}][{keys}]`
         }, {
             returns: `*`,
-            get: search.bind(null, {loadViewsById, readSelection}),
-            route: `${base}['inspector'].rows[{keys:openTab}][{keys:searchTerm}][{keys:sortKey}][{keys:sortOrder}][{ranges:rows}][{keys:fields}]`
+            get: searchCount.bind(null, {loadViewsById, readSelection}),
+            route: `${base}['inspector'].rows[{keys:openTabs}][{keys:searchTerms}][{keys:sortKeys}][{keys:sortOrders}].count`
+        }, {
+            returns: `*`,
+            get: searchRows.bind(null, {loadViewsById, readSelection}),
+            route: `${base}['inspector'].rows[{keys:openTabs}][{keys:searchTerms}][{keys:sortKeys}][{keys:sortOrders}][{ranges:rows}][{keys:fields}]`
         }];
     }
 }
 
+//====== HELPERS ====
+function queryPage ({readSelection}, {view, openTab, range, searchTerm, sortKey, sortOrder}) {
+    const {from, to} = range;
 
-function search ({loadViewsById, readSelection}, path) {
-
-    //======= DEFS
-
-    const basePath = path.slice(0, path.length - 6);
-
-    const workbookIds = [].concat(path[1]);
-    const viewIds = [].concat(path[3]);
-    const openTabs = [].concat(path[path.length - 6]);
-    const searchTerms = [].concat(path[path.length - 5]);
-    const sortKeys = [].concat(path[path.length - 4]);
-    const sortOrders = [].concat(path[path.length - 3]);
-    const ranges = [].concat(path[path.length - 2]);
-    const fields = [].concat(path[path.length - 1]);
-
-
-    //======= HELPERS
-
-    var queryPage = function ({view, openTab, range, searchTerm, sortKey, sortOrder}) {
-        const {from, to} = range;
-
-        return readSelection({
-            view,
-            type: openTab === 'points' ? 'point' : 'edge',
-            query: {
-                sel: {all: true},
-                page: 1,
-                per_page: to - from + 1,
-                sort_by: !sortKey ? null : sortKey,
-                order: sortOrder,
-                search: searchTerm
-            }
-        })
-    };
-
-    var pageToValue = function (
-            {workbook, view, openTab, searchTerm, sortKey, sortOrder, range, fields},
-            page) {
-
-        //{...range: { ...fields} }
-        const fragment =
-            _.object(
-                page.values.map((v,idx)=> range.from + idx),
-                page.values.map((o) => _.pick(o, fields)));
-
-        //[openTab, `search-`, `sort-`, sortOrder]
-        const midFragment = {
-            [openTab]: {
-                [`search-${searchTerm||''}`]: {
-                    [`sort-${sortKey||''}`]: {
-                        [sortOrder]: fragment } } } };
-
-        //basePath
-        const fullFragment = {
-            workbooksById: {
-                [basePath[1][0]]: {
-                    viewsById: {
-                        [basePath[3][0]]: {
-                            inspector: {
-                                rows: midFragment } } } } } };
-
-        const prefix =
-            ['workbooksById', [basePath[1][0]], 'viewsById', [basePath[3][0]],
-            'inspector', 'rows',
-            [openTab], [`search-${searchTerm||''}`], [`sort-${sortKey||''}`], [sortOrder]];
-
-        const paths = [ prefix.concat([ [range], fields]) ];
-
-        return {paths, jsonGraph: fullFragment };
-    };
-
-    //======= ETL
-
-    return loadViewsById({
-        workbookIds, viewIds
+    return readSelection({
+        view,
+        type: openTab === 'points' ? 'point' : 'edge',
+        query: {
+            sel: {all: true},
+            page: 1,
+            per_page: to - from + 1,
+            sort_by: !sortKey ? null : sortKey,
+            order: sortOrder,
+            search: searchTerm
+        }
     })
-    .mergeMap(({ workbook, view }) => {
+}
 
-        //iterate over query combos to generate/run/format needed searches (hopefully, only 1!)
-        const queries = openTabs.reduce((values, openTab) =>
+function generateQueries(
+        {workbook, view},
+        {openTabs, searchTerms, sortKeys, sortOrders, rows, fields}) {
+
+    return openTabs.reduce((values, openTab) =>
             searchTerms.reduce((values, searchTerm) =>
                 sortKeys.reduce((values, sortKey) =>
                     sortOrders.reduce((values, sortOrder) =>
-                        ranges.reduce((values, range) =>
+                        rows.reduce((values, range) =>
                             values.concat(
                                 [{workbook, view,
                                  openTab,
@@ -141,6 +87,86 @@ function search ({loadViewsById, readSelection}, path) {
                 values),
             values),
         []);
+}
+
+function genPathPrefix (basePath, {openTab, searchTerm, sortKey, sortOrder}) {
+    return [
+        'workbooksById', [basePath[1][0]], 'viewsById', [basePath[3][0]],
+        'inspector', 'rows',
+        [openTab], [`search-${searchTerm||''}`], [`sort-${sortKey||''}`], [sortOrder]
+    ];
+}
+
+//////////// ROW QUERY
+
+
+//TODO also return count path, even though not part of the explicit request
+function pageToRows (
+        {basePath},
+        {workbook, view, openTab, searchTerm, sortKey, sortOrder, range, fields},
+        page) {
+
+    //{...range: { ...fields} }
+    const fragment =
+        _.object(
+            page.values.map((v,idx)=> range.from + idx),
+            page.values.map((o) => _.pick(o, fields)));
+
+    //[openTab, `search-`, `sort-`, sortOrder]
+    const midFragment = {
+        [openTab]: {
+            [`search-${searchTerm||''}`]: {
+                [`sort-${sortKey||''}`]: {
+                    [sortOrder]: fragment } } } };
+
+    //basePath
+    const fullFragment = {
+        workbooksById: {
+            [basePath[1][0]]: {
+                viewsById: {
+                    [basePath[3][0]]: {
+                        inspector: {
+                            rows: midFragment } } } } } };
+
+    const prefix = genPathPrefix(basePath, {openTab, searchTerm, sortKey, sortOrder});
+
+    const paths = [ prefix.concat([ [range], fields]) ];
+
+    return {paths, jsonGraph: fullFragment };
+}
+
+function searchRows ({loadViewsById, readSelection}, path) {
+
+    const basePath = path.slice(0, path.length - 6);
+    const workbookIds = [].concat(path[1]);
+    const viewIds = [].concat(path[3]);
+    const { openTabs, searchTerms, sortKeys, sortOrders, rows, fields } = path;
+
+    return loadViewsById({
+        workbookIds, viewIds
+    })
+    .mergeMap(({ workbook, view }) => {
+
+        const queries = generateQueries(
+            {workbook, view},
+            {openTabs, searchTerms, sortKeys, sortOrders, rows, fields});
+
+        const searches =
+            queries.map((query) =>
+                queryPage({readSelection}, query)
+                    .map(pageToRows.bind(null, {basePath}, query)));
+
+        return Observable.merge(...searches);
+
+    });
+};
+
+
+
+//////////// COUNT QUERY
+
+
+function searchCount ({loadViewsById, readSelection}, path) {
 
         const searches =
             queries.map((query) =>
@@ -150,4 +176,4 @@ function search ({loadViewsById, readSelection}, path) {
         return Observable.merge(...searches);
 
     });
-};
+}
