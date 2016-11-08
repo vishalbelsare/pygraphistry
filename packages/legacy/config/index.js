bb851ba24a183050ab5f77b198e3abb29e2b6a05
Paden Tomasello <paden@graphistry.com>
2016-11-08 12:26:23 -0800

    Update pygraphistry to 0.9.38

--- packages/legacy/config/index.js
+++ packages/legacy/config/index.js
@@ -88,7 +88,7 @@ function defaults() {
 
         PYGRAPHISTRY: {
             minVersion: '0.9.0',
-            latestVersion: '0.9.37'
+            latestVersion: '0.9.38'
         },
 
         RELEASE: 'Nov 16 R1',  // human-readable, shown under logo
