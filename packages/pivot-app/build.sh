b3217ce2d29c003ff8432dbb907cac2894e16a57
thibaudh <thibaud@graphistry.com>
2016-11-08 16:23:46 -0800

    wip build

--- packages/pivot-app/build.sh
+++ packages/pivot-app/build.sh
@@ -2,9 +2,18 @@
 
 MAJORMINOR=`jq -r .version package.json | cut -d '.' -f 1,2`
 VERSION=${MAJORMINOR}.${BUILD_NUMBER}
-
 COMMIT_ID=`git rev-parse --short HEAD`
 REV_NAME=`git name-rev --name-only HEAD`
 
-docker build --build-arg BUILD_NUMBER=$BUILD_NUMBER --build-arg COMMIT_ID=$COMMIT_ID --build-arg REV_NAME=$REV_NAME -t graphistry/pivot-app:$VERSION .
+docker build -f Dockerfile-build \
+       --build-arg BUILD_NUMBER=$BUILD_NUMBER \
+       --build-arg COMMIT_ID=$COMMIT_ID \
+       --build-arg REV_NAME=$REV_NAME \
+       -t graphistry/pivot-app:build \
+       .
+
+docker run --rm graphistry/pivot-app sh -c 'tar --create build node_modules tests package.json' > artifact.tar
+
+docker build -f Dockerfile -t graphistry/pivot-app:$VERSION .
+
 docker push graphistry/pivot-app:$VERSION
