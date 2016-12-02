847628c0bea053295350da1da56201a5f7cad4fe
Leo Meyerovich <leo@graphistry.com>
2016-12-02 15:45:06 -0800

    feature(restyled nav):   -- dropped lbd template dependency   -- refactored screens to reuse nav component   -- refactored screens to use global style vars   -- styled to be closer to mockups   -- added tooltips

--- packages/pivot-app/src/shared/containers/index.js
+++ packages/pivot-app/src/shared/containers/index.js
@@ -1,6 +1,6 @@
 export * from './App';
 export * from './DevTools';
-export * from './Sidebar';
+export * from './Sidebar/Sidebar';
 export * from './Investigation';
 export * from './InvestigationDropdown';
 export * from './PivotRow';
