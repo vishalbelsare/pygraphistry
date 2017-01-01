1d34a863a343afa9ac54ef89e3f355f6ac6021d5
Leo Meyerovich <leo@graphistry.com>
2016-12-31 16:25:31 -0800

    feature(connect to graphistry api on load)   — update rx to 5.0.2 for parity

--- packages/pivot-app/src/client/GraphistryIframe.js
+++ packages/pivot-app/src/client/GraphistryIframe.js
@@ -1,14 +1,29 @@
 import React from 'react';
+
 import styles from './styles.less';
+import GraphistryJS from '@graphistry/graphistry-client';
 
 export class GraphistryIframe extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.iframeRefHandler = this.iframeRefHandler.bind(this);
+    }
+
+    iframeRefHandler (maybeIframe) {        
+        if (maybeIframe) {
+            //mounted
+            const iframe = maybeIframe;
+            GraphistryJS(iframe)
+                .subscribe(() => undefined, console.error.bind(console, 'graphistry-client', 'error'));
+        } else {
+            //unmounted
+        }
     }
 
     render () {
         const { src } = this.props;
         return <iframe
+            ref={this.iframeRefHandler}
             allowFullScreen="true"
             scrolling="no"
             className={styles.iframe}
