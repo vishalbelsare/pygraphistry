fe50b5ccac8bb152100baf439b4dcaa1a58c243c
Paden Tomasello <paden@graphistry.com>
2017-01-25 13:10:25 -0800

    Accordion pivots

--- packages/pivot-app/src/shared/pivots/components/pivot-table.js
+++ packages/pivot-app/src/shared/pivots/components/pivot-table.js
@@ -1,5 +1,6 @@
 import { PivotRow } from 'pivot-shared/pivots';
 import styles from './pivots.less';
+import { Accordion, Panel } from 'react-bootstrap';
 
 export default function PivotTable({
     id, pivots, templates,
@@ -9,21 +10,38 @@ export default function PivotTable({
     
     return (
         <div className={styles['pivot-table']}>
-        {
-                pivots.map((pivot, index) => (
-                    <PivotRow data={pivot}
-                              pivots={pivots}
-                              rowIndex={index}
-                              investigationId={id}
-                              templates={templates}
-                              searchPivot={searchPivot}
-                              splicePivot={splicePivot}
-                              insertPivot={insertPivot}
-                              togglePivots={togglePivots}
-                              key={`${index}: ${pivot.id}`}
-                              />
-                ))
-        }
+            <Accordion>
+                {
+                    pivots.map((pivot, index) => (
+                        <Panel 
+                            header={
+                                <div> 
+                                    <span>
+                                        { `${index}. ` }
+                                    </span>
+                                    <span>
+                                        { pivot.pivotTemplate.name }
+                                    </span>
+                                </div>
+                            } 
+                            key={index} 
+                            eventKey={`Pivot:${index}`}
+                        >
+                            <PivotRow data={pivot}
+                                pivots={pivots}
+                                rowIndex={index}
+                                investigationId={id}
+                                templates={templates}
+                                searchPivot={searchPivot}
+                                splicePivot={splicePivot}
+                                insertPivot={insertPivot}
+                                togglePivots={togglePivots}
+                                key={`${index}: ${pivot.id}`}
+                            />
+                        </Panel>
+                    ))
+                }
+            </Accordion>
         </div>
     );
 }
