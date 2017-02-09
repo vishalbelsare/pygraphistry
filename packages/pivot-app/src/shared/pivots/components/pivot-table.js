1454f23fc8a9d3936f2701ab66d59b261b807127
Paden Tomasello <paden@graphistry.com>
2017-02-08 18:46:04 -0800

    Fixes full page reload on many actions

--- packages/pivot-app/src/shared/pivots/components/pivot-table.js
+++ packages/pivot-app/src/shared/pivots/components/pivot-table.js
@@ -1,83 +1,58 @@
 import { PivotRow, PivotRowHeader } from 'pivot-shared/pivots';
 import styles from './pivots.less';
-import { PanelGroup, Panel, Button } from 'react-bootstrap';
+import { Panel, Button } from 'react-bootstrap';
 
-const PivotPanels = React.createClass({
-    getInitialState() {
-        return {
-            activeKey: '1'
-        };
-    },
+class PivotPanel extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      open: true
+    };
+  }
 
-    handleSelect(activeKey) {
-        if (activeKey === this.state.activeKey) {
-            this.setState({ activeKey: '' });
-        } else {
-            this.setState({ activeKey });
-        }
-    },
-
-    render() {
-        const {
-            pivots,
-            id,
-            templates,
-            togglePivots,
-            insertPivot,
-            searchPivot,
-            splicePivot
-        } = this.props;
-        return (
-            <PanelGroup
-                activeKey={this.state.activeKey}
-                onSelect={this.handleSelect}
-                accordion
-            >
-                {
-                    pivots.map((pivot, index) => (
-                            <Panel eventKey={index}
-                                header={
-                                    <PivotRowHeader
-                                        data={pivot}
-                                        investigationId={id}
-                                        rowIndex={index}
-                                        pivots={pivots}
-                                        templates={templates}
-                                        togglePivots={togglePivots}
-                                    />
-                                }
-                                footer={
-                                    <span className={styles['pivot-footer']}>
-                                        <i style={{float: 'right', width: '100%', textAlign: 'right'}}
-                                            className={`fa fa-fw fa-caret-${this.state.activeKey === index ? 'up' : 'left'}`}
-                                            onClick={((event) => {
-                                                event.preventDefault()
-                                                this.handleSelect(index)
-                                            })}
-                                        />
-                                    </span>
-                                }
-                                key={index}
-                            >
-                                <PivotRow data={pivot}
-                                    pivots={pivots}
-                                    rowIndex={index}
-                                    investigationId={id}
-                                    templates={templates}
-                                    searchPivot={searchPivot}
-                                    splicePivot={splicePivot}
-                                    insertPivot={insertPivot}
-                                    togglePivots={togglePivots}
-                                    handleSelect={this.handleSelect}
-                                    key={`${index}: ${pivot.id}`}
-                                />
-                            </Panel>
-                    ))
-                }
-            </PanelGroup>
-        );
-    }
-});
+  render() {
+      const { pivot, id, index, pivots, templates, togglePivots, searchPivot, 
+          splicePivot, insertPivot } = this.props;
+      return (
+          <Panel 
+              collapsible 
+              expanded={this.state.open}
+              header={
+                  <PivotRowHeader
+                      data={pivot}
+                      investigationId={id}
+                      rowIndex={index}
+                      pivots={pivots}
+                      templates={templates}
+                      togglePivots={togglePivots}
+                  />
+              }
+              footer={
+                  <span className={styles['pivot-footer']}>
+                      <i style={{float: 'right', width: '100%', textAlign: 'right'}}
+                          className={`fa fa-fw fa-caret-${this.state.open ? 'up' : 'left'}`}
+                          onClick={(() => this.setState({open: !this.state.open}))}
+                      />
+                  </span>
+              }
+              key={index}
+          >
+              <PivotRow data={pivot}
+                  pivots={pivots}
+                  rowIndex={index}
+                  investigationId={id}
+                  templates={templates}
+                  searchPivot={searchPivot}
+                  splicePivot={splicePivot}
+                  insertPivot={insertPivot}
+                  togglePivots={togglePivots}
+                  handleSelect={this.handleSelect}
+                  key={`${index}: ${pivot.id}`}
+              />
+          </Panel>
+      );
+  }
+}
 
 export default function PivotTable({
     id, pivots, templates,
@@ -87,15 +62,23 @@ export default function PivotTable({
 
     return (
         <div className={styles['pivot-table']}>
-            <PivotPanels
-                id={id}
-                pivots={pivots}
-                templates={templates}
-                insertPivot={insertPivot}
-                splicePivot={splicePivot}
-                searchPivot={searchPivot}
-                togglePivots={togglePivots}
-            />
+            <div>
+            {
+                pivots.map((pivot, index) => (
+                    <PivotPanel 
+                        id={id}
+                        index={index}
+                        key={index}
+                        pivots={pivots}
+                        pivot={pivot}
+                        templates={templates}
+                        spliacePivot={splicePivot}
+                        searchPivot={searchPivot}
+                        togglePivots={togglePivots}
+                    />
+                ))
+            }
+            </div>
             <Button block
                 onClick={() => insertPivot({index: pivots.length - 1})}
             >{ 'Add new pivot' }</Button>
