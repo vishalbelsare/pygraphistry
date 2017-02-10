4c48a7a03ee24cfb8f457d0dcc3beb09b985a5af
Paden Tomasello <paden@graphistry.com>
2017-02-10 14:07:57 -0800

    Bump pygraphistry version 0.9.41

--- packages/legacy/config/index.js
+++ packages/legacy/config/index.js
@@ -90,7 +90,7 @@ function defaults() {
 
         PYGRAPHISTRY: {
             minVersion: '0.9.0',
-            latestVersion: '0.9.40'
+            latestVersion: '0.9.41'
         },
 
         RELEASE: 'Nov 16 R1',  // human-readable, shown under logo
