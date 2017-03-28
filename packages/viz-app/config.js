5eebd4e653f465a1040e63b8fb70d1df887c0bad
Matt Torok <github@overblown.net>
2017-03-27 19:18:49 -0700

    Move node-convict config schema definition into its own file

--- packages/viz-app/config.js
+++ packages/viz-app/config.js
@@ -7,67 +7,10 @@ const glob = require('glob');
 const convict = require('convict');
 const config = require('@graphistry/config')();
 
+const configSchema = require('./config-schema.js');
+
 // Define a schema
-const conf = convict({
-    env: {
-        doc: 'The applicaton environment.',
-        format: ['production', 'development', 'test'],
-        default: 'development',
-        env: 'NODE_ENV'
-    },
-    host: {
-        doc: 'Viz-app host name/IP',
-        format: 'ipaddress',
-        default: config.VIZ_LISTEN_ADDRESS,
-        env: 'HOST',
-    },
-    port: {
-        doc: 'Viz-app port number',
-        format: 'port',
-        default: config.VIZ_LISTEN_PORT,
-        arg: 'port',
-        env: 'PORT'
-    },
-    log: {
-        level: {
-            doc: `Log levels - ['TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL']`,
-            format: ['TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'],
-            default: 'INFO',
-            arg: 'log-level',
-            env: 'GRAPHISTRY_LOG_LEVEL' // LOG_LEVEL conflicts with mocha
-        },
-        file: {
-            doc: 'Log to a file intead of stdout',
-            format: String,
-            default: undefined,
-            arg: 'log-file',
-            env: 'LOG_FILE'
-        },
-        logSource: {
-            doc: 'Logs line numbers with debug statements. Bad for Perf.',
-            format: Boolean,
-            default: false,
-            arg: 'log-source',
-            env: 'LOG_SOURCE'
-        }
-    },
-    authentication: {
-        username: {
-            doc: 'The username used to access this service',
-            format: String,
-            default: 'admin',
-            arg: 'username',
-            env: 'USERNAME'
-        },
-        passwordHash: {
-            doc: 'Bcrypt hash of the password required to access this service, or unset/empty to disable authentication (default)',
-            format: String,
-            default: '',
-            arg: 'password-hash',
-            env: 'PASSWORD_HASH'
-        }
-    }
-});
+const conf = convict(configSchema);
 
 
 function loadConfigFiles() {
