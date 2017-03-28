26dd9c129a31450e600f0c0d6e0edeb0642e7604
Matt Torok <github@overblown.net>
2017-03-27 18:04:58 -0700

    Check that config files exist before trying to load them (DEV-1250)
    This fixes the bug where a deployment is configured to load the ‘box/deploy override’ config file(s), but those config files haven’t been dropped onto the host system during the current deploy and/or a previous deploy.

--- packages/viz-app/config.js
+++ packages/viz-app/config.js
@@ -1,6 +1,7 @@
 // Warning: only use Node.js natively supported JavaScript features in this file. It is imported
 // and run directly into Node, without Babel complication, by the root `viz-server/index.js`.
 
+const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 const convict = require('convict');
@@ -68,21 +69,40 @@ const conf = convict({
     }
 });
 
-let configFiles = [];
 
-if (process.env.CONFIG_FILES) {
-    configFiles = process.env.CONFIG_FILES.split(',');
-} else {
-    const defaultConfigPath = path.resolve('.', 'config', '*.json');
-    configFiles = glob.sync(defaultConfigPath);
+function loadConfigFiles() {
+    let configFiles = [];
+
+    if (process.env.CONFIG_FILES) {
+        // eslint-disable-next-line no-console
+        console.log(`$CONFIG_FILES environment variable set to "${process.env.CONFIG_FILES}". Will use that value instead of the files in './config/*.json'`);
+        configFiles = process.env.CONFIG_FILES
+            .split(',')
+            .map(function(configFilePath) { return configFilePath.trim() });
+    } else {
+        const defaultConfigPath = path.resolve('.', 'config', '*.json');
+        configFiles = glob.sync(defaultConfigPath);
+    }
+
+    let validConfigFiles = configFiles.filter(function(configFilePath) {
+        if(!fs.existsSync(configFilePath)) {
+            console.error(`Warning: config file "${configFilePath}" does not exist, and will be omitted from config file loading.`);
+            return false;
+        } else {
+            return true;
+        }
+    })
+
+    if (validConfigFiles.length > 0) {
+        // eslint-disable-next-line no-console
+        console.log(`Loading configuration from ${validConfigFiles.join(', ')}`);
+        conf.loadFile(validConfigFiles);
+    }
+
+    return validConfigFiles;
 }
 
-if (configFiles && configFiles.length) {
-    // eslint-disable-next-line no-console
-    console.log(`Loading configuration from ${configFiles.join(', ')}`);
-    conf.loadFile(configFiles);
-}
-
+loadConfigFiles();
 conf.validate({ strict: true });
 
 module.exports = conf;
