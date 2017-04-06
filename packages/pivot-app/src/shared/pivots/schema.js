8041c8a564e41677644fdecfd356f772ee69aff3
Leo Meyerovich <leo@graphistry.com>
2017-04-05 19:03:36 -0700

    wip(load pivot default value from template)

--- packages/pivot-app/src/shared/pivots/schema.js
+++ packages/pivot-app/src/shared/pivots/schema.js
@@ -9,7 +9,7 @@ const log = logger.createLogger(__filename);
 
 export default withSchema((QL, { get, set }, services) => {
 
-    const { loadPivotsById } = services;
+    const { loadPivotsById, loadTemplatesById } = services;
     const readOnlyHandler = {
         get: get(loadPivotsById)
     };
@@ -20,6 +20,56 @@ export default withSchema((QL, { get, set }, services) => {
             service: loadPivotsById
         })
     };
+
+    const paramHandler = {
+        set: readWriteHandler.set,
+        get: function (...args) {
+            return get(loadPivotsById).apply(this, args)
+                .mergeMap(({value,path}, i) => {
+                    if (value === undefined) {                        
+                        const field = path.slice(-1)[0];                        
+                        const template = 
+                            path.slice(0,-2)
+                                .concat(['pivotTemplate', 'pivotParametersUI', field]);
+                        return get(loadPivotsById).apply(this, [template])
+                            //.map({value} => ({path, value}))
+                            .mergeMap((v) => {
+
+                                /*
+                                { original: 
+                                   [ 'pivotsById',
+                                     '3f2fc63c1e995707',
+                                     'pivotParameters',
+                                     'manual-data$$$attributes' ],
+                                  'redirecting to': 
+                                   [ 'pivotsById',
+                                     '3f2fc63c1e995707',
+                                     'pivotTemplate',
+                                     'pivotParametersUI',
+                                     'manual-data$$$attributes' ],
+                                  'got path': [ 'pivotsById', '3f2fc63c1e995707', 'pivotTemplate' ],
+                                  'got value': { '$type': 'ref', value: [ 'templatesById', 'manual-data' ] } }
+                                */
+                                console.log('==== handling unknown param', field, {
+                                    'original': path, 'redirecting to': template,
+                                    'got path': v.path,
+                                    'got value': v.value,
+                                });
+
+                                //----Trying to access the ref differently.. [ [ 'templatesById', 'manual-data' ] ]
+                                console.log('----Trying to access the ref differently..', [v.value.value]);
+                                return get(loadTemplatesById).apply(this, [v.value.value])
+                                    .map((v) => {
+                                        console.log('--- and got: ', v);
+                                    });                         
+                            });
+                    } else {
+                        return Observable.of(value);
+                    }
+                });
+        }
+    };
+    
 
     const searchPivotHandler = {
         call: searchPivotCallRoute(services)
@@ -34,7 +84,7 @@ export default withSchema((QL, { get, set }, services) => {
         },
         pivotParameters: {
             [{ keys }]: ${
-                readWriteHandler
+                paramHandler
             }
         },
         searchPivot: ${
