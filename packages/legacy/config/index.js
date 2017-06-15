5cdc469f26d69988f66edef1fa4a4143dda4cc6c
Leo Meyerovich <leo@graphistry.com>
2017-06-15 00:28:38 -0700

    bump(pygraphistry to 0.9.47)

--- packages/legacy/config/index.js
+++ packages/legacy/config/index.js
@@ -93,7 +93,7 @@ function defaults() {
 
         PYGRAPHISTRY: {
             minVersion: '0.9.0',
-            latestVersion: '0.9.43'
+            latestVersion: '0.9.47'
         },
 
         RELEASE: 'Nov 16 R1',  // human-readable, shown under logo
