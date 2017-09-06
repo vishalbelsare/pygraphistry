bf4ec8a01dd555e75aa59e67dce7be25748f40c7
lsb <leebutterman@gmail.com>
2017-09-05 19:18:18 -0700

    Bring start:dev in line with start:cpu-dev

--- packages/viz-app/tools/npm-start-dev-cpu.sh
+++ packages/viz-app/tools/npm-start-dev-cpu.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker run --rm -it -v ~/.npmrc:/root/.npmrc:ro -v "${PWD}":/app -v "${PWD}/../../.git":/app/.git:ro -v "${PWD}"/data_cache:/tmp/graphistry/data_cache -w /app -p 10000:10000 graphistry/baseapp-cpu sh -c "npm install && echo installed && npm build && echo built && npm run start:cpu-dev"
+if [ $INSTALL ]
+then
+  docker run --rm -v ~/.npmrc:/root/.npmrc:ro -v "${PWD}":/app -v "${PWD}/../../.git":/app/.git:ro -w /app graphistry/baseapp-cpu npm install
+fi
+
+docker run --rm -it -v "${PWD}":/app -v "${PWD}/../../.git":/app/.git:ro -v "${PWD}"/data_cache:/tmp/graphistry/data_cache -w /app -p 10000:10000 graphistry/baseapp-cpu sh -c "npm build && npm run start:cpu-dev"
