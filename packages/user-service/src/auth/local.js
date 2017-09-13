1f3db5d30a77f8876cf06eef75ffb56a4f70c5b6
myk bilokonsky <mbilokonsky@gmail.com>
2017-09-13 13:41:25 -0400

    cleaned up scripts

--- packages/user-service/src/auth/local.js
+++ packages/user-service/src/auth/local.js
@@ -8,6 +8,7 @@ const encodeToken = user => jwt.sign({ data: user.id }, SECRET, { expiresIn: '1h
 const decodeToken = (token, callback) => {
   const payload = jwt.verify(token, process.env.GRAPHISTRY_SECRET);
   const now = moment().unix();
+  
   // check if the token has expired
   if (now > payload.exp) callback('Token has expired.');
   else callback(null, payload);
