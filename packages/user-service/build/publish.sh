b8850c72fbec71fef546c962df8afbbafdcb8f66
myk bilokonsky <mbilokonsky@gmail.com>
2017-09-18 18:43:28 -0400

    pulled publish.sh from correct origin, added Dockerfile

--- packages/user-service/build/publish.sh
+++ packages/user-service/build/publish.sh
@@ -1,14 +1,43 @@
 #!/bin/bash -ex
 
-# silently cd into this shell script's directory
-cd $(dirname "$0") > /dev/null
+BUILD_TAG=graphistry/user-service:build
+MAJORMINOR=`jq -r .version package.json | cut -d '.' -f 1,2`
+VERSION=${MAJORMINOR}.${BUILD_NUMBER}
+ARTIFACTS="build node_modules src migrations seeds knexfile.js package.json"
 
-NODE_ENV=production
+for ARG in "$@"; do
+    case $ARG in
+    --build-tag=*)
+            BUILD_TAG="${ARG#*=}"
+            shift
+            ;;
+    *)
+        echo "Unknown argument $ARG"
+        exit 1
+        ;;
+    esac
+done
 
-./build.sh
 
-docker run --rm \
-    -e NODE_ENV=${NODE_ENV} \
-    ${CONTAINER_NAME}:${BUILD_TAG}
+##########################################
+# Extract artifacts from build container #
+##########################################
 
-echo "publish $CONTAINER_NAME finished"
+docker run --rm ${BUILD_TAG} sh -c "npm prune --production && tar --create ${ARTIFACTS}" > artifact.tar
+
+#######################################
+# Create + publish artifact container #
+#######################################
+
+docker build -f Dockerfile \
+             -t graphistry/user-service:${VERSION} \
+             -t graphistry/user-service:latest \
+             .
+
+docker push graphistry/user-service:${VERSION}
+
+docker push graphistry/user-service:latest
+
+echo "Docker image graphistry/user-service:${VERSION} successfully published."
+
+echo "${VERSION}" > RELEASE
