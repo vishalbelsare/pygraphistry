20dd4b7c0cc4a847ecb0321dcfbbdbc869d342b2
lsb <leebutterman@gmail.com>
2017-09-21 12:14:09 -0700

    Too much CPU concurrency for radial axes, and weird OS X hypervisor fs issues

--- packages/viz-app/tools/npm-start-dev-cpu.sh
+++ packages/viz-app/tools/npm-start-dev-cpu.sh
@@ -4,4 +4,4 @@ then
   docker run --rm -v ~/.npmrc:/root/.npmrc:ro -v "${PWD}":/app -v "${PWD}/../../.git":/app/.git:ro -w /app graphistry/baseapp-cpu npm install
 fi
 
-docker run --rm -it -v "${PWD}":/app -v "${PWD}/../../.git":/app/.git:ro -v "${PWD}"/data_cache:/tmp/graphistry/data_cache -w /app -p 10000:10000 graphistry/baseapp-cpu sh -c "npm run build && npm run start:cpu-dev"
+docker run --rm -it -v "${PWD}":/app -v "${PWD}/../../.git":/app2/.git:ro -v "${PWD}"/data_cache:/tmp/graphistry/data_cache -w /app2 -p 10000:10000 graphistry/baseapp-cpu sh -c "cp -r /app/* /app2 && npm run build && npm run start:cpu-dev"
