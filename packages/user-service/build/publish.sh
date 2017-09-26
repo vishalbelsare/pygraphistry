815e18d578457bd110a31039ad66f39faa9e2c0e
Paul Taylor <paul.e.taylor@me.com>
2017-09-26 15:03:11 -0700

    fixup user-service dockerfile publishes

--- packages/user-service/build/publish.sh
+++ packages/user-service/build/publish.sh
@@ -3,13 +3,19 @@
 # silently cd into this shell script's directory
 cd $(dirname "$0") > /dev/null
 
-VERSION= $(jq -r .version package.json)
-ARTIFACTS="build node_modules src migrations seeds knexfile.js package.json"
+VERSION=$(jq -r .version ../package.json)
+ARTIFACTS="node_modules src migrations seeds knexfile.js package.json"
+
+./build.sh
 
 ##########################################
 # Extract artifacts from build container #
 ##########################################
-docker run --rm ${CONTAINER_NAME}:${BUILD_TAG} sh -c "npm prune --production && tar --create ${ARTIFACTS}" > artifact.tar
+
+docker run --rm -v "$PWD":/artifacts ${CONTAINER_NAME}:${BUILD_TAG} sh -c "
+    rm -rf node_modules package-lock.json &&
+    npm install --production &&
+    tar -cf /artifacts/artifact.tar ${ARTIFACTS}"
 
 #######################################
 # Create + publish artifact container #
@@ -24,6 +30,6 @@ docker push ${CONTAINER_NAME}:${VERSION}
 
 docker push ${CONTAINER_NAME}:latest
 
-echo "Docker image graphistry/${CONTAINER_NAME}:${VERSION} successfully published."
+echo "Docker image ${CONTAINER_NAME}:${VERSION} successfully published."
 
 echo "${VERSION}" > RELEASE
