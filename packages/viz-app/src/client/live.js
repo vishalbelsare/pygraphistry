9ee4e07d8d6f90e4ad000ab949a06fed467b0de4
Paul Taylor <paul.e.taylor@me.com>
2017-09-29 16:07:38 -0700

    update dependencies

--- packages/viz-app/src/client/live.js
+++ packages/viz-app/src/client/live.js
@@ -36,7 +36,6 @@ function congfigureLive(options) {
     const model = new Model({
         recycleJSON: true,
         scheduler: Scheduler.async,
-        comparator: rootComparator,
         allowFromWhenceYouCame: true,
         cache: window.__INITIAL_CACHE__ // eslint-disable-line no-underscore-dangle
     });
@@ -68,41 +67,3 @@ function congfigureLive(options) {
 
 export { congfigureLive };
 export default congfigureLive;
-
-function rootComparator(node, message) {
-    var cTime, cType = node && node.$type;
-    var mTime, mType = message && message.$type;
-    if (cType) {
-        // If the cache has a type, but the message is a primitive,
-        // the message might be the primitive response from the datasource.
-        // If so, return true, so we don't update the back-reference versions.
-        if (!mType) { return node.value === message; }
-        // If their types are different, then isDistinct = true
-        if (cType !== mType) { return false; }
-        // If the message is older than the cache node, then isDistinct = false
-        if ((mTime = message.$timestamp) < (cTime = node.$timestamp)) { return true; }
-        // If the message has a timestamp but the cache doesn't, then isDistinct = true
-        if (mTime !== undefined && cTime === undefined) { return false; }
-        // If $expires is different, then isDistinct = true
-        if (node.$expires !== message.$expires) { return false; }
-        // If they're both refs, compare their paths
-        if (cType === 'ref' && mType === cType) {
-            var nRef = node.value;
-            var nLen = nRef.length;
-            var mRef = message.value;
-            // If their lengths are different, then isDistinct = true
-            if (nLen !== mRef.length) { return false; }
-            while (~--nLen) {
-                // If their paths are different, then isDistinct = true
-                if (nRef[nLen] !== mRef[nLen]) { return false; }
-            }
-            // The refs are equal, so isDistinct = false
-            return true;
-        }
-        // If everything else matches, compare their values
-        return node.value === message.value;
-    }
-    // If cache doesn't have a type but the message does, they must be different.
-    // If neither have a type, they must be primitives, so compare the raw values.
-    return mType ? false : node === message;
-}
