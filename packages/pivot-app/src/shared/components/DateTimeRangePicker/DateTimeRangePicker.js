4203233d828b72f53330d86c2a5c91190e5645c8
myk bilokonsky <mbilokonsky@gmail.com>
2017-10-04 14:49:21 -0400

    Mostly `prettier` changes, but here I’m updating the RangePicker to call the `rangeChanged` handler when it updates. (See line 33 where I add rangeChanged and line 42 where I invoke it)

--- packages/pivot-app/src/shared/components/DateTimeRangePicker/DateTimeRangePicker.js
+++ packages/pivot-app/src/shared/components/DateTimeRangePicker/DateTimeRangePicker.js
@@ -1,23 +1,23 @@
-import moment from 'moment';
-import DateTimePicker from './DateTimePicker';
-import mapPropsStream from 'recompose/mapPropsStream';
-import { $atom } from '@graphistry/falcor-json-graph';
-import createEventHandler from 'recompose/createEventHandler';
-import { Col, Row, ControlLabel, FormGroup } from 'react-bootstrap';
+import moment from "moment";
+import DateTimePicker from "./DateTimePicker";
+import mapPropsStream from "recompose/mapPropsStream";
+import { $atom } from "@graphistry/falcor-json-graph";
+import createEventHandler from "recompose/createEventHandler";
+import { Col, Row, ControlLabel, FormGroup } from "react-bootstrap";
 
-const defaultToTime = '11:59:59 PM';
-const defaultFromTime = '12:00:00 AM';
-const timePickerPlaceholder = 'Select a date';
+const defaultToTime = "11:59:59 PM";
+const defaultFromTime = "12:00:00 AM";
+const timePickerPlaceholder = "Select a date";
 
 function getTimeProps(range, baseid, dir, defaultTime) {
   const base = range[dir] || {};
 
   return {
     date: base.date,
-    baseid: `${baseid}_${dir || ''}`,
+    baseid: `${baseid}_${dir || ""}`,
     placeholder: timePickerPlaceholder,
-    timezone: base.timezone || 'America/Los_Angeles',
-    time: base.time || moment.utc(defaultTime, 'hh:mm:ss a').toJSON()
+    timezone: base.timezone || "America/Los_Angeles",
+    time: base.time || moment.utc(defaultTime, "hh:mm:ss a").toJSON()
   };
 }
 
@@ -26,42 +26,53 @@ export const withTimeRanges = mapPropsStream(propsStream => {
   const { handler: onFromChange, stream: fromChanges } = createEventHandler();
 
   const changes = toChanges.merge(fromChanges);
-  const handleToChange = val => onToChange({ val, dir: 'to' });
-  const handleFromChange = val => onFromChange({ val, dir: 'from' });
+  const handleToChange = val => onToChange({ val, dir: "to" });
+  const handleFromChange = val => onFromChange({ val, dir: "from" });
 
-  return propsStream.switchMap(({ $falcor, baseid, range = {}, timeKey = 'time', ...props }) => {
-    const ranges = changes.scan(
-      (acc, { dir, val }) => ({
-        ...acc,
-        [dir]: { ...acc[dir], ...val }
-      }),
-      range
-    );
-    const rangesCommitted = ranges.switchMap(
-      range =>
-        $falcor
-          .set({
-            json: {
-              [timeKey]: $atom(range)
-            }
-          })
-          .progressively(),
-      (range, { json }) => json[timeKey]
-    );
-    return rangesCommitted.startWith(range).map(range => ({
-      ...props,
-      toProps: {
-        ...getTimeProps(range, baseid, 'to', defaultToTime),
-        ...props.toProps,
-        onValueChange: handleToChange
-      },
-      fromProps: {
-        ...getTimeProps(range, baseid, 'from', defaultFromTime),
-        ...props.fromProps,
-        onValueChange: handleFromChange
-      }
-    }));
-  });
+  return propsStream.switchMap(
+    ({
+      $falcor,
+      baseid,
+      range = {},
+      rangeChanged = () => {},
+      timeKey = "time",
+      ...props
+    }) => {
+      const ranges = changes
+        .scan(
+          (acc, { dir, val }) => ({
+            ...acc,
+            [dir]: { ...acc[dir], ...val }
+          }),
+          range
+        )
+        .do(rangeChanged);
+      const rangesCommitted = ranges.switchMap(
+        range =>
+          $falcor
+            .set({
+              json: {
+                [timeKey]: $atom(range)
+              }
+            })
+            .progressively(),
+        (range, { json }) => json[timeKey]
+      );
+      return rangesCommitted.startWith(range).map(range => ({
+        ...props,
+        toProps: {
+          ...getTimeProps(range, baseid, "to", defaultToTime),
+          ...props.toProps,
+          onValueChange: handleToChange
+        },
+        fromProps: {
+          ...getTimeProps(range, baseid, "from", defaultFromTime),
+          ...props.fromProps,
+          onValueChange: handleFromChange
+        }
+      }));
+    }
+  );
 });
 
 const defaultLabelColumns = { xs: 4, sm: 4, md: 4, lg: 4 };
@@ -77,7 +88,7 @@ export const DateTimeRangePicker = withTimeRanges(function DateTimeRange({
 }) {
   return (
     <FormGroup {...formGroupProps}>
-      {label === null || label === undefined || label === '' ? (
+      {label === null || label === undefined || label === "" ? (
         undefined
       ) : (
         <Row>
