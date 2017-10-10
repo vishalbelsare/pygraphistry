24933b12ef49afee8be9d0467359600f36700974
Paul Taylor <paul.e.taylor@me.com>
2017-10-10 13:27:25 -0700

    use pbf to deserialize and partition vgraph faster (wip)

---
+++ packages/lib-vgraph/src/pbf/runner.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as Pbf from 'pbf';
+import * as path from 'path';
+
+import { VectorMetadata } from './partition';
+import { fork, ChildProcess, ForkOptions } from 'child_process';
+
+const workerPath = path.resolve(require.resolve('./worker'));
+
+export interface RunnerOptions extends ForkOptions {
+    fileDir: string;
+    workers?: number;
+}
+
+export function runner(options: RunnerOptions) {
+    const numWorkers = options.workers || os.cpus().length;
+    return function runWorkers(vectors: VectorMetadata[]) {
+        
+    }
+}
