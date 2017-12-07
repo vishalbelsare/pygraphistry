affd2452acd8f871e1b7780257e80faa6ec2f550
Miller Hooks <millerhooks@Millers-MacBook-Pro.local>
2017-12-06 20:22:25 -0600

    remove development from history

--- packages/pivot-app/src/shared/services/connectors/index.js
+++ packages/pivot-app/src/shared/services/connectors/index.js
@@ -1,5 +1,5 @@
 export * from './splunk.js';
-// export * from './neo4j.js';
+export * from './neo4j.js';
 export * from './elasticsearch.js';
 export * from './whois.js';
 export * from './http.js';
